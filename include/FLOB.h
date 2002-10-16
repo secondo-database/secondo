bba4afccb719d90a485a18b259fa50d1244eea06
guenster <>
2002-10-16 15:23:25 +0000

    FLOB.h small modifications

--- include/FLOB.h
+++ include/FLOB.h
@@ -11,7 +11,6 @@ private:
   SmiRecord lob;
   SmiRecordId lobId;
   
-  //char *start;
   int size;
 
   friend class Tuple;
@@ -20,21 +19,20 @@ private:
   
 public:
 	char *start;
-  FLOB(SmiRecordFile* inlobFile);
-  FLOB(SmiRecordFile* inlobFile, int sz);   /* Create from scratch */
-  ~FLOB();        /* Destroy LOB instance */
+  	FLOB(SmiRecordFile* inlobFile);
+  	FLOB(SmiRecordFile* inlobFile, int sz);   /* Create from scratch */
+  	~FLOB();        /* Destroy LOB instance */
   
-  void Destroy(); /* Destroy persistent representation */
+  	void Destroy(); /* Destroy persistent representation */
  
-  void Get(int offset, int length, char *target); /* Read by copying */
-  void Write(int offset, int length, char *source);
-   int Size();
+  	void Get(int offset, int length, char *target); /* Read by copying */
+  	void Write(int offset, int length, char *source);
+   	int Size();
   
-  // to be worked out later
-  // void Resize(int size);
+  	void Resize(int size);
   
-   int Restore(char *address); /* Restore from byte string */
-  bool IsLob();                /* true, if value stored in underlying LOB, otherwise false */
+   	int Restore(char *address); /* Restore from byte string */
+  	bool IsLob();                /* true, if value stored in underlying LOB, otherwise false */
   
 };
 #endif
