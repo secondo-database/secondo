cdd08594c2530cb4e5adc04a23d4b8f4d0cf2653
spieker <>
2003-01-23 18:09:52 +0000

    Small improvements and comment added.

--- setvar.bash
+++ setvar.bash
@@ -1,6 +1,20 @@
 # bash Script for setting up a secondo environment
 #
 # The first argument is interpreted as SECONDO_BUILD_DIR
+# copy this file to an apropriate location and set up
+# an shell alias in your .bashrc file:
+#
+#    alias setvar  source <newpath>/setvar.bash
+#
+# Adjust the directory entries in the configurable
+# part.
+#
+# if you want this script executed at startup of the shell
+# add the command 
+#
+#    source <newpath>/setvar.bash <secondo_directory>
+#
+# in your profile.
 
 if [ -z "$FIRST_CALL" ]; then
   export FIRST_CALL="true"
@@ -23,29 +37,44 @@ fi
 
 fi
 
-export SECONDO_PLATFORM="win32"
-
 export SECONDO_BUILD_DIR="$1"
+
+#########################################################
+##
+## start configurable part
+##
+#########################################################
+
+export SECONDO_PLATFORM="win32"
 
 export SECONDO_CONFIG="/SecBase/secondo/bin/MyConfig.ini"
 
 export BERKELEY_DB_DIR="/usr/local/db-4.0.14"
 
-if [ $SECONDO_PLATFORM != "win32" ]; then
-   export PATH="$COPY_OF_PATH:$SECONDO_BUILD_DIR/bin"
-   export LD_LIBRARY_PATH="$COPY_OF_LD_PATH:$SECONDO_BUILD_DIR/lib:$BERKELEY_DB_DIR/lib"
-else
-   # windows looks for dlls in the directories included in the PATH variable
-   export PATH="$COPY_OF_PATH:$SECONDO_BUILD_DIR/bin:$SECONDO_BUILD_DIR/lib"	
-fi
-
-
 export CVSROOT=":pserver:spieker@robinson.fernuni-hagen.de:2401/cvs-projects/CVS_REPOS"
 
-echo "SECONDO_PLATFORM: " $SECONDO_PLATFORM
-echo "SECONDO_BUILD_DIR: " $SECONDO_BUILD_DIR
-echo "SECONDO_CONFIG: " $SECONDO_CONFIG
-echo "BERKELEY_DB_DIR: " $BERKELEY_DB_DIR
-echo "PATH: " $PATH
-echo "LD_LIBRARY_PATH: " $LD_LIBRARY_PATH
-echo "CVSROOT: " $CVSROOT
+if [ $SECONDO_PLATFORM != "win32" ]; then 
+   
+   # unix
+   export PL_INCLUDE_DIR="/Programme/pl/include"
+   export PL_LIBRARY_DIR="/Programme/pl/lib"
+
+else 
+
+   # windows use \ as separator	
+   export SWI_HOME_DIR="C:\Programme\pl"
+fi
+
+#########################################################
+##
+## end of configurable part
+##
+#########################################################
+
+if [ $SECONDO_PLATFORM != "win32" ]; then
+   export PATH="$COPY_OF_PATH:$SECONDO_BUILD_DIR/bin"
+   export LD_LIBRARY_PATH="$COPY_OF_LD_PATH:$SECONDO_BUILD_DIR/lib:$BERKELEY_DB_DIR/lib:$PL_LIBRARY_DIR" 
+else
+   export PATH="$COPY_OF_PATH:$SECONDO_BUILD_DIR/bin:$SECONDO_BUILD_DIR/lib:$SWI_HOME_DIR/bin"
+#export LD_LIBRARY_PATH="$COPY_OF_LD_PATH:$SECONDO_BUILD_DIR/lib:$BERKELEY_DB_DIR/lib:$PL_LIBRARY_DIR"
+fi
--- setvar.csh
+++ setvar.csh
@@ -1,6 +1,23 @@
 # csh Script for setting up a secondo environment
 #
 # The first argument is interpreted as SECONDO_BUILD_DIR
+# copy this file to an apropriate location and set up
+# an shell alias in your .cshrc file:
+#
+#    alias setvar  source <newpath>/setvar.bash
+#
+# Adjust the directory entries in the configurable
+# part.
+#
+# if you want this script executed at startup of the shell
+# add the command 
+#
+#    source <newpath>/setvar.csh <secondo_directory>
+#
+# in your login or login_user file.
+#
+# Note: This file is not suitable for setting the environment
+# on windows. You can only use the setvar.bash there.
 
 if (! $?FIRST_CALL) then
   setenv FIRST_CALL "true"
@@ -23,24 +40,31 @@ endif
 
 endif
 
+setenv SECONDO_BUILD_DIR "$argv[1]"
+
+#########################################################
+##
+## start configurable part
+##
+#########################################################
 
 setenv SECONDO_PLATFORM "solaris"
-
-setenv SECONDO_BUILD_DIR "$argv[1]"
 
 setenv SECONDO_CONFIG "$HOME/SecBase/MyConfig.ini"
 
 setenv BERKELEY_DB_DIR "/usr/local/Berkeley-DB"
 
+setenv CVSROOT=":pserver:spieker@robinson.fernuni-hagen.de:2401/cvs-projects/CVS_REPOS"
+
+setenv PL_INCLUDE_DIR="/Programme/pl/include"
+setenv PL_LIBRARY_DIR="/Programme/pl/lib"
+
+#########################################################
+##
+## end of configurable part
+##
+#########################################################
+
 setenv PATH $COPY_OF_PATH":$SECONDO_BUILD_DIR/bin"
 
 setenv LD_LIBRARY_PATH $COPY_OF_LD_PATH":$SECONDO_BUILD_DIR/lib:$BERKELEY_DB_DIR/lib"
-
-setenv CVSROOT=":pserver:spieker@robinson.fernuni-hagen.de:2401/cvs-projects/CVS_REPOS"
-
-echo "SECONDO_PLATFORM: " $SECONDO_PLATFORM
-echo "SECONDO_BUILD_DIR: " $SECONDO_BUILD_DIR
-echo "SECONDO_CONFIG: " $SECONDO_CONFIG
-echo "BERKELEY_DB_DIR: " $BERKELEY_DB_DIR
-echo "PATH: " $PATH
-echo "LD_LIBRARY_PATH: " $LD_LIBRARY_PATH
