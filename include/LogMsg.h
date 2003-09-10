df32fc3ae9ca32e3378f7095d4a75980db7411e9
spieker <>
2003-09-10 15:09:50 +0000

    New class for logging information.

---
+++ include/LogMsg.h
@@ -0,0 +1,31 @@
+#ifndef CLASS_LOGMSG_H
+#define CLASS_LOGMSG_H
+
+#include <map>
+#include <string>
+
+
+#ifndef LOGMSG_OFF
+#define LOGMSG(a, b) if ( LogMsg::isActive(a) ) { b }
+#endif
+
+class LogMsg {
+
+public:
+
+  LogMsg(){};
+  ~LogMsg(){};
+
+  static void initByString( const string &keyList );
+
+  static bool isActive( const string &key ); 
+
+private:
+
+  static map<string,bool> logMap;
+  
+  typedef map<string,bool>::iterator logMapIter;
+
+};
+
+#endif
