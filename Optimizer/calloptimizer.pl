97d21c26c51253852a9f562a99c19a884d2f43e1
gueting <>
2003-09-25 13:20:26 +0000

    Reorganized to separate operator syntax.

--- Optimizer/calloptimizer.pl
+++ Optimizer/calloptimizer.pl
@@ -11,15 +11,9 @@ The optimizer is started by loading this file.
 
 */
 
+:- [opsyntax].
+
 :-
-  op(800, xfx, =>),
-  op(800, xfx, <=),
-  op(800, xfx, #),
-  op(800, xfx, div),
-  op(800, xfx, mod),
-  op(800, xfx, starts),
-  op(800, xfx, contains),
-  op(200, xfx, :),
   assert(highNode(0)),
   assert(boundarySize(0)),
   assert(boundaryMaxSize(0)),
