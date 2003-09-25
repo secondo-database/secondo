4a3d6cbd7961088f903b4fa7ef37d044751a50a5
gueting <>
2003-09-25 13:22:07 +0000

    Rearranged a bit to make extensions easier.

--- Optimizer/database.pl
+++ Optimizer/database.pl
@@ -3,19 +3,8 @@
 
 [File ~database.pl~]
 
-1.1 Rules about Commutativity of Predicates
 
-*/
-
-commute(X = Y, Y = X).
-commute(X < Y, Y > X).
-commute(X <= Y, Y >= X).
-commute(X > Y, Y < X).
-commute(X >= Y, Y <= X).
-commute(X # Y, Y # X).
-
-/*
-1.2 Relation Schemas
+1.1 Relation Schemas
 
 */
 relation(staedte, [sname, bev, plz, vorwahl, kennzeichen]).
@@ -24,6 +13,11 @@ relation(ten, [no]).
 relation(thousand, [no]).
 relation(orte, [kennzeichen, ort, vorwahl, bevt]).
 
+
+/*
+1.2 Spelling of Relation and Attribute Names
+
+*/
 spelling(staedte:plz, pLZ).
 spelling(staedte:sname, sName).
 spelling(plz, lc(plz)).
@@ -34,11 +28,36 @@ spelling(thousand, lc(thousand)).
 spelling(thousand:no, lc(no)).
 spelling(orte:bevt, bevT).
 
+
+/*
+
+1.3  Cardinalities of Relations
+
+*/
+card(staedte, 58).
+card(staedte_sample, 58).
+card(plz, 41267).
+card(plz_sample, 428).
+card(ten, 10).
+card(ten_sample, 10).
+card(thousand, 1000).
+card(thousand_sample, 89).
+card(orte, 506).
+card(orte_sample, 100).
+
+
+/*
+1.4 Indexes
+
+*/
 hasIndex(Rel, attr(_:A, _, _), IndexName) :-
   hasIndex(Rel, attr(A, _, _), IndexName).
 
-hasIndex(rel(plz, _, _), attr(ort, _, _), plz_Ort).
-hasIndex(rel(plz, _, _), attr(pLZ, _, _), plz_PLZ).
+hasIndex(rel(Rel, _, _), attr(Attr, _, _), Index) :- index(Rel, Attr, _, Index).
+
+index(plz, ort, btree, plz_Ort).
+index(plz, plz, btree, plz_PLZ).
+
 
 
 
