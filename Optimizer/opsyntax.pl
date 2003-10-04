a40045db0972d13b8d7b9a2896a7c5c3de7d6dc6
gueting <>
2003-10-04 14:48:02 +0000

    New method to specify operator syntax.

--- Optimizer/opsyntax.pl
+++ Optimizer/opsyntax.pl
@@ -16,3 +16,59 @@
   op(200, xfx, :).
 
 :- op(800, xfx, inside).
+:- op(800, xfx, intersects).
+:- op(800, xfx, touches).
+
+/*
+
+----	secondoOp(Op, Syntax, NoArgs) :-
+----
+
+~Op~ is a Secondo operator written in ~Syntax~, with ~NoArgs~ arguments.
+Currently implemented:
+
+  * postfix, 1 or 2 arguments: corresponds to _ # and _ _ #
+
+  * postfixbrackets, 2 or 3 arguments, of which the last one is put into
+the brackets: _ # [ _ ] or _ _ # [ _ ]
+
+  * prefix, 2 arguments: # (_, _)
+
+  * prefix, either 1 or 3 arguments, does not need a rule here, is
+translated by default.
+
+  * infix, 2 arguments: does not need a rule, translated by default.
+
+For all other forms, a plan_to_atom rule has to be programmed explicitly.
+
+*/
+
+
+secondoOp(feed, postfix, 1).
+secondoOp(consume, postfix, 1).
+secondoOp(count, postfix, 1).
+secondoOp(product, postfix, 2).
+secondoOp(filter, postfixbrackets, 2).
+secondoOp(loopjoin, postfixbrackets, 2).
+secondoOp(exactmatch, postfixbrackets, 3).
+secondoOp(leftrange, postfixbrackets, 3).
+secondoOp(rightrange, postfixbrackets, 3).
+secondoOp(extend, postfixbrackets, 2).
+secondoOp(remove, postfixbrackets, 2).
+secondoOp(project, postfixbrackets, 2).
+secondoOp(sortby, postfixbrackets, 2).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
