a3a08094df121bacd95f706614bbdda443d419b4
spieker <>
2003-10-07 16:15:44 +0000

    Interface should now work with the main memory relational algebra.

--- include/DBArray.h
+++ include/DBArray.h
@@ -1,133 +1,254 @@
-/******************************************************************************
-//paragraph	[10]	title:		[\centerline{\Large \bf] [}]
-//paragraph	[11]	TTp:		[\begin{quote} {TODO: ] [} \end{quote}]
-//paragraph	[12]	Serif:		[{\sf ] [}]
-//characters	[1]	tt:		[{\tt ] [}]
-//characters	[2]	Type:		[\underline{\em ] [}]
-//characters	[3]	ProperName:	[{\sc ]	[}]
-//[i]	[\'{\i}]
-//[n]	[\~{n}]
-//[ae] [\"a]
-//[ue] [\"u]
-//[oe] [\"o]
-
-
-\title{DBArray.h}
-\author{Miguel Rodr[i]guez Luaces}
-\date{April 15th, 1998}
-
-\maketitle
-
-[12] October 30th, 2001 Mirco G[ue]nster. Port to the new Secondo SMI.
-
-[12] April 15th, 1999. MRL. First Version.
-
-[12] May 28th, 1999. MRL. Comments.
-
-Printed on: \today
-
-1 Overview
-
-This file declares the database array interface
-A database array offers an open array of elements of
-equal size called ~slots~. One can read valid slots with "Get"[1] and
-write in any slot with "Put"[1]. The size of the database array changes
-automatically if a "Put"[1] operation refers to a non existent
-element.
-
-2 Implementation
-
-******************************************************************************/
-#ifndef DBARRAY_H
-#define DBARRAY_H
-
-#include "FLOB.h"
-
-
-/* 
-
-A database array is implemented in top of Secondo FLOBs, so a database 
-array is a FLOB itself and it can be used as a FLOB. 
-
-*/
-
-class DBArray : public FLOB {
-	private:
-		// Slot number of highest valid element.
-  		int mHigh;
-		// Size of each slot.
-  		int mSlotSize;
-		// Size of the DBArray (number of slots allocated).
-  		int mSize;
-		// Size of the DB information (header of the DBArray).
-  		int mSizeOfMetaInfo;
-		
-	public:
-
-/* 
-
-2.1 Contructor which takes an SmiRecordFile only, to be used by the tuple
-manager. It cannot used in a program.
-
-*/
-  		DBArray(SmiRecordFile *inlobFile);
-		
-/* 
-
-2.2 Creates a dbarray with slots of size SlotSize. The initial number of slots 
-allocated is SizeClue. 
-
-*/ 
-  		DBArray(SmiRecordFile *inlobFile, int SlotSize, 
-				int SizeClue = 20);
-		
-/* 
-
-2.3 Destroys the handle to the DBArray, but not the persistent array. 
-
-*/
-  		~DBArray();
-  
-/* 
-
-2.4 Destroys the persistent array. 
-
-*/
-  		void Destroy();
-		
-/* 	
-
-2.5 Changes the size of the DBArray to NewSize. 
-NewSize must be smaller than the actual size
-of the DBArray. 
-
-*/
-  		void Shrink(int NewSize);
-		
-		
 /*
- 
-2.6 Copies the slot with index Index to the memory
-starting at Dest. 
+//paragraph	[10]	title:		[{\Large \bf ] [}]
+//paragraph	[11]	title:		[{\large \bf ] [}]
+//paragraph	[12]	title:		[{\normalsize \bf ] [}]
+//paragraph	[21]	table1column:	[\begin{quote}\begin{tabular}{l}]	[\end{tabular}\end{quote}]
+//paragraph	[22]	table2columns:	[\begin{quote}\begin{tabular}{ll}]	[\end{tabular}\end{quote}]
+//paragraph	[23]	table3columns:	[\begin{quote}\begin{tabular}{lll}]	[\end{tabular}\end{quote}]
+//paragraph	[24]	table4columns:	[\begin{quote}\begin{tabular}{llll}]	[\end{tabular}\end{quote}]
+//[--------]	[\hline]
+//characters	[1]	verbatim:	[$]	[$]
+//characters	[2]	formula:	[$]	[$]
+//characters    [3]    capital:    [\textsc{]    [}]
+//characters    [4]    teletype:   [\texttt{]    [}]
+//[ae] [\"a]
+//[oe] [\"o]
+//[ue] [\"u]
+//[ss] [{\ss}]
+//[<=] [$\leq$]
+//[#]  [\neq]
+//[tilde] [\verb|~|]
+
+1 Header File: DBArray
+
+Version: 0.7
+
+August 2002 RHG
+
+1.1 Overview
+
+This module offers a generic persistent array implemented on top of the
+SecondoSMI interface.
+
+
+
+1.2 Interface methods
+
+This module offers the following methods:
+
+[23]	Creation/Removal 	& Access   	& Inquiries	\\ 	
+	[--------]
+	DBArray        		& Get 		& Size		\\  	
+	[tilde]DBArray		& Put		& Id		\\
+	MarkDelete		&		& 		\\
+
+
+1.3 Class ~DBArray~
+
+An instance of the class is a handle to a persistent array of fixed size with
+
+elements of type ~T~.
 
 */
-  		void Get(int Index, char *Dest);
-		
-/* 
 
-2.7 Copies from memory starting at Source into the slot with
-index Index. If necessary the DBArray will grow to make
-this slot available.  
+#ifndef MARRAY_H
+#define MARRAY_H
+
+#include <iostream> 
+#include <cassert>
+#include <vector>
+#include <algorithm>
+#include "GArray.h"
+
+template<class T>
+class DBArray : public GArray<T>
+{
+ public:
+
+  DBArray( SmiRecordFile *parrays, const int initsize = 0 );
+/*
+Creates a new ~SmiRecord~ on the ~SmiRecordFile~ for this
+persistent array. One can define an initial size of the persistent
+array with the argument ~initsize~. 
 
 */
-  		void Put(int Index, char *Source);
-	
-/* 
 
-2.8 Returns the current highest valid index. 
+  DBArray( const int initsize = 0 );
+/*
+Create a new memory version of the DBArray. It is used for temporary arrays.
 
 */
-		  		int High();
+  
+  DBArray( SmiRecordFile *parrays, const SmiRecordId& id, const bool update = true );
+/*
+Opens the ~SmiRecordFile~ and the ~SmiRecord~ for the persistent array. The boolean 
+value ~update~ indicates if open mode: ~true~ for update and ~false~ for read-only.
+
+*/
+
+  ~DBArray();
+/*
+Destroys the handle. If the array is marked for deletion, then it also destroys the
+persistent array.
+
+*/
+
+  void MarkDelete();
+/*
+Marks the persistent array for deletion. It will be permanently deleted on the 
+destruction of the object.
+
+*Precondition:* The array must be opened in update mode.
+
+*/
+
+  void Put(int const index, const T& elem);
+/*
+Copies element ~elem~ into the persistent array at index ~index~.
+
+*Precondition:* 0 [<=] ~index~ [<=] ~size~ - 1. The array must be opened in update mode.
+
+*/
+
+  void Get(int const index, T& elem);
+/*
+Returns the element ~index~ of the array.
+
+*Precondition:* 0 [<=] ~index~ [<=] ~size~ - 1. 
+
+*/
+
+  void Clear();
+/*
+Clears the persistent array.
+
+*/
+
+  void Sort( bool (*cmp)(const T&, const T&) )  
+  {
+    if( size <= 1 ) 
+      return;
+
+    sort( marray->begin(), marray->end(), cmp );
+  }
+/*
+Sorts the persisten array given the ~cmp~ comparison criteria.
+
+*/
+
+  const int Size() const;
+/*
+Returns the size of this array.
+
+*/
+
+  const SmiRecordId Id() const;
+/*
+Returns the identifier of this array.
+
+*/
+
+ private:
+
+  bool writeable;
+  int size;
+  vector<T> *marray;
+
 };
 
+
+/*
+2 Implementation of DBArray
+
+Version: 0.7
+
+August 2002 RHG
+October 2003 M. Spiekermann
+
+2.1 Overview
+
+This module offers the interface of a generic persistent array implemented on top of the
+SecondoSMI interface. It is the same as DBArray but renamed into DBArray
+
+*/
+
+template<class T>
+DBArray<T>::DBArray( SmiRecordFile *parrays, const int initsize ) :
+writeable( true ),
+size( 0 ),
+marray( new vector<T>( initsize ) )
+{
+}
+
+template<class T>
+DBArray<T>::DBArray( const int initsize ) :
+writeable( true ),
+size( 0 ),
+marray( new vector<T>( initsize ) )
+{
+}
+
+template<class T>
+DBArray<T>::DBArray( SmiRecordFile *parrays, const SmiRecordId& id, const bool update ) 
+{
+  assert( false );
+}
+
+template<class T>
+DBArray<T>::~DBArray()
+{
+  delete marray;  
+}
+
+template<class T>
+void DBArray<T>::Clear()
+{
+  marray->clear();
+  size = 0;
+}
+
+template<class T>
+void DBArray<T>::Put(const int index, const T& elem)
+{
+  assert ( writeable );
+  
+  if ( size <= index ) 
+  {
+    size = index + 1;
+    marray->resize( size );
+  }
+
+  (*marray)[index] = elem;
+}
+
+
+template<class T>
+void DBArray<T>::Get(int const index, T& elem)
+{
+  assert ( 0 <= index && index < size );
+
+  elem = (*marray)[index];
+}
+
+template<class T>
+void DBArray<T>::MarkDelete() 
+{
+  assert( writeable );
+}
+
+
+template<class T>
+const int DBArray<T>::Size() const
+{
+  return size;
+}
+
+
+template<class T>
+const SmiRecordId DBArray<T>::Id() const 
+{ 
+  return 0;
+}
+
 #endif
+
