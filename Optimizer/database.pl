5cd8a4745075cfc0149340048987166e8c05b0ea
gueting <>
2003-11-11 15:43:09 +0000

    Added facts about database "spatial".

--- Optimizer/database.pl
+++ Optimizer/database.pl
@@ -13,6 +13,10 @@ relation(ten, [no]).
 relation(thousand, [no]).
 relation(orte, [kennzeichen, ort, vorwahl, bevt]).
 
+relation(distrikte, [nr, dname, flaeche, bev, bev_maennlich, gebiet, bev20]).
+relation(laender, [lname, gebiet]).
+relation(staedte2, [sname, ort, bev, plz, vorwahl, kennzeichen]).
+
 
 /*
 1.2 Spelling of Relation and Attribute Names
@@ -22,11 +26,16 @@ spelling(staedte:plz, pLZ).
 spelling(staedte:sname, sName).
 spelling(plz, lc(plz)).
 spelling(plz:plz, pLZ).
-spelling(ten, lc(ten)).
+spelling(ten, lc(ten)).  
 spelling(ten:no, lc(no)).
 spelling(thousand, lc(thousand)).
 spelling(thousand:no, lc(no)).
 spelling(orte:bevt, bevT).
+
+spelling(distrikte:dname, dName).
+spelling(laender:lname, lName).
+spelling(staedte2:sname, sName).
+spelling(staedte2:plz, pLZ).
 
 
 /*
@@ -45,6 +54,9 @@ card(thousand_sample, 89).
 card(orte, 506).
 card(orte_sample, 100).
 
+card(distrikte, 569).
+card(laender, 9).
+card(staedte2, 59).
 
 /*
 1.4 Indexes
@@ -59,6 +71,8 @@ hasIndex(rel(Rel, _, _), attr(Attr, _, _), Index) :- index(Rel, Attr, _, Index).
 
 index(plz, ort, btree, plz_Ort).
 index(plz, pLZ, btree, plz_PLZ).
+
+index(distrikte, gebiet, rtree, distrikte_Gebiet).
 
 
 index(_, _, _, _) :- fail.
