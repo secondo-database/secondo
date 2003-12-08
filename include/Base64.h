7ad461f20b1d8cfc479aaa94f3ec5966dad4b250
spieker <>
2003-12-08 18:42:52 +0000

    Heade File for Class Base64

---
+++ include/Base64.h
@@ -0,0 +1,37 @@
+#ifndef CLASS_BASE64_H
+#define CLASS_BASE64_H
+
+/*
+December 08, M. Spiekermann: Initial Version
+
+This class provides static member functions used for encoding and decoding
+the Base64 data format as specified in RFC 1341. The interface was designed
+to interact kindly with the text atoms of nested lists. 
+
+*/
+
+#include <string>
+
+using namespace std;
+
+class Base64{
+
+public:
+   Base64(){};
+   ~Base64(){};    
+
+   static void encode(char* buffer, string& text, int size);
+   /*  encodes size bytes of the buffer into base64 returned as string.  
+    *  If finish is true the data will be closed, e.g. padded with "=".
+    */ 
+   static bool decode(char* buffer, string text);
+
+private:
+   /* the characters used in the base64 format */
+   static char base64Alphabet[];
+
+
+};
+
+#endif
+
