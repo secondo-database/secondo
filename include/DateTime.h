45ee348c65f6c814dd918a235d94721563c758f2
behr <>
2004-05-13 10:32:49 +0000

    Changes for pd-system

--- include/DateTime.h
+++ include/DateTime.h
@@ -1,7 +1,9 @@
 /*
 \sloppy
 
-1 The Class DateTime
+1 The Header File of DateTime
+
+1.1 Remarks
 
 This class can be used to represent time. They are two kinds of time,
 durations and instants. The class ~DateTime~ can handle each of
@@ -15,7 +17,6 @@ checked by calling ~assert~. Note that a few of the operators
 change the type of the calling instance. E.g. calling the ~Minus~
 function on an {\tt instant} with an argument of {\tt instant}
 will change the type of the this object to a {\tt duration}.
-
 
 1.1 Includes and Constants
 
@@ -38,7 +39,7 @@ will change the type of the this object to a {\tt duration}.
 
 This number represents the time reference point of this Time class
 as julian day. The value must be a multiple of 7.
-The value 2451547 corresponds to 3.1.2004
+The value 2451547 corresponds to 3.1.2000
 
 */
 #define NULL_DAY  2451547
@@ -49,7 +50,6 @@ The value 2451547 corresponds to 3.1.2004
 */
 enum TimeType  {instanttype, durationtype};
 
-
 /*
 1.2 The class declaration
 
@@ -59,7 +59,7 @@ class DateTime : public StandardAttribute  {
   public:
 
 /*
-1.2.1 The empty Constructor
+~Constructor~
 
 The empty constructor is the special constructor for the
 cast function.
@@ -68,8 +68,7 @@ cast function.
      DateTime();
 
 /*
-1.2.2 Constructor
-
+~Constructor~
 
 The next constructor creates a datetime instant at
 the nullday and duration 0 respectively. The created type of
@@ -79,7 +78,7 @@ this DateTime is fixed by the argument.
      DateTime(TimeType type);
 
 /*
-1.2.3 Constructor
+~Constructor~
 
 This constructor creates a DateTime instance at the specified
 day, milliseconds and TimeType.
@@ -88,13 +87,13 @@ day, milliseconds and TimeType.
      DateTime(const long Day,const long MilliSeconds,TimeType type);
 
 /*
-1.2.4 Destructor
+~Destructor~
 
 */
      ~DateTime();
 
 /*
-1.2.5 GetDay
+~GetDay~
 
 This function returns the day part of a duration.
 ~GetDay~ is only defined for duration types.
@@ -103,7 +102,7 @@ This function returns the day part of a duration.
      long GetDay();
 
 /*
-1.2.6 GetAllMilliseconds
+~GetAllMilliseconds~
 
 The function ~GetMilliseconds~ returns the time part of the day.
 The result will be in interval [ 0, 86400000 ].
@@ -111,13 +110,12 @@ The result will be in interval [ 0, 86400000 ].
 */
      long GetAllMilliSeconds();
 
-
 /*
-1.2.7 Access Functions
+~Access Functions~
 
 The next functions extract information of an instant.
 In the comment the value range of the result is given.
-All this fucntions can only applied to instanty types.
+All this functions can only applied to instant types.
 
 */
      int GetGregDay();        // [1..31]
@@ -127,12 +125,10 @@ All this fucntions can only applied to instanty types.
      int GetMinute();         // [0..59]
      int GetSecond();        // [0..59]
      int GetMillisecond();   // [0..999]
-
      int GetWeekday();       // [0..6] <=> [Monday .. Sunday]
 
-
 /*
-1.2.9 Destroy
+~Destroy~
 
 The familiar ~Destroy~ function included in the most algebras.
 
@@ -140,7 +136,7 @@ The familiar ~Destroy~ function included in the most algebras.
      void Destroy();
 
 /*
-1.2.10 Open
+~Open~
 
 This function overwrites the inherited ~Open~ function for the reason
 of resistency against recompiling.
@@ -149,7 +145,7 @@ of resistency against recompiling.
      virtual void Open(SmiRecord& valueRecord, const ListExpr typeInfo);
 
 /*
-1.2.11 Save
+~Save~
 
 The ~Save~ function is the counterpart of the ~Open~ function.
 
@@ -157,9 +153,9 @@ The ~Save~ function is the counterpart of the ~Open~ function.
      virtual void Save(SmiRecord& valueRecord, const ListExpr typeInfo);
 
 /*
-1.2.12 Conversion from and into other formats
+~Conversion from and into other formats~
 
-1.12.1 ToListExpr
+~ToListExpr~
 
 This function converts a DateTime into its nested list representation.
 The output depends on the type of this DateTime as well as the argument.
@@ -168,7 +164,7 @@ The output depends on the type of this DateTime as well as the argument.
      ListExpr ToListExpr(bool typeincluded);
 
 /*
-1.12.2 ReadFrom
+~ReadFrom~
 
 This function reads the value of this DateTime instance from the argument {\tt LE}.
 The possible formats are described in {\tt STFormat}, which can be found in
@@ -177,9 +173,8 @@ the {\tt document} directory of {\textsc{SECONDO}}.
 */
      bool ReadFrom(ListExpr LE, bool typeincluded);
 
-
 /*
-1.2.8 ToString
+~ToString~
 
 The function ~ToString~ returns the string representation of this
 DateTime instance. The format depends on the type of time.
@@ -192,25 +187,24 @@ day$\mid\mid$millisecond.
      string ToString();
 
 /*
-1.2.9 ~SetType~
+~SetType~
 
-Sets the type of this DateTime;
+Sets the type of this DateTime. Be careful in using of this function.
 
 */
       void SetType(TimeType TT);
 
 /*
-1.2.9 ~SetType~
+~GetType~
 
-Sets the type of this DateTime;
+This function returns the type of this DateTime;
 
 */
       TimeType GetType();
 
 
-
 /*
-1.12.3 ReadFrom
+~ReadFrom~
 
 This function reads the value of this DateTime from the argument.
 This function is only defined for instants.
@@ -218,9 +212,8 @@ This function is only defined for instants.
 */
      bool ReadFrom(const string Time);
 
-
 /*
-1.12.3 ReadFrom
+~ReadFrom~
 
 This functions read the value of this DateTime from the given double
 value. The return value is allways true.
@@ -228,9 +221,8 @@ value. The return value is allways true.
 */
     bool ReadFrom(const double Time);
 
-
 /*
-1.2.7 ToDouble
+~ToDouble~
 
 The ~GetDouble~ function returns this DateTime instance as an
 double value.
@@ -238,9 +230,8 @@ double value.
 */
      double ToDouble();
 
-
 /*
-1.2.13 CompareTo
+~CompareTo~
 
 This function compares this instance with the
 value of the argument. The types of this and P2 have to
@@ -250,7 +241,7 @@ be equal.
      int CompareTo(DateTime* P2);
 
 /*
-1.2.14 Mathematical functions
+~Mathematical Functions~
 
 The following functions are the usual mathematical
 operations and comparisons. These functions are only
@@ -300,18 +291,15 @@ The next two function are defined for any DateTime type.
      bool LessThanZero();
 
 /*
-1.2.15 Equalize
+~Equalize~
 
 When this function is called this DateTime instance takes its value
 from the argument.
 
 */
-
      void Equalize(DateTime* P2);
-
-
 /*
-1.2.16 IsValid
+~IsValid~
 
 This functions checks if the combination of the argument values
 build a valid date.
@@ -320,17 +308,17 @@ build a valid date.
      bool IsValid(int year, int month, int day);
 
 /*
-1.2.17 Now
+~Now~
 
-This functions reads out the currenty system time an adopt the
-value of tho DateTime to it. This function is only defined for
+This functions reads out the current system time an adopt the
+value of the DateTime to it. This function is only defined for
 an {\tt instant} type.
 
 */
      void Now();
 
 /*
-1.2.18 Today
+~Today~
 
 This function read this dateTime value from the System
 ignoring the time part. (for {\tt instant}s only)
@@ -338,10 +326,8 @@ ignoring the time part. (for {\tt instant}s only)
 */
      void Today();
 
-
-
 /*
-1.2.17 Algebra functions
+~Algebra Functions~
 
 The next functions are needed for the DateTime class to act as
 an attribute of a relation.
@@ -355,7 +341,24 @@ an attribute of a relation.
      size_t HashValue();
      void CopyFrom(StandardAttribute* arg);
      DateTime* Clone();
+
+/*
+~WriteToSmiRecord~
+
+The following function writes the content of this DateTime into
+the SmiRecord {\tt valueRecord} beginning at position {\tt offset}.
+After calling this function, {\tt offset} will holds the position
+behind the written data.
+
+*/
      void WriteToSmiRecord(SmiRecord& valueRecord, int& offset);
+/*
+~ReadFromSmiRecord~
+
+This function reads the value of this DateTime instance. The offset
+is adjusted.
+
+*/
      void ReadFromSmiRecord(SmiRecord& valueRecord, int& offset);
 
   private:
@@ -369,6 +372,4 @@ an attribute of a relation.
     long ToJulian(int year, int month, int day);
     void ToGregorian(long Julian, int &year, int &month, int &day);
 };
-
-
 
