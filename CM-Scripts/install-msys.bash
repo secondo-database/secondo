a1fcc07df4d55075e53b326e011989f999c539fe
spieker <>
2004-05-26 16:44:13 +0000

    Errors in install script removed

--- CM-Scripts/install-msys.bash
+++ CM-Scripts/install-msys.bash
@@ -1,5 +1,5 @@
 #!/bin/sh
-# install.bash - uncompress files and change MSYS configuration
+# install.bash <path> - uncompress files and change MSYS configuration
 #
 # Oct. 2003, M. Spiekermann - initial version
 # May  2004, M. Spiekermann - some improvements
@@ -54,16 +54,19 @@ printf "\n* '$cdpath' to '$instpath' \n"
 printf "\n\n* Creating mount points ... \n"
 
 
-mkdir "$msysdir/secondo-sdk"
-mkdir "$msysdir/secondo"
-mkdir "$msysdir/mingw"
+install -d "$msysdir/secondo-sdk"
+install -d "$msysdir/secondo"
+install -d "$msysdir/mingw"
 
 printf "\n\n* Installing unzip ..."
 
+install -d "$instpath/secondo-sdk/bin"
 cd "$instpath/secondo-sdk/bin"
 "$cdpath/non-gnu/unzip/unz550xN.exe"
 export PATH="$instpath/secondo-sdk/bin:$instpath/secondo-sdk/lib:$PATH"
+unzip -q -o "$cdpath/non-gnu/unzip/zip23xN.zip"
 
+cd "$instpath/secondo-sdk"
 printf "\n\n* Uncompressing archives ... "
 
 for folder in $cdpath/gnu $cdpath/non-gnu $cdpath/../java/cvs; do
@@ -71,20 +74,26 @@ for folder in $cdpath/gnu $cdpath/non-gnu $cdpath/../java/cvs; do
   gzFiles=$(find $folder -maxdepth 1 -name "*.*gz")
   for file in $zipFiles; do
     printf "\n  processing $file ..."
-    unzip -q -o $file
+    if { ! unzip -q -o $file; }; then
+      exit 1
+    fi
   done
   for file in $gzFiles; do
     printf "\n  processing $file ..."
-    tar -xzf $file
+    if { ! tar -xzf $file; }; then
+      exit 2 
+    fi
   done
 done
 
 cd "$HOME"
 printf "\n  Uncompressing SECONDO source files ..."
-tar -xzf "$cdpath/secondo.tgz"
+if { ! tar -xzf "$cdpath/secondo.tgz"; }; then
+  exit 3
+fi
 
 printf  "\n\n* Copying configuration files ... \n"
-cd "$instpath/secondo/CM-Scripts"
+cd "$HOME/secondo/CM-Scripts"
 cp --backup setvar.bash catvar.sh "$instpath/secondo-sdk/bin"
 cp --backup .secondorc .bashrc-sample "$HOME"
 cp --backup .bashrc-sample "$HOME/.bashrc"
@@ -92,10 +101,12 @@ cd "$instpath/secondo-sdk/bin"
 chmod u+x setvar.bash catvar.sh 
 cd "$HOME"
 chmod u+x .secondorc .bashrc
-cd "$instpath/secondo/Win32/MSYS"
+cd "$HOME/secondo/Win32/MSYS"
 cp --backup fstab profile "$msysdir/etc"
 
-"$cdpath/prolog/w32pl5010.exe"
+cd "$cdpath/prolog"
+w32pl5010.exe
+cd $cdpath
 
 printf  "\n* MSYS Configuration and file extraction has been finished."
 printf  "\n* Close all open MSYS windows and open a new one, otherwise"
