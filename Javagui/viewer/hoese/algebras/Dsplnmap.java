9807e2e2af09ae4282aa182053deabd7b27608a4
behr <>
2004-10-01 12:27:26 +0000

    New version of the display class for nautical maps by A. Lopper

--- Javagui/viewer/hoese/algebras/Dsplnmap.java
+++ Javagui/viewer/hoese/algebras/Dsplnmap.java
@@ -26,30 +26,52 @@ public class Dsplnmap extends DisplayGraph {
    * @see <a href="Dsplnmapsrc.html#init">Source</a>
    */
   public void init (ListExpr type, ListExpr value, QueryResult qr) {
+    value.writeListExpr();
     AttrName = type.first().symbolValue();
     ListExpr nmap;
-    ListExpr nmapRest;
     ListExpr rel1, rel2, rel3;
-    nmap= type.first();
-    nmapRest= type.rest();
-    rel1= nmapRest.third();
-    rel2= nmapRest.fourth();
-    rel3= nmapRest.fifth();
+    nmap= type.rest();
 
-    String ts;
+    if (nmap.listLength() == 3)
+    {
+       rel1= nmap.third();
+//       rel1.writeListExpr();
+       LEUtils.analyse(rel1, value.third(), qr);
+       qr.addEntry("---------");
+    }
+    else if (nmap.listLength() == 4)
+    {
+       rel2= nmap.fourth();
+//     rel2.writeListExpr();
+       LEUtils.analyse(rel2, value.fourth(), qr);
+       qr.addEntry("---------");
+       rel1= nmap.third();
+//     rel1.writeListExpr();
+       LEUtils.analyse(rel1, value.third(), qr);
+       qr.addEntry("---------");
+    }
+    else if (nmap.listLength() == 5)
+    {
+       rel3= nmap.fifth();
+//     rel3.writeListExpr();
+       LEUtils.analyse(rel3, value.fifth(), qr);
+       qr.addEntry("---------");
+       rel2= nmap.fourth();
+//     rel2.writeListExpr();
+       LEUtils.analyse(rel2, value.fourth(), qr);
+       qr.addEntry("---------");
+       rel1= nmap.third();
+//     rel1.writeListExpr();
+       LEUtils.analyse(rel1, value.third(), qr);
+       qr.addEntry("---------");
+    }
+    else
+    {
+       System.out.println("Error in ListExpr :parsing aborted");
+       qr.addEntry(new String("(" + AttrName + ": GA(nmap))"));
+       return;
+    }
 
-    rel1.writeListExpr();
-    LEUtils.analyse(rel1, value.third(), qr);
-    qr.addEntry("---------");
-
-    ts = rel2.writeListExprToString();
-    System.out.println("ts: "+ ts);
-    LEUtils.analyse(rel2, value.fourth(), qr);
-    qr.addEntry("---------");
-    ts = rel3.writeListExprToString();
-    System.out.println("ts: "+ ts);
-    LEUtils.analyse(rel3, value.fifth(), qr);
-    qr.addEntry("---------");
   }
 
 }
