843e683a2ebdf45a79bcc7543d8932f1960d7c7a
hoffman <>
2004-10-06 10:47:19 +0000

    Changed specification of type constructor 'polygon'.

--- Algebras/Polygon/PolygonAlgebra.cpp
+++ Algebras/Polygon/PolygonAlgebra.cpp
@@ -507,7 +507,7 @@ PolygonProperty()
 	                  nl->StringAtom("polygon"),
 			  nl->StringAtom("(<point>*) where <point> is "
 			  "(<x> <y>)"),
-			  nl->StringAtom("( () (3 4) (10 10) (8 2) (6 4) "
+			  nl->StringAtom("( (3 4) (10 10) (8 2) (6 4) "
 			  "(3 4) )"),
 			  nl->StringAtom("x- and y-coordinates must be of "
 			  "type int."))));
