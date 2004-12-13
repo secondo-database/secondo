e99149b8c152c632ee3fc673893148b2025831b6
spieker <>
2004-12-13 17:46:13 +0000

    Scripts and Tools for measurement of runtimes added

--- Tools/Generators/TPC-H/queries/tpc-compare.sh
+++ Tools/Generators/TPC-H/queries/tpc-compare.sh
@@ -3,14 +3,14 @@
 # Dec. 2004, M. Spiekermann
 
 if [ "$1" == "" ]; then
-  echo -e "\n  Usage: $0 <database>\n";
+  echo -e "\n  Usage: $0 <database> <query numbers>\n";
   exit 1;
 fi
 
 db="$1"
 queryDir="$HOME/secondo/Tools/Generators/TPC-H/queries"
-soDir="$HOME/secondo"
-PATH="$PATH:$soDir/bin:$soDir/Optimizer:/$soDir/CM-Scripts"
+soDir="../../../../"
+PATH="$PATH:$soDir/bin:$soDir/Optimizer:$soDir/CM-Scripts"
 so="ttyrun.sh"
 sosrv="SecondoTTYBDB"
 pg="pgrun.sh"
@@ -19,11 +19,13 @@ prof="ps-memprof.sh"
 
 # run TPC queries
 
-for nr in 3 5 10; do
+for nr in $2; do
 #for nr in 5; do
 
+  cat XLoad > /dev/null
 #  $prof $sosrv $sosrv"."$nr".prof.log" 1 & 
   $so $db "@tpc"$nr"_opt"
+  cat XLoad > /dev/null
 #  $prof $pgsrv $pgsrv"."$nr".prof.log" 1 & 
   $pg $db $nr".sql.pg"
 
