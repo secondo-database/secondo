8d8f9e4ac074d5d9b4ab3c83f97c575d684bd116
spieker <>
2005-02-04 11:30:09 +0000

    List of command aliases

---
+++ CM-Scripts/aliaslist.sh
@@ -0,0 +1,48 @@
+# list of command aliases
+#
+# Feb 2005, M. Spiekermann
+
+
+# function newAlias
+#
+# $1 alias name
+# $2 command name
+#
+# Adds path information before the command name $2
+function newAlias() {
+
+if [ -z $buildDir ]; then
+  alias ${1}=$2
+else
+  alias ${1}=${buildDir}/${cmdDir}/$2
+fi
+
+}
+###########################################################
+##
+## Start of configurable part
+##
+## Please add new shell scripts here. $scriptDir defines
+## a path relative to $SECONDO_BUILD_DIR.
+##
+###########################################################
+
+cmdDir=CM-Scripts
+newAlias backup backup.sh
+newAlias runtests run-tests.sh
+newAlias cvstest cvs-make.sh
+newAlias catvar catvar.sh
+
+cmdDir=Tools/Generators/TPC-H/secondo
+newAlias tpcgen tpcgen.sh
+newAlias tpcrun tpcrun.sh
+
+cmdDir=bin
+newAlias SecondoTTY SecondoTTYBDB
+
+
+# define some useful aliases
+alias setvar='source ${sdkDir}/bin/setvar.bash' 
+alias jcvs='java -jar ${sdkDir}/jCVS-5.2.2/jars/jcvsii.jar'
+alias cvs-info='cvs -nq update | grep "^[A-Z]"'
+
