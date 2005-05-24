9a248caa1195269480cdba7eb25e775fea0dcbb2
behr <>
2005-05-24 09:48:43 +0000

    The method copy() is not longer required in an AlgebraType class.

--- Tools/Jni/wrapper/AlgebraType.java
+++ Tools/Jni/wrapper/AlgebraType.java
@@ -25,8 +25,6 @@ import java.io.Serializable;
 public interface AlgebraType extends Serializable,Comparable{
 
    // additionally we need an constructor without any arguments
-  /** returns a depth copy of this objects */
-  Object copy();
   /** returns a hashValue depending of the value of this object */
   int getHashValue();
   /** Reads the value of this from  instance */
