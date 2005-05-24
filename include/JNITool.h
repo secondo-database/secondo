6549c9b3af9261ba9cb5fd5bc34061a669b12107
behr <>
2005-05-24 06:59:00 +0000

    Method ids now stored instead of asked each time when used.

--- include/JNITool.h
+++ include/JNITool.h
@@ -14,10 +14,13 @@ class JNITool{
       JNITool(JNIEnv *env,NestedList* nl);
       jobject GetJavaList(JNIEnv *env,ListExpr LE);
       ListExpr GetCppList(JNIEnv* env, jobject obj);
+      void PrintJavaList(jobject list);
+      void Gc();
    private:
       JNIEnv *env;
       NestedList *nl;
       jclass nlclass;
+      jclass systemclass;
       static bool initialized;
       int jsymbol_atom;
       int jstring_atom;
@@ -25,6 +28,25 @@ class JNITool{
       int jreal_atom;
       int jint_atom;
       int jno_atom;
+      jmethodID realAtomID;
+      jmethodID intAtomID;
+      jmethodID symbolAtomID;
+      jmethodID stringAtomID;
+      jmethodID textAtomID;
+      jmethodID theEmptyListID;
+      jmethodID oneElemListID;
+      jmethodID appendID;
+      jmethodID atomTypeID;
+      jmethodID symbolValueID;
+      jmethodID stringValueID;
+      jmethodID textValueID;
+      jmethodID realValueID;
+      jmethodID intValueID;
+      jmethodID isEmptyID;
+      jmethodID firstID;
+      jmethodID restID;
+      jmethodID gcID;
+      void Error(int line);
 }; 
 
 
