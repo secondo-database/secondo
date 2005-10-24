5642a56084122672854161b42263242628448657
spieker <>
2005-10-24 15:12:56 +0000

    Support for select * and select count(*) for queries using the first N operation

--- Optimizer/optimizer.pl
+++ Optimizer/optimizer.pl
@@ -30,6 +30,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //[Oe] [\"{O}]
 //[Ue] [\"{U}]
 //[**] [$**$]
+//[star] [$*$]
 //[toc] [\tableofcontents]
 //[=>] [\verb+=>+]
 //[:Section Translation] [\label{sec:translation}]
@@ -2433,7 +2434,9 @@ callLookup(Query, Query2) :-
   lookup(Query, Query2), !.
 
 starQuery(select * from _) :- assert(isStarQuery), !.
+starQuery(select * from _ first _) :- assert(isStarQuery), !.
 starQuery(select count(*) from _) :- assert(isStarQuery), !.
+starQuery(select count(*) from _ first _) :- assert(isStarQuery), !.
 starQuery(_).
 
 newQuery :- not(clearVariables), not(clearQueryRelations), 
@@ -3135,7 +3138,7 @@ names have been looked up already.
 
 */
 
-queryToPlan(Query, Stream, Cost) :-
+queryToPlan(Query, count(Stream), Cost) :-
   countQuery(Query),
   queryToStream(Query, Stream, Cost),
   !.
@@ -3154,6 +3157,7 @@ Check whether ~Query~ is a counting query.
 
 
 countQuery(select count(*) from _) :- !.
+countQuery(select count(*) from _ first _) :- !.
 
 countQuery(Query groupby _) :-
   countQuery(Query).
@@ -3237,14 +3241,17 @@ fSort(Stream, SortAttrs, sortby(Stream, AttrNames)) :-
 
 
 
-fProject(Stream, *, Stream) :- !.
+/*
+don't modify the stream if the projection is [star] 
+or count([star]).
 
-fProject(Stream, count(*), count(Stream)) :- !.
+*/
+fProject(Stream, *, Stream) :- !.
+fProject(Stream, count(*), Stream) :- !.
+
 
 fProject(Stream, Project, project(Stream, AttrNames)) :-
   attrnames(Project, AttrNames).
-
-
 
 
 extendProject([], [], []).
