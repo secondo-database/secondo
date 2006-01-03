436528a9237fc052245c9097b264e2def933a83d
dansorge <>
2006-01-03 19:10:13 +0000

    Added new error class.

---
+++ Algebras/Rose/RoseJava/RoseAlgebraError.java
@@ -0,0 +1,19 @@
+/*
+ * RoseAlgebraError.java 2006-01-03
+ *
+ * Dirk Ansorge, FernUniversitaet Hagen
+ *
+ */
+
+/**
+ * An exception of this type is thrown for any error that occurs during the execution of a ROSE algebra operation.
+ * Use the string to pass detailed information about the error.
+ */
+public class RoseAlgebraError extends RuntimeException {
+    /**
+     * Constructs a new instance of RoseAlgebraError using the argument supplied.
+     *
+     * @param s the string with description about this exception.
+     */
+    public RoseAlgebraError(String s) { super(s); }
+}//end class RoseAlgebraError
