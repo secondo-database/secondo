587562ad0b2b624b7790122d3c22abd34bf2f5c8
behr <>
2006-01-30 15:46:29 +0000

    Classpath corrected

--- Javagui/sgui.bat
+++ Javagui/sgui.bat
@@ -1,1 +1,4 @@
-java -classpath ".;lib/java_cup_v10i_runtime.jar" gui.MainWindow
+
+SET CP="lib/java_cup_v10k_runtime.jar";"lib/jl0.4.jar";".";"lib/14_os_jpedal.jar";"lib/je.jar"
+
+java  -Xmx580M  -classpath %CP% gui.MainWindow
