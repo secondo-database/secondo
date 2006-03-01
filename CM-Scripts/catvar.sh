71b59acecffd8964784b9d2b14fc13dc7fc7bd01
spieker <>
2006-03-01 09:46:39 +0000

    Now also variable J2SDK_ROOT will be shown

--- CM-Scripts/catvar.sh
+++ CM-Scripts/catvar.sh
@@ -4,7 +4,7 @@
 echo ""
 echo "Environment variables used by SECONDO:"
 echo "--------------------------------------"
-env | grep -e "\(SECONDO_\|LD_LIB\|^PATH\|INCLUDE\|BERKELEY\|PL_\|CVSROOT\)" \
+env | grep -e "\(SECONDO_\|LD_LIB\|^PATH\|INCLUDE\|BERKELEY\|PL_\|J2SDK\|CVSROOT\)" \
     | sort \
     | sed -e 's#\(.*\)=\(.*\)#\1 = "\2" \
 --#g'
