b724414b533f1d731b4ae917f4c595e44776c049
spieker <>
2006-03-10 16:24:00 +0000

    Some new functions were added

--- include/NList.h
+++ include/NList.h
@@ -22,6 +22,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 August 2005, M. Spiekermann. Initial version.
 
+January - March 2006, M. Spiekermann. New constructors and functions added. Documentation revised.
+
 */
 
 #ifndef SECONDO_NLIST_H
@@ -37,20 +39,46 @@ August 2005, M. Spiekermann. Initial version.
 
 /*
 
-The class ~NList~ can be used as replacement for the syntactical noisy
-interface defined in NestedList.h. 
+1 Overview
+   
+The class ~NList~ can be used as replacement for the syntactical ~noisy~
+interface defined in ~NestedList.h~. Currently, not all methods of the ~old~ 
+interface are supported, but may be easily added when needed. 
 
-Currently, not all methods of the ~old~ interface are supported.  It simplifes
-the check of a list structure, e.g.  n.isSymbol() will return true, if n is a
-symbol.
+This interface simplifes the check of a list structure, e.g.  member function
+~isSymbol()~ will return TRUE, if ~n~ is a symbol atom. I think this will make
+type mappings better readable since most Operations are done by
+~list.function()~ instead of ~nl->Function(list)~. For example retrieving the
+second element of the first element can be done by
+
+----    old: nl->Second(nl->First(list))
+        new: nlist.first().second()  
+----
+
+Conversion from/to type ~ListExpr~ which is used inside SECONDO whenever nested
+lists are used is done by
+
+---- #include "NList.h"
+  
+     ListExpr f(ListExpr list) 
+     {
+       Nlist nlist(list);
+       ...
+       return nlist.listExpr();
+     }  
+----
 
 */
 
 extern NestedList* nl;
 
 /*
-Some string constants for symbols reserved as keaywords in the
-queryprocessor or often used types.
+2 String Constants
+   
+   
+Type ~Symbols~ defines some string constants for symbols reserved as keywords in
+the query processor or often used types. This should be used to avoid writing
+always redundant string constants into the code.
    
 */   
 
@@ -77,7 +105,9 @@ struct Symbols {
 
 
 /*
-2 ~NListErr~ a class representing Nested-List exceptions
+3 Exception Class ~NListErr~ 
+
+A class representing nested list exceptions.
    
 */
 
@@ -103,37 +133,76 @@ class NListErr : public SecondoException {
 #define CHECK(n) {if (n>0 && length()<n) { \
         throw NListErr("Element "#n" not in list!"); } }
 
+/*
+4 Class ~NList~
+
+This is a ~wrapper~ class for values of type ~ListExpr~. 
+
+*/
+        
 class NList {
 
-  ListExpr l;
-  Cardinal len;
-  inline bool isNodeType(const int n, const NodeType t) const
-  {
-    if ( !n ) 
-      return ( /*nl->IsAtom(l) &&*/ (nl->AtomType(l) == t) );
-
-    ListExpr list = nl->Nth(n,l);
-    return ( /*nl->IsAtom(list) &&*/ (nl->AtomType(list) == t) ); 
-  }
 
  public:
-  NList(const ListExpr list) : l(list), len(nl->ListLength(list)) { 
-    /*cout << "Nlist: nl = " << (void*)nl << endl;*/ 
-  }
-  NList() : l(nl->TheEmptyList()), len(0) { 
-    /*cout << "Nlist: nl = " << (void*)nl << endl;*/ 
-  }
-  NList(const NList& rhs) : l(rhs.l), len(rhs.len) { 
-    /*cout << "Nlist(const NList& rhs)  << endl;*/ 
-  }
-  NList(const NList& e1, const NList& e2) : 
-    l( nl->TwoElemList(e1.l, e2.l) ), 
+
+/*
+2.2 Construction and Destruction
+   
+  * ~NList()~: Empty List.
+  
+  * NList(const NList\& rhs): Create a new reference to ~rhs~.
+  
+  * ~NList(const ListExpr list)~: Initialize with ~list~.
+  
+  * ~NList(const NList\& a, const NList\& b)~: Create a two elem list ~(a b)~.
+  
+  * ~NList(const NList\& a, const NList\& b, const NList\& c)~: Three elems ~(a b
+c)~.
+  
+  * ~NList(const string\& s, const bool isStr = false)~: Create a symbol or string atom.
+  
+*/  
+  
+  NList() : 
+    l(nl->Empty()), 
+    len(0) 
+  {}
+  NList(const NList& rhs) : 
+    l(rhs.l), 
+    len(rhs.len) 
+  {}
+  NList(const ListExpr list) : 
+    l(list), 
+    len(nl->ListLength(list)) 
+  {}
+  NList(const NList& a, const NList& b) : 
+    l( nl->TwoElemList(a.l, b.l) ), 
     len(2)
   {}
-  NList(const string& s) : l(nl->SymbolAtom(s)), len(1)
-  {} 
+  NList(const NList& a, const NList& b, const NList& c) : 
+    l( nl->ThreeElemList(a.l, b.l, c.l) ), 
+    len(3)
+  {}
+  NList(const string& s, const bool isStr = false)
+  {
+   
+    if (isStr)
+      l = nl->StringAtom(s);
+    else 
+      l = nl->SymbolAtom(s);
+    len = 1;
+  }
   
   ~NList() {}
+
+/*
+2.3 Structure Tests and Value Retrieval
+
+The following functions can be used to get information about the
+list stucture and to extract subexpressions or atom values.
+
+*/  
+  
   inline bool isEmpty() const { return nl->IsEmpty(l); }
 
   inline bool isEqual(const string& s) const { 
@@ -178,6 +247,11 @@ class NList {
   inline NList fifth()  const { return elem(5); }
   inline NList sixth()  const { return elem(6); }
 
+  inline Cardinal length() const
+  {
+    return len;
+  }
+
   inline NList elem(Cardinal n) const
   {
     if ( (length() < n)   ) {
@@ -191,15 +265,21 @@ class NList {
     return NList(nl->Nth(n,l)); 
   }
   
+  // for usage in type mappings 
   inline static ListExpr typeError(const string& msg)
   { 
     ErrorReporter::ReportError(msg); 
     return nl->TypeError(); 
   }
 
-  // for interchange with the old NestedList interface
+  // interchange with type ~ListExpr~ 
   inline ListExpr listExpr() const { return l; }
  
+/*
+Functions applicable for ~symbol/string/text~ atoms.
+
+*/
+  
   inline bool hasStringValue() const 
   { 
     return (isString() || isText() || isSymbol()); 
@@ -224,8 +304,18 @@ class NList {
     return result;
   } 
 
+/*
+Conversion to C++ strings or a string Atom. The latter conversion can be
+used to convert a symbol or text atom into a string atom.
+
+*/  
   inline string convertToString() const { return nl->ToString(l); } 
+  inline NList toStringAtom() const { return NList( str(), true); } 
   
+/*
+Comparison between ~NList/NList~ and ~NList/string~ instances
+
+*/  
   inline bool operator==(const NList& rhs) const 
   { 
     return nl->Equal(l,rhs.l);
@@ -243,11 +333,34 @@ class NList {
     }
   }
  
-  inline Cardinal length() const
-  {
-    return len;
-  }
 
+/*
+Construction of big lists
+
+*/
+  inline void makeHead(NList head) 
+  {
+    l = nl->OneElemList( head.l );
+    len = 1;
+  } 
+  
+  inline NList append(NList tail)
+  {
+     return NList(nl->Append(l, tail.l));
+  }
+           
+ private:
+  ListExpr l;
+  Cardinal len;
+  inline bool isNodeType(const int n, const NodeType t) const
+  {
+    if ( !n ) 
+      return ( /*nl->IsAtom(l) &&*/ (nl->AtomType(l) == t) );
+
+    ListExpr list = nl->Nth(n,l);
+    return ( /*nl->IsAtom(list) &&*/ (nl->AtomType(list) == t) ); 
+  }
+  
 }; 
 
 ostream& operator<<(ostream& os, const NList& n);
