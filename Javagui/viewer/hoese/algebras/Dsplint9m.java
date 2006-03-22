9b7b36e29ef3e880ecb3336aec48d6c848578930
behr <>
2006-03-22 15:45:09 +0000

    External display added.

--- Javagui/viewer/hoese/algebras/Dsplint9m.java
+++ Javagui/viewer/hoese/algebras/Dsplint9m.java
@@ -20,16 +20,29 @@
 
 package  viewer.hoese.algebras;
 
-import  sj.lang.ListExpr;
-import  viewer.*;
+import sj.lang.ListExpr;
+import viewer.*;
 import viewer.hoese.*;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.Font;
 
 
 /**
  * A displayclass for the int9m type 
  */
-public class Dsplint9m extends DsplGeneric implements DsplSimple{
+public class Dsplint9m extends DsplGeneric implements DsplSimple, ExternDisplay{
+  /** string which is used for wrong formatted nested lists **/
   static final String ERROR="wrong list";
+  /** a frame for external display **/
+  static ExtWin extWin = new ExtWin();
+  /** the formatted text for displaying in the external entry **/
+  private String text;
+  /** the matrix as a single line for the entry **/
+  private String entry; 
 
   /* returns the string representation for the given value list */
   private String getDisplay(ListExpr value){
@@ -71,6 +84,15 @@ public class Dsplint9m extends DsplGeneric implements DsplSimple{
            break;
        default: display = ERROR;
      }
+     if(display.length()==9){
+        text =   display.substring(0,3)+"\n"+display.substring(3,6)
+               + "\n"+display.substring(6,9);
+        text = text.replaceAll("0","0 ");
+        text = text.replaceAll("1","1 ");
+        text = text.trim();
+     } else{
+       text = display;
+     }
      return display;
 
   }
@@ -79,7 +101,8 @@ public class Dsplint9m extends DsplGeneric implements DsplSimple{
   /* Sets the entry for the queryresult.
   */
   public void init (ListExpr type, ListExpr value, QueryResult qr) {
-     qr.addEntry(new String(type.symbolValue() + ":" + getDisplay(value)));
+     entry = type.symbolValue()+" : "+getDisplay(value);
+     qr.addEntry(this);
   }
 
   public void init (ListExpr type,int typewidth,ListExpr value,int valuewidth, QueryResult qr)
@@ -88,8 +111,57 @@ public class Dsplint9m extends DsplGeneric implements DsplSimple{
      String V = getDisplay(value);
      T=extendString(T,typewidth);
      V=extendString(V,valuewidth);
-     qr.addEntry(T + " : " + V);
+     entry = T + " : " + V;
+     qr.addEntry(this);
      return;
+  }
+
+  public String toString(){
+     return entry;
+  }
+
+   /** shows this matrix in an external window **/
+   public void displayExtern(){
+       extWin.setMatrix(this);
+       extWin.setVisible(true);
+   }
+
+   public boolean isExternDisplayed(){
+      return this==extWin.int9m && extWin.isVisible();
+   }
+
+  private static class ExtWin extends JFrame{
+     private JTextArea textArea = new JTextArea(5,5);
+     private JButton   closeBtn = new JButton("close");
+     private Dsplint9m int9m = null;
+     private static java.awt.Dimension dim = new java.awt.Dimension(100,100);
+
+     /** creates a new external window **/
+     public ExtWin(){
+       super("9 intersection matrix");
+       getContentPane().setLayout(new BorderLayout());
+       setSize(100,150);
+       getContentPane().add(closeBtn,BorderLayout.SOUTH);
+       getContentPane().add(textArea,BorderLayout.CENTER);
+       closeBtn.addActionListener(new ActionListener(){
+           public void actionPerformed(ActionEvent evt){
+                ExtWin.this.setVisible(false);
+           }
+       });
+       textArea.setFont(new Font("Monospaced",Font.BOLD,24));
+       textArea.setEditable(false); 
+     }    
+
+     
+    /** sets the matrix to display **/
+    public void setMatrix(Dsplint9m matrix){
+       int9m=matrix;
+       if(matrix==null){
+           textArea.setText("");
+       } else{
+           textArea.setText(matrix.text);
+       }
+    }
 
   }
 
