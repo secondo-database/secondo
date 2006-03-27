f8bb41675d73e85b2ed03ec9246cb76412764c2a
behr <>
2006-03-27 14:32:44 +0000

    Additional classes for changing labels

---
+++ Javagui/viewer/hoese/CurrentState.java
@@ -0,0 +1,16 @@
+
+
+package viewer.hoese;
+
+/**
+This class holds the current configuration of the HoeseViewer.
+
+
+**/
+
+public class CurrentState{
+
+
+public static double ActualTime=0.0;
+
+}
---
+++ Javagui/viewer/hoese/DefaultLabelAttribute.java
@@ -0,0 +1,28 @@
+
+package viewer.hoese;
+
+/**
+ This class provides a LabelAttribute for displaying simple
+ constant values.
+**/
+
+public class DefaultLabelAttribute implements LabelAttribute{
+  /** holds the value for this label **/
+  private String text;
+
+  /** Creates a new instance from the given string **/
+  public DefaultLabelAttribute(String text){
+     this.text = text;
+  }
+
+  /** creates a new instance displaying an integer number **/
+  public DefaultLabelAttribute(int text){
+     this.text = "" + text;
+  }
+
+  /** method implementing the LabelAttribute interface **/
+  public String getLabel(double time){
+     return text;
+  } 
+
+}
