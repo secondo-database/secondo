22b57d8632f7c5e83b51ea8852a3a7bd98d06c68
duentgen <>
2006-05-24 09:46:27 +0000

    Reset optimizer options to standard presettings

--- Optimizer/calloptimizer.pl
+++ Optimizer/calloptimizer.pl
@@ -500,21 +500,21 @@ This are the optional standart settings for the optimizer when getting started.
 Feel free to change.
  
 */
-% :- setOption(entropy).          % Using entropy extension?
+ :- setOption(entropy).          % Using entropy extension?
 % :- setOption(immediatePlan).    % Don't create complete POG?
 % :- setOption(intOrders(on)).    % Consider interesting orders (on-variant)?
 % :- setOption(intOrders(quick)). % Consider interesting orders (path-variant)?
 % :- setOption(intOrders(path)).  % Consider interesting orders (path-variant)?
 % :- setOption(intOrders(test)).  % Consider interesting orders (test-variant)?
-% :- setOption(pathTiming).       % Prompt time used to create immediate plan?
+ :- setOption(pathTiming).       % Prompt time used to create immediate plan?
 % :- setOption(uniformSpeed).     % Using uniform machine speed factor?
 % :- setOption(costsConjunctive). % Applying costs only to conjunctive sub query?
 % :- setOption(dynamicSample).    % Using dynamic samples instead of static ones?
-% :- setOption(rewriteMacros).    % Using macro expansion features?
-% :- setOption(rewriteInference). % Using automatic inference of predicates?
-% :- setOption(rtreeIndexRules).  % Use additional rules to exploit R-tree indices?
-% :- setOption(rewriteNonempty).  % Handle 'nonempty' in select statements?
-% :- setOption(rewriteCSE).       % Substitute common subexpressions in queries?
+ :- setOption(rewriteMacros).    % Using macro expansion features?
+ :- setOption(rewriteInference). % Using automatic inference of predicates?
+ :- setOption(rtreeIndexRules).  % Use additional rules to exploit R-tree indices?
+ :- setOption(rewriteNonempty).  % Handle 'nonempty' in select statements?
+ :- setOption(rewriteCSE).       % Substitute common subexpressions in queries?
 % :- setOption(rewriteCSEall).    % Extend attributes for ALL CSEs?
 % :- setOption(rewriteRemove).    % Apply early removal of unused attributes?
 :- assert(optDebugLevel(selectivity)), setOption(debug). % Activating selectivity debugging code?
