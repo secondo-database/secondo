dcb26e36e3865e782c02b2b56b005d7d7074194e
behr <>
2006-05-30 11:01:00 +0000

    Now the parser differs between integer, float and atom (before only atom was created).

--- OptServer/PrologParser.java
+++ OptServer/PrologParser.java
@@ -36,147 +36,147 @@ class PrologParser{
 
        private class Scanner{
          public Scanner(String i){
-	    input = i;
-	    nextStart=0;
-	    length=input.length();
-	 }
+            input = i;
+            nextStart=0;
+            length=input.length();
+         }
 
          int getNext(){
              int state = 0;
-	     int startPos = nextStart;
-	     int pos = nextStart;
-	     char c=' ';
-	     while(pos<length){
+             int startPos = nextStart;
+             int pos = nextStart;
+             char c=' ';
+             while(pos<length){
                 c = input.charAt(pos);
-	        switch(state){
-		   case(0) :  if(isWhiteSpace(c)){
-		                  pos++;
-			       }
-			       else if(c=='\"'){
+                switch(state){
+                   case(0) :  if(isWhiteSpace(c)){
+                                  pos++;
+                               }
+                               else if(c=='\"'){
                                   startPos=pos;
-				  pos++;
-				  state=1;
-			       }else if( c=='\''){
-			          startPos=pos;
-				  pos++;
-				  state=2;
-			       }else if(c=='('){
+                                  pos++;
+                                  state=1;
+                               }else if( c=='\''){
+                                  startPos=pos;
+                                  pos++;
+                                  state=2;
+                               }else if(c=='('){
                                    nextStart=pos+1;
-				   value="(";
-				   return OPENBRACKET1;
-			       }else if(c=='['){
+                                   value="(";
+                                   return OPENBRACKET1;
+                               }else if(c=='['){
                                    nextStart=pos+1;
-				   value="[";
-				   return OPENBRACKET2;
-			       }else if(c==')'){
+                                   value="[";
+                                   return OPENBRACKET2;
+                               }else if(c==')'){
                                    nextStart=pos+1;
-				   value=")";
-				   return CLOSEBRACKET1;
-			       }else if(c==']'){
+                                   value=")";
+                                   return CLOSEBRACKET1;
+                               }else if(c==']'){
                                    nextStart=pos+1;
-				   value="]";
-				   return CLOSEBRACKET2;
-			       }else if (isUpperCase(c)){
-			          startPos=pos;
-				  state=3;
-				  pos++;
-			       }else{
-			          startPos=pos;
-				  pos++;
+                                   value="]";
+                                   return CLOSEBRACKET2;
+                               }else if (isUpperCase(c)){
+                                  startPos=pos;
+                                  state=3;
+                                  pos++;
+                               }else{
+                                  startPos=pos;
+                                  pos++;
                                   state=4;
-			       }
-			       break;
+                               }
+                               break;
 
                    case(1) :  if(c=='\"'){  // string found
                                  nextStart=pos+1;
-				 value = input.substring(startPos,pos+1);
-				 return ATOM;
-		              }else{
-			         pos++;
-			      }
-			      break;
-		   case(2) :  if(c=='\''){  // founc a complex atom
-		                 nextStart=pos+1;
-				 value = input.substring(startPos+1,pos);
-				 return ATOM;
-    		              } else{
-			        pos++;
-			      }
-			      break;
-	           case(3)  : if(isSeparator(c)){
-		                  nextStart=pos;
-				  value = input.substring(startPos,pos);
-				  return VARIABLE;
-		              }else{
-			         pos++;
-			      }
-			      break;
+                                 value = input.substring(startPos,pos+1);
+                                 return ATOM;
+                              }else{
+                                 pos++;
+                              }
+                              break;
+                   case(2) :  if(c=='\''){  // founc a complex atom
+                                 nextStart=pos+1;
+                                 value = input.substring(startPos+1,pos);
+                                 return ATOM;
+                                  } else{
+                                pos++;
+                              }
+                              break;
+                   case(3)  : if(isSeparator(c)){
+                                  nextStart=pos;
+                                  value = input.substring(startPos,pos);
+                                  return VARIABLE;
+                              }else{
+                                 pos++;
+                              }
+                              break;
                    case(4)  : if(isSeparator(c)){
-		                  nextStart=pos;
-				  value = input.substring(startPos,pos);
-				  return ATOM;
-		              }else{
-			         pos++;
-			      }
-			      break;
+                                  nextStart=pos;
+                                  value = input.substring(startPos,pos);
+                                  return ATOM;
+                              }else{
+                                 pos++;
+                              }
+                              break;
 
-		}
-	     }
-	     // end of input
-	     nextStart = length;
-	     switch(state){
-	        case(0) : value ="";
-		          return EOI;
-	        case(1) : value ="unclosed string literal";
-		          return ERROR;
-		case(2) : value ="unclosed atom";
-		          return ERROR;
-	        case(3) : value = input.substring(startPos);
-		          return VARIABLE;
-	        case(4) : value = input.substring(startPos);
-		          return ATOM;
-	     }
+                }
+             }
+             // end of input
+             nextStart = length;
+             switch(state){
+                case(0) : value ="";
+                          return EOI;
+                case(1) : value ="unclosed string literal";
+                          return ERROR;
+                case(2) : value ="unclosed atom";
+                          return ERROR;
+                case(3) : value = input.substring(startPos);
+                          return VARIABLE;
+                case(4) : value = input.substring(startPos);
+                          return ATOM;
+             }
              value = "unexpected end";
-	     return ERROR;
+             return ERROR;
 
-	 }
-
-         /** returns the next token
-	   * in the next getNext command the same token is returned
-	   * the value is updated
-	   */
-	 public int preview(){
-	    int oldStart = nextStart;
-	    int Token = getNext();
-	    nextStart = oldStart;
-	    return Token;
-	 }
-
-
-	 /** returns true if c is upper case */
-	 private boolean isUpperCase(char c){
-             return c>='A' && c <='Z';
-	 }
-
-	 /** returns true if c is a separator without meaning*/
-	 private boolean isWhiteSpace(char c){
-	    return c==' ' | c=='\n' | c=='\t'  |   c==',' | c==';' | c==':' | c=='|';
-	 }
-
-	 /** returns true if c is a character which indicates the end of a atom or a variable */
-	 private boolean isSeparator(char c){
-	   return isWhiteSpace(c) | c=='(' | c==')' | c=='[' | c==']' | c=='\"'  | c=='\'';
          }
 
-	 /** return the value of the last getNext or preview command */
-	 public  String getValue(){
-	     return value;
-	 }
+         /** returns the next token
+           * in the next getNext command the same token is returned
+           * the value is updated
+           */
+         public int preview(){
+            int oldStart = nextStart;
+            int Token = getNext();
+            nextStart = oldStart;
+            return Token;
+         }
 
-	 private String value;
-	 private int nextStart;
-	 private String input;
-	 private int length;
+
+         /** returns true if c is upper case */
+         private boolean isUpperCase(char c){
+             return c>='A' && c <='Z';
+         }
+
+         /** returns true if c is a separator without meaning*/
+         private boolean isWhiteSpace(char c){
+            return c==' ' | c=='\n' | c=='\t'  |   c==',' | c==';' | c==':' | c=='|';
+         }
+
+         /** returns true if c is a character which indicates the end of a atom or a variable */
+         private boolean isSeparator(char c){
+           return isWhiteSpace(c) | c=='(' | c==')' | c=='[' | c==']' | c=='\"'  | c=='\'';
+         }
+
+         /** return the value of the last getNext or preview command */
+         public  String getValue(){
+             return value;
+         }
+
+         private String value;
+         private int nextStart;
+         private String input;
+         private int length;
 
        private int EOI = 0; // end of input
        private int ATOM = 1;
@@ -209,7 +209,7 @@ class PrologParser{
        Token = Scan.getNext();
        if(Token != Scan.ATOM){
           ErrorMessage="prolog command must begin with an  atom";
-	  return null;
+          return null;
        }
        Atom Command = new Atom(Scan.getValue());
        int P = Scan.preview();
@@ -229,7 +229,7 @@ class PrologParser{
        P=Scan.preview();
        if(P!=Scan.EOI){
           ErrorMessage="input after end of command found";
-	  return null;
+          return null;
        }
        if(V==null)  // an error is occured
          return null;
@@ -241,60 +241,84 @@ class PrologParser{
     }
 
 
+    /** construct one of atom, float, integer, long from the string **/
+    private Term getSimpleTerm(String value){
+      try{
+        int v = Integer.parseInt(value);
+        return new jpl.Integer(v);
+      }catch(Exception e){}
+      
+      try{
+        long v = Long.parseLong(value);
+        return new jpl.Long((int)v);
+      }catch(Exception e){}
+      
+      try{
+        double v = Double.parseDouble(value);
+        return new jpl.Float(v);
+      }catch(Exception e){}
+
+      if(value.startsWith("\"") && value.endsWith("\"")){
+          return new jpl.String(value.substring(1,value.length()-2));
+      }
+      return new jpl.Atom(value);
+    }
+
     private Vector getArgumentList(Vector Variables,int EndToken){
         int T = Scan.getNext();
-	Vector result = new Vector();
+        Vector result = new Vector();
         while(T!=EndToken){ // add the next argument into the Vector
 
-	   if(T==Scan.VARIABLE){
-	     NamedVariable Var = new NamedVariable(Scan.getValue());
-	     int index = Variables.indexOf(Var);
-	     if(index<0){      // a new Variable
-	        result.add(Var.getTerm());
-		Variables.add(Var);
-	     }else{
-	        result.add(Variables.get(index));
-	     }
-	   }else if(T==Scan.ATOM){
-	       Atom A = new Atom(Scan.getValue());
+           if(T==Scan.VARIABLE){
+             NamedVariable Var = new NamedVariable(Scan.getValue());
+             int index = Variables.indexOf(Var);
+             if(index<0){      // a new Variable
+                result.add(Var.getTerm());
+                Variables.add(Var);
+             }else{
+                result.add(Variables.get(index));
+             }
+           }else if(T==Scan.ATOM){
+               String value = Scan.getValue();
                int P = Scan.preview();
-	       if(P==Scan.OPENBRACKET1){
+               if(P==Scan.OPENBRACKET1){
+                  Atom A = new Atom(Scan.getValue());
                   int dummy = Scan.getNext(); // read the open bracket
-		  Vector sublist = getArgumentList(Variables,Scan.CLOSEBRACKET1);
-		  if(sublist==null)  // error in parsing sublist
-		     return null;
-		  Term[] args = new Term[sublist.size()];
-		  for(int i=0;i<sublist.size();i++)
-		      args[i] = (Term) sublist.get(i);
-		  result.add(new Compound(A,args));
-	       } else{
-	          result.add(A);
-	       }
+                  Vector sublist = getArgumentList(Variables,Scan.CLOSEBRACKET1);
+                  if(sublist==null)  // error in parsing sublist
+                     return null;
+                  Term[] args = new Term[sublist.size()];
+                  for(int i=0;i<sublist.size();i++)
+                      args[i] = (Term) sublist.get(i);
+                  result.add(new Compound(A,args));
+               } else{
+                  result.add(getSimpleTerm(value));
+               }
 
-	   }else if(T==Scan.OPENBRACKET2){ // indicating a prolog list
+           }else if(T==Scan.OPENBRACKET2){ // indicating a prolog list
               Vector sublist = getArgumentList(Variables,Scan.CLOSEBRACKET2);
-      	      if(sublist==null)  // error in parsing sublist
-		 return null;
-     	      if(sublist.size()==0)
-	         result.add(List.NIL); // found a empty list
-	      else{
+                    if(sublist==null)  // error in parsing sublist
+                 return null;
+                   if(sublist.size()==0)
+                 result.add(List.NIL); // found a empty list
+              else{
               List L = new List((Term)sublist.get(sublist.size()-1),List.NIL); // build a list
-	      for(int i=sublist.size()-2;i>=0;i--){
-		   L = new List( (Term) sublist.get(i),L);
+              for(int i=sublist.size()-2;i>=0;i--){
+                   L = new List( (Term) sublist.get(i),L);
               }
                  result.add(L);
-	      }
-	    }  else {
-	       if(T==Scan.ERROR)
-	         ErrorMessage=Scan.getValue();
-	       else
-	         ErrorMessage="error in building argument list"+T;
-	       return null;
+              }
+            }  else {
+               if(T==Scan.ERROR)
+                 ErrorMessage=Scan.getValue();
+               else
+                 ErrorMessage="error in building argument list"+T;
+               return null;
          }
          T = Scan.getNext();
 
-	}
-	return result;
+        }
+        return result;
     }
 
 
@@ -305,16 +329,16 @@ class PrologParser{
 
       try{
          BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	 PrologParser P = new PrologParser();
-	 String line;
-	 Query Q;
-	 while(!(line=in.readLine()).equals("exit")){
-	    Q = P.parse(line,null);
-	    if(Q==null){
-	      System.out.println("error in parsing command: "+P.ErrorMessage);
-	    }else
-	       System.out.println("=> "+Q);
-	 }
+         PrologParser P = new PrologParser();
+         String line;
+         Query Q;
+         while(!(line=in.readLine()).equals("exit")){
+            Q = P.parse(line,null);
+            if(Q==null){
+              System.out.println("error in parsing command: "+P.ErrorMessage);
+            }else
+               System.out.println("=> "+Q);
+         }
 
       }
       catch(Exception e){
