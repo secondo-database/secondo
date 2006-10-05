d98a7ede2df7f114fe9d4542ed55235f6e7277c6
behr <>
2006-10-05 08:20:00 +0000

    Changed for running the opt server ion windows platforms

--- UserInterfaces/SecondoPLMode.cpp
+++ UserInterfaces/SecondoPLMode.cpp
@@ -32,6 +32,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <iostream> 
 #include "SWI-Prolog.h"
 #include <stdlib.h>
+#include "SecondoConfig.h"
 
 using namespace std;
 
