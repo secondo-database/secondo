b8b17e5dd7061e1f9fa017cbe83a055510b652a1
spieker <>
2006-10-31 17:07:38 +0000

    #include <assert.h> inserted. Since gcc 4.1.1 complains

--- Tests/base64encoder.cpp
+++ Tests/base64encoder.cpp
@@ -25,6 +25,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <fstream>
 #include <iostream>
 #include <unistd.h>
+#include <assert.h>
 #include <sstream>
 
 using namespace std;
