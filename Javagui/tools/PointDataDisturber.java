593e738866e039e0bde61cb0585dabf809ad5ad4
behr <>
2006-12-12 08:41:45 +0000

    Delay manager implemented

--- Javagui/tools/PointDataDisturber.java
+++ Javagui/tools/PointDataDisturber.java
@@ -80,8 +80,8 @@ private double removeSeqProb = 0.0001;
 /** the unitwriter */
 private UnitWriter writer;
 
-
-
+/** the delayManager **/
+private DelayManager delayManager= new DelayManager();
 
 
 /* some statistical information */
@@ -89,8 +89,6 @@ private UnitWriter writer;
 private int measures;
 /** numbe rof skipped units */
 private int skippedUnits;
-
-
 
 
 public PointDataDisturber(){
@@ -208,7 +206,12 @@ private void createData(){
  Time max = pmpoint.getMaxTime();
  int writtenUnits=0;
    while((currentTime.compareTo(max)<=0)  && (writtenUnits<maxUnits)){
-       addLocation(currentTime,pmpoint.getPosition(currentTime));
+       Time actualTime=delayManager.getTime(currentTime);
+   //    System.err.println("currentTime : " + currentTime+" actualTime "+ actualTime);
+   //    Time delay = currentTime.minus(actualTime);
+   //    System.err.println("delay is " + delay);
+
+       addLocation(currentTime,pmpoint.getPosition(actualTime));
        currentTime.addInternal(defaultDiff);
        writtenUnits++;
    }
@@ -258,6 +261,171 @@ public static void main(String[] args){
 
 
 }
+
+
+
+private static class DelayManager{
+
+
+
+private Time scheduledDelay= new Time(0,0);
+private Time lastReceivedTime;
+private Time lastSendTime;
+private double probability = 0.01; // create an event in 1% 
+
+/** maximum dela yin seconds **/
+private double maxDelay = 200;
+
+private int state = STATE_NORMAL;
+
+/** returns the current state as String **/
+public String getState(int stateCode){
+   switch(stateCode){
+      case STATE_NORMAL: return "NORMAL";
+      case STATE_STOP: return "STOP";
+      case STATE_DECELERATION: return "DECELERATION";
+      case STATE_HURRY: return "HURRY";
+      default: return "UNKNOWN";
+   }
+
+}
+
+/** prints the state to cerr **/
+private void printState(){
+  System.err.println("state "+getState(state));
+  System.err.println("lastReceivedTime " + lastReceivedTime);
+  System.err.println("lastSendTime " + lastSendTime);
+  System.err.println("scheduledDelay " + scheduledDelay.toDurationString()); 
+}
+
+
+/** checks whether origTime &lt;= sendTime **/
+private void checkSendTime(Time origTime, Time sendTime){
+   if(sendTime==null) return;
+   int cmp = origTime.compareTo(sendTime);
+   if(cmp<0){
+        System.err.println("wrong computation:  sendTime > origTime");
+        printState();
+        System.exit(1);
+   }
+}
+
+
+/** returns the delayed time for a given one */
+public Time getTime(Time origTime){
+
+   if(lastReceivedTime==null){ //first call
+        lastReceivedTime = origTime.copy();
+        lastSendTime = origTime.copy();
+        checkSendTime(origTime,lastSendTime);
+        return lastSendTime;
+   }
+   if(origTime.compareTo(lastReceivedTime)<=0){
+     System.err.println("DelayManager: wrong order of instants !!");
+     return origTime;
+   }
+
+   createEvent(); // randomized creation of an event
+
+
+   if(state==STATE_NORMAL){
+       lastSendTime = origTime.copy();
+       lastReceivedTime = origTime.copy();
+       checkSendTime(origTime,lastSendTime);
+       return origTime;
+   }
+
+   // process a stop event
+   if(state==STATE_STOP){
+      // leave the stop state
+      if(origTime.minus(lastSendTime).compareTo(scheduledDelay)>=0){
+        System.err.println("Change state to HURRY");
+        state = STATE_HURRY;
+      }      
+      lastReceivedTime = origTime.copy();
+      checkSendTime(origTime,lastSendTime);
+      return lastSendTime.copy(); 
+   }
+
+   // process a deceleration
+   if(state==STATE_DECELERATION){
+     if(origTime.minus(lastSendTime).compareTo(scheduledDelay)>=0){
+        state = STATE_HURRY;
+        System.err.println("Change state to HURRY");
+     }      
+     // compute the deceleration between 10 and 90 percent
+     double dec = 0.1+Math.random()*0.8;
+     Time origDelta = origTime.minus(lastReceivedTime);
+     Time delta = origDelta.mul(dec);
+     lastSendTime.addInternal(delta);
+     lastReceivedTime = origTime.copy();
+     checkSendTime(origTime,lastSendTime);
+     return lastSendTime.copy(); 
+   }
+
+   // process hurry 
+   if(state==STATE_HURRY){
+     // compute the acceleration between 10 and 20 percent
+     double dec = 1.1+Math.random()+0.1;
+     Time origDelta = origTime.minus(lastReceivedTime);
+     Time delta = origDelta.mul(dec);
+     lastSendTime.addInternal(delta);
+     // acceleration finished
+     if(lastSendTime.compareTo(origTime)>=0){
+         lastSendTime.equalize(origTime);
+         state = STATE_NORMAL;
+         System.err.println("Change state to NORMAL");
+     }
+     lastReceivedTime = origTime.copy();
+     checkSendTime(origTime,lastSendTime);
+     return lastSendTime.copy(); 
+   }
+   lastReceivedTime = origTime.copy();
+   System.err.println("Unknown state " + state);
+   return null;
+
+}
+
+/** creates a new exception 
+  **/
+private void createEvent(){
+   if(state!=STATE_NORMAL){ // don't create an event in othe states
+     return;
+   }
+   // cancel randomized
+   double create= Math.random();
+   if(create > probability){
+       return;
+   }   
+
+   // randomize the value of delay
+   int delay = (int)(maxDelay * Math.random() * 1000);
+   scheduledDelay = new Time(0,delay); 
+
+   // flip a coin for the kind of event
+   if(Math.random()<0.5){
+       System.err.println("create a stop of " + (delay/1000) +" seconds");
+       state = STATE_STOP;
+   } else{
+       System.err.println("create a deceleration of " + (delay/1000) +" seconds");
+       state = STATE_DECELERATION;
+   }
+}
+
+
+
+/** normal state - no delay **/
+private static final int STATE_NORMAL=0; 
+/** unscheduled stop until delay is reached **/
+private static final int STATE_STOP=1;
+/** deceleration until delay is reached **/
+private static final int STATE_DECELERATION=2;
+/** state to reach the normal position **/
+private static final int STATE_HURRY=3;
+
+
+}
+
 
 
 
