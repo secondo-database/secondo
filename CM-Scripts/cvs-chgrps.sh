5eb124d72e129b29e219b5384a9f37a1906590fb
spieker <>
2007-03-21 16:36:09 +0000

    New parameters introduced

--- CM-Scripts/cvs-chgrps.sh
+++ CM-Scripts/cvs-chgrps.sh
@@ -1,19 +1,54 @@
 #!/bin/sh
 #
-# 08.06.2006 M. Spiekermann
+# Since 08.06.2006, M. Spiekermann
 # 
 # A shell script wich updates group membership for
 # CVS repository files. This is needed since access
 # privileges could only be managed by group memberships
 
-lockRoot="/tmp/cvslock"
-cvsHome="/home/cvsroot"
+function checkDir 
+{
+  if [ ! -d $1 ]; then
+    echo "Error: $1 is not a directory!"
+  fi
+  exit 2 
+}
 
-chgrp -R cvs-dipl $lockRoot/secondo
+##
+## parse arguments
+##
+while [ $# -ne 0 ]; do
 
-chgrp -R cvs-dipl $lockRoot/students
-chgrp -R cvs-dipl $cvsHome/students
+  if [ "$1" == "-lockRoot" ]; then
+    shift
+    lockRoot="$1"
+    checkDir "$lockRoot"
+    shift
+  else if [ "$1" == "-cvsHome" ]; then
+    shift
+    cvsHome="$1"
+    checkDir "$cvsHome"
+    shift
+  else
+    echo "Unknown option ${1}!"
+    exit 1
+  fi
+  fi
 
-#chgrp -R cvs-pub /var/lock/cvs/secondo-data
-#chgrp -R cvs-pub /home/cvsroot/secondo-data
+done
+
+
+exit 3
+
+##
+## change group memberships
+##
+
+chgrp -fR cvs-dipl $lockRoot/secondo
+
+chgrp -fR cvs-dipl $lockRoot/students
+chgrp -fR cvs-dipl $cvsHome/students
+
+#chgrp -R cvs-pub $lockRoot/secondo-data
+#chgrp -R cvs-pub $cvsHome/secondo-data
 
