3078e0e9421269f4c90ca096263d3f395f0b3e3d
spieker <>
2007-05-11 11:24:57 +0000

    Send an alert mail if disk space runs out

---
+++ CM-Scripts/df-alert.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+#
+# May 2007, M. Spiekermann
+#
+# Send an email alert if disk capacity runs out 
+#
+
+
+# Include function definitions of libutil.sh. It must be in the same direcory
+# as this script file or in the search path. 
+
+baseDir=$HOME/${0%/*}
+if [ -s $baseDir/libutil.sh ]; 
+then
+  if ! source $baseDir/libutil.sh; then exit 1; fi
+else
+  if ! source libutil.sh; then exit 1; fi
+fi
+
+if [ $? -ne  0 ]; then 
+  printf "%s\n" "This script needs routines from the file $libFile"
+  exit 1; 
+fi
+
+if [ $# != 2 ]; then
+  echo "Error: please specify device and threshold value, e.g. /dev/sda1 90"
+  exit 1
+fi
+
+device="$1"
+declare -i treshold=$2
+
+
+function dfUsage()
+{
+  local device="$1"
+  local -i usage=$(df -H /dev/sda1 | sed -ne '2s#\(.*\) \([0-9]*\)%\(.*\)#\2#p')
+
+  return $usage
+}
+
+dfUsage $device 
+declare -i usage=$?
+
+if (( $usage > $treshold )); then
+
+LU_SENDMAIL_FROM="spieker@bassi"
+LU_SENDMAIL="true"
+XmailBody="This is a generated message!  
+ 
+  Host      : $HOST
+  Device    : $device 
+  Fill rate : ${usage}%
+"
+
+  sendMail "disk alert! on $HOST" "spieker@bassi" "$XmailBody"
+fi
