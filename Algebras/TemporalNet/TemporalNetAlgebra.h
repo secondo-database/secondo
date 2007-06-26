5df687acd3d283b2560e1fd727cc4659a6326021
scheppokat <>
2007-06-26 15:28:45 +0000

    First version of temporal-net Fixed copy of network algebra

--- Algebras/TemporalNet/TemporalNetAlgebra.h
+++ Algebras/TemporalNet/TemporalNetAlgebra.h
@@ -38,7 +38,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "NestedList.h"
 #include "QueryProcessor.h"
 #include "TemporalAlgebra.h"
-#include "Network2Algebra.h"
+#include "../Network/NetworkAlgebra.h"
 
 using namespace datetime;
 
