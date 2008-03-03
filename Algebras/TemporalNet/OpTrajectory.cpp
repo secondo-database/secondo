b5cf2b9825daf3c60bad0dd9c4633594d8c59f1a
jandt <>
2008-03-03 09:01:20 +0000

    TemporalNetAlgebra OpTrajectory changed to optimize resulting gline. Simone

--- Algebras/TemporalNet/OpTrajectory.cpp
+++ Algebras/TemporalNet/OpTrajectory.cpp
@@ -1,7 +1,7 @@
 /*
 This file is part of SECONDO.
 
-Copyright (C) 2004, University in Hagen, Department of Computer Science, 
+Copyright (C) 2004, University in Hagen, Department of Computer Science,
 Database Systems for New Applications.
 
 SECONDO is free software; you can redistribute it and/or modify
@@ -24,9 +24,12 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 1.1 Implementation of Operator Trajectory
 
-The operator finds the trajectory of a moving points 
+The operator finds the trajectory of a moving points
 
 Oktober 2007 Martin Scheppokat
+
+March 2008 Simone Jandt ValueMapping added to reduce the number of parts of the
+resulting gline.
 
 Defines, includes, and constants
 
@@ -50,6 +53,93 @@ Defines, includes, and constants
 
 #include "OpTrajectory.h"
 
+
+struct RITree {
+
+  RITree(){};
+
+  RITree( int ri,double pos1, double pos2, RITree *left = 0, RITree *right = 0){
+    m_iRouteId = ri;
+    m_dStart = pos1;
+    m_dEnd = pos2;
+    m_left = left;
+    m_right = right;
+  };
+
+  void insert (int rid, double pos1, double pos2) {
+    if (rid < this->m_iRouteId) {
+      if (this->m_left != 0) {
+        this->m_left->insert(rid, pos1, pos2);
+      } else {
+        this->m_left = new RITree(rid, pos1, pos2,0,0);
+      }
+    } else {
+      if (rid > this->m_iRouteId) {
+        if (this->m_right != 0) {
+          this->m_right->insert(rid, pos1, pos2);
+        } else {
+          this->m_right = new RITree(rid, pos1, pos2,0,0);
+        }
+      }else{
+        if(rid == this->m_iRouteId) {
+          if (pos2 < this->m_dStart) {
+            if (this->m_left != 0) {
+               this->m_left->insert(rid, pos1, pos2);
+            } else {
+                this->m_left = new RITree(rid, pos1, pos2,0,0);
+            }
+          } else {
+            if (pos1 > this->m_dEnd) {
+              if (this->m_right != 0) {
+                this->m_right->insert(rid, pos1, pos2);
+              } else {
+                this->m_right =
+                    new RITree(rid, pos1, pos2,0,0);
+              }
+            } else {
+              if (this->m_dStart > pos1) {
+                this->m_dStart = pos1;
+              }
+              if (this->m_dEnd < pos2) {
+                this->m_dEnd = pos2;
+              }
+            }
+          }
+        } // endif rid=rid
+      }
+    }
+  };
+
+  void treeToGLine (GLine *gline) {
+    if (this->m_left != 0) {
+      this->m_left->treeToGLine (gline);
+    }
+    gline->AddRouteInterval(this->m_iRouteId, this->m_dStart, this->m_dEnd);
+    if (this->m_right != 0) {
+      this->m_right->treeToGLine (gline);
+    }
+  };
+
+  int m_iRouteId;
+  double m_dStart, m_dEnd;
+  RITree *m_left, *m_right;
+
+};
+
+/*
+Check if StartPos <= EndPos if not change StartPos and EndPos.
+
+*/
+void chkStartEnd(double &StartPos, double &EndPos){
+  double help;
+  if (StartPos > EndPos) {
+    help = StartPos;
+    StartPos = EndPos;
+    EndPos = help;
+  }
+};
+
+
 /*
 Typemap function of the operator
 
@@ -67,7 +157,7 @@ ListExpr OpTrajectory::TypeMap(ListExpr in_xArgs)
   {
     return (nl->SymbolAtom( "typeerror" ));
   }
-  
+
   return nl->SymbolAtom( "gline" );
 }
 
@@ -75,34 +165,64 @@ ListExpr OpTrajectory::TypeMap(ListExpr in_xArgs)
 Value mapping function of operator ~trajectory~
 
 */
-int OpTrajectory::ValueMapping(Word* args, 
-                                   Word& result, 
-                                   int message,  
-                                   Word& local, 
+int OpTrajectory::ValueMapping(Word* args,
+                                   Word& result,
+                                   int message,
+                                   Word& local,
                                    Supplier in_xSupplier)
 {
   // Get (empty) return value
   GLine* pGLine = (GLine*)qp->ResultStorage(in_xSupplier).addr;
-  result = SetWord( pGLine); 
-
+  result = SetWord( pGLine);
   // Get input values
-  MGPoint* pMGPoint = (MGPoint*)args[0].addr;  
-
-
-    
-  for (int i = 0; i < pMGPoint->GetNoComponents(); i++) 
+  MGPoint* pMGPoint = (MGPoint*)args[0].addr;
+  if(pMGPoint == NULL || pMGPoint->GetNoComponents() < 1 ||
+      !pMGPoint->IsDefined()) {
+    cerr << "MGPoint does not exist." << endl;
+    pGLine->SetDefined(false);
+    return 0;
+  }
+  const UGPoint *pCurrentUnit;
+  pMGPoint->Get(0, pCurrentUnit);
+  pGLine->SetNetworkId(pCurrentUnit->p0.GetNetworkId());
+  pGLine->SetDefined(true);
+  int aktRouteId = pCurrentUnit->p0.GetRouteId();
+  double aktStartPos = pCurrentUnit->p0.GetPosition();
+  double aktEndPos = pCurrentUnit->p1.GetPosition();
+  chkStartEnd(aktStartPos, aktEndPos);
+  double curStartPos, curEndPos;
+  RITree *tree = new RITree(aktRouteId, aktStartPos, aktEndPos,0,0);
+  int curRoute;
+  for (int i = 1; i < pMGPoint->GetNoComponents(); i++)
   {
     // Get start and end of current unit
-    const UGPoint *pCurrentUnit;
     pMGPoint->Get(i, pCurrentUnit);
-
-    pGLine->SetNetworkId(pCurrentUnit->p0.GetNetworkId());
-    pGLine->SetDefined(true);
-    pGLine->AddRouteInterval(pCurrentUnit->p0.GetRouteId(), 
-                             pCurrentUnit->p0.GetPosition(), 
-                             pCurrentUnit->p1.GetPosition());  
+    curRoute = pCurrentUnit->p0.GetRouteId();
+    curStartPos = pCurrentUnit->p0.GetPosition();
+    curEndPos = pCurrentUnit->p1.GetPosition();
+    chkStartEnd(curStartPos, curEndPos);
+    if (curRoute != aktRouteId) {
+      tree->insert(aktRouteId, aktStartPos, aktEndPos);
+      aktRouteId = curRoute;
+      aktStartPos = curStartPos;
+      aktEndPos = curEndPos;
+    } else { // curRoute == aktRouteId concat pieces if possible
+      if (AlmostEqual(aktStartPos,curEndPos)) {
+        aktStartPos = curStartPos;
+      } else {
+        if (AlmostEqual(aktEndPos,curStartPos)) {
+          aktEndPos = curEndPos;
+        } else { //concat impossible start new routeInterval for gline.
+          tree->insert(aktRouteId, aktStartPos, aktEndPos);
+          aktRouteId = curRoute;
+          aktStartPos = curStartPos;
+          aktEndPos = curEndPos;
+        }
+      }
+    }
   }
-
+  tree->insert(aktRouteId, aktStartPos, aktEndPos);
+  tree->treeToGLine(pGLine);
   return 0;
 }
 
@@ -111,7 +231,7 @@ int OpTrajectory::ValueMapping(Word* args,
 Specification of operator ~trajectory~
 
 */
-const string OpTrajectory::Spec  = 
+const string OpTrajectory::Spec  =
   "( ( \"Signature\" \"Syntax\" \"Meaning\" \"Example\" ) "
   "( <text>mgpoint -> gline" "</text--->"
   "<text>trajectory(mgpoint)</text--->"
