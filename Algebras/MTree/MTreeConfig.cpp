dfc0f6c6d4e6d23f37c76e11feaa15f49c9eb83a
mdibbert <>
2008-05-26 03:02:46 +0000

    several minor changes, removed some configs, added new configs 'limit20e', 'limit40e' and 'limit80e', which use the default split policy and limit the maximum number of entries per node to 20, 40 or 80.

--- Algebras/MTree/MTreeConfig.cpp
+++ Algebras/MTree/MTreeConfig.cpp
@@ -1,6 +1,4 @@
 /*
-\newpage
-
 ----
 This file is part of SECONDO.
 
@@ -28,14 +26,11 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //characters      [3]   capital:    [\textsc{]  [}]
 //characters      [4]   teletype:   [\texttt{]  [}]
 
-January-February 2008, Mirko Dibbert
-
 1 Implementation file "MTreeConfig.cpp"[4]
 
-This file implements the "MTreeConfig"[4] class.
+January-May 2008, Mirko Dibbert
 
 */
-#include <limits>
 #include "MTreeConfig.h"
 
 using namespace mtreeAlgebra;
@@ -46,15 +41,13 @@ Initialize static members :
 */
 bool MTreeConfigReg::initialized = false;
 map<string, MTreeConfig> MTreeConfigReg::configs;
-string MTreeConfigReg::defaultConfigName = "undef";
 
 
 /*
 Method ~getConfig~ :
 
 */
-MTreeConfig
-MTreeConfigReg::getConfig(const string& name)
+MTreeConfig MTreeConfigReg::getConfig(const string &name)
 {
   if (!initialized)
     initialize();
@@ -72,8 +65,7 @@ MTreeConfigReg::getConfig(const string& name)
 Method ~isDefined~ :
 
 */
-bool
-MTreeConfigReg::isDefined(const string& name)
+bool MTreeConfigReg::isDefined(const string &name)
 {
   if (!initialized)
     initialize();
@@ -88,45 +80,70 @@ MTreeConfigReg::isDefined(const string& name)
 }
 
 /*
+Method ~definedNames~ :
+
+*/
+string MTreeConfigReg::definedNames()
+{
+    if(!initialized)
+        initialize();
+
+    ostringstream result;
+    map<string, MTreeConfig>::iterator iter = configs.begin();
+    result << "\"" << CONFIG_DEFAULT << "\" ";
+    while (iter != configs.end())
+    {
+        if (iter->first != CONFIG_DEFAULT)
+            result << "\"" << iter->first << "\" ";
+        ++iter;
+    }
+    return result.str();
+}
+
+/*
 Method ~initialize~ :
 
 */
 void
 MTreeConfigReg::initialize()
 {
-/*
-Create node configs
-
-*/
   NodeConfig defaultleafConfig(
-        Leaf, leafPrio, minLeafEntries, maxLeafEntries,
-        minLeafPages, maxLeafPages, leafCacheable);
+        LEAF, leafPrio, minLeafEntries, maxLeafEntries,
+        maxLeafPages, leafCacheable);
 
   NodeConfig defaultinternalConfig(
-        Internal, internalPrio, minIntEntries, maxIntEntries,
-        minIntPages, maxIntPages, internalCacheable);
+        INTERNAL, internalPrio, minIntEntries, maxIntEntries,
+        maxIntPages, internalCacheable);
+
+  NodeConfig leafConfig20(
+        LEAF, leafPrio, minLeafEntries, 20,
+        maxLeafPages, leafCacheable);
+
+  NodeConfig internalConfig20(
+        INTERNAL, internalPrio, minIntEntries, 20,
+        maxIntPages, internalCacheable);
 
   NodeConfig leafConfig40(
-        Leaf, leafPrio, minLeafEntries, 40,
-        minLeafPages, maxLeafPages, leafCacheable);
+        LEAF, leafPrio, minLeafEntries, 40,
+        maxLeafPages, leafCacheable);
 
   NodeConfig internalConfig40(
-        Internal, internalPrio, minIntEntries, 40,
-        minIntPages, maxIntPages, internalCacheable);
+        INTERNAL, internalPrio, minIntEntries, 40,
+        maxIntPages, internalCacheable);
 
   NodeConfig leafConfig80(
-        Leaf, leafPrio, minLeafEntries, 80,
-        minLeafPages, maxLeafPages, leafCacheable);
+        LEAF, leafPrio, minLeafEntries, 80,
+        maxLeafPages, leafCacheable);
 
   NodeConfig internalConfig80(
-        Internal, internalPrio, minIntEntries, 80,
-        minIntPages, maxIntPages, internalCacheable);
+        INTERNAL, internalPrio, minIntEntries, 80,
+        maxIntPages, internalCacheable);
 
 /*
 Set default config
 
 */
-  defaultConfigName = "mlb_HP";
+  configs[CONFIG_DEFAULT] =  MTreeConfig();
 
 /*
 Add config objects with unlimited entries per node.
@@ -165,76 +182,20 @@ Add config objects with unlimited entries per node.
       M_LB_DIST, GENERALIZED_HYPERPLANE);
 
 /*
-Add config objects with max. 80 entries per node.
+Add config objects with limited entries per node (default splitpol)
 
 */
-  configs["random_Bal80"] =  MTreeConfig(
-      leafConfig80, internalConfig80,
-      RANDOM, BALANCED);
+  configs["limit20e"] =  MTreeConfig(
+      leafConfig20, internalConfig20,
+      defaultPromoteFun, defaultPartitionFun);
 
-  configs["mRad_Bal80"] =  MTreeConfig(
-      leafConfig80, internalConfig80,
-      m_RAD, BALANCED);
-
-  configs["mMRad_Bal80"] =  MTreeConfig(
-      leafConfig80, internalConfig80,
-      mM_RAD, BALANCED);
-
-  configs["mlb_Bal80"] =  MTreeConfig(
-      leafConfig80, internalConfig80,
-      M_LB_DIST, BALANCED);
-
-  configs["random_HP80"] =  MTreeConfig(
-      leafConfig80, internalConfig80,
-      RANDOM, GENERALIZED_HYPERPLANE);
-
-  configs["mRad_HP80"] =  MTreeConfig(
-      leafConfig80, internalConfig80,
-      m_RAD, GENERALIZED_HYPERPLANE);
-
-  configs["mMRad_HP80"] =  MTreeConfig(
-      leafConfig80, internalConfig80,
-      mM_RAD, GENERALIZED_HYPERPLANE);
-
-  configs["mlb_HP80"] =  MTreeConfig(
-      leafConfig80, internalConfig80,
-      M_LB_DIST, GENERALIZED_HYPERPLANE);
-
-/*
-Add config objects with max. 40 entries per node.
-
-*/
-  configs["random_Bal40"] =  MTreeConfig(
+  configs["limit40e"] =  MTreeConfig(
       leafConfig40, internalConfig40,
-      RANDOM, BALANCED);
+      defaultPromoteFun, defaultPartitionFun);
 
-  configs["mRad_Bal40"] =  MTreeConfig(
-      leafConfig40, internalConfig40,
-      m_RAD, BALANCED);
-
-  configs["mMRad_Bal40"] =  MTreeConfig(
-      leafConfig40, internalConfig40,
-      mM_RAD, BALANCED);
-
-  configs["mlb_Bal40"] =  MTreeConfig(
-      leafConfig40, internalConfig40,
-      M_LB_DIST, BALANCED);
-
-  configs["random_HP40"] =  MTreeConfig(
-      leafConfig40, internalConfig40,
-      RANDOM, GENERALIZED_HYPERPLANE);
-
-  configs["mRad_HP40"] =  MTreeConfig(
-      leafConfig40, internalConfig40,
-      m_RAD, GENERALIZED_HYPERPLANE);
-
-  configs["mMRad_HP40"] =  MTreeConfig(
-      leafConfig40, internalConfig40,
-      mM_RAD, GENERALIZED_HYPERPLANE);
-
-  configs["mlb_HP40"] =  MTreeConfig(
-      leafConfig40, internalConfig40,
-      M_LB_DIST, GENERALIZED_HYPERPLANE);
+  configs["limit80e"] =  MTreeConfig(
+      leafConfig80, internalConfig80,
+      defaultPromoteFun, defaultPartitionFun);
 
   initialized = true;
 }
