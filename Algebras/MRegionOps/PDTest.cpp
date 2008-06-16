9ed936b5879475bacb16d582719adcc24cb9b0c7
mhoeger <>
2008-06-16 13:40:17 +0000

    test

--- Algebras/MRegionOps/PDTest.cpp
+++ Algebras/MRegionOps/PDTest.cpp
@@ -16,6 +16,88 @@ April 2008, initial version created by M. H[oe]ger for bachelor thesis.
 
 */
 
+#include "SetOpUtil.h"
+
+bool Point3DExt::Lower(const Point3DExt& p) const {
+
+    if (NumericUtil::Lower(GetX(), p.GetX()))
+        return true;
+    else if (NumericUtil::NearlyEqual(GetX(), p.GetX())) {
+        if (NumericUtil::Lower(GetY(), p.GetY()))
+            return true;
+        else if (NumericUtil::NearlyEqual(GetY(), p.GetY())) {
+            if (NumericUtil::Lower(GetT(), p.GetT()))
+                return true;
+            else if (NumericUtil::NearlyEqual(GetT(), p.GetT())) {
+                if (sourceFlag < p.sourceFlag)
+                    return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+pair<IntersectionSegment*, IntersectionSegment*> 
+PointExtSet::GetIntersectionSegment() const {
+
+    pair<IntersectionSegment*, IntersectionSegment*>
+            noIntersectionSegment(0, 0);
+
+    if (s.size() != 4)
+        return noIntersectionSegment;
+
+    set<Point3DExt>::iterator it = s.begin();
+
+    Point3DExt p1 = (Point3DExt)*it;
+    it++;
+    Point3DExt p2 = (Point3DExt)*it;
+
+    if (p1.sourceFlag == p2.sourceFlag)
+        return noIntersectionSegment;
+
+    it++;
+    Point3DExt p3 = (Point3DExt)*it;
+
+    if (NumericUtil::NearlyEqual(p2.GetX(), p3.GetX())
+            && NumericUtil::NearlyEqual(p2.GetY(), p3.GetY())
+            && NumericUtil::NearlyEqual(p2.GetT(), p3.GetT())) {
+
+        // The length of the intersection segment is zero.
+        return noIntersectionSegment;
+    }
+
+    return IntersectionSegment::createBuddyPair(p2, p3);
+}
+
+bool Segment2D::IsAbove(const Point2D& p) const {
+
+    // TODO:
+    
+}
+
+PUnit::PUnit(const bool _isUnitA, 
+             const URegionEmb* const _uRegion,
+             const DBArray<MSegmentData>* _array, 
+             PUnitPair* const _parent) :
+                 
+            isUnitA(_isUnitA),
+            uRegion(_uRegion), 
+            array(_array),
+            parent(_parent), 
+            startTime(_uRegion->timeInterval.start.ToDouble()),
+            endTime(_uRegion->timeInterval.end.ToDouble()),
+            medianRegion(0) {
+
+    ComputeBoundingRect();
+    hasMedianRegion = false;
+    medianTime = GetTimeInterval().start + 
+     ((GetTimeInterval().end - GetTimeInterval().start) * 0.5);
+    
+    cout << "medianTime = " << medianTime.ToDouble() << endl;
+}
+
+
 void PUnit::ComputeBoundingRect() {
 
     // Calculate the projection bounding rectangle in the (x, y)-plane
