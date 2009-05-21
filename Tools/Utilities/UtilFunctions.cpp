b8be9c4dda916225ead3fe4c1eb0fd62cf60b908
xu <>
2009-05-21 16:42:25 +0000

    change the calculating method of CPU time so that it will not overflow and not generate minus number

--- Tools/Utilities/UtilFunctions.cpp
+++ Tools/Utilities/UtilFunctions.cpp
@@ -1,5 +1,5 @@
 /*
----- 
+----
 This file is part of SECONDO.
 
 Copyright (C) 2002-2007, University in Hagen, Faculty of Mathematics
@@ -20,7 +20,7 @@ along with SECONDO; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ----
 
-Implementention of some useful helper funtions. 
+Implementention of some useful helper funtions.
 
 November 2002 M. Spiekermann, Implementation of TimeTest.
 
@@ -28,7 +28,7 @@ September 2003 M. Spiekermann, Implementation of LogMsg/RTFlags.
 
 December 2003 M. Spiekermann, Implementation of class Counter.
 
-July 2004 M. Spiekermann, Implementation of showActiveFlags. 
+July 2004 M. Spiekermann, Implementation of showActiveFlags.
 
 */
 
@@ -71,8 +71,9 @@ StopWatch::start() {
   //cout << startReal.tv_sec << endl;
 #else
   time(&startReal);
-#endif 
-  startCPU=clock();
+#endif
+//  startCPU=clock();
+  times(&startCPU);
   stopCPU=startCPU;
 }
 
@@ -95,22 +96,25 @@ StopWatch::diffSecondsReal() {
 
 const double
 StopWatch::diffSecondsCPU() {
-
-  stopCPU = clock();
-  return ((double) (stopCPU - startCPU)) / CLOCKS_PER_SEC;
+//      stopCPU = clock();
+//      return ((double) (stopCPU - startCPU)) / CLOCKS_PER_SEC;
+     times(&stopCPU);
+    double click = (double)sysconf(_SC_CLK_TCK);
+    return (double)((stopCPU.tms_utime-startCPU.tms_utime)/click +
+                    (stopCPU.tms_stime-startCPU.tms_stime)/click);
 }
 
 
 const string
 StopWatch::minutesAndSeconds(const double seconds) {
-  
+
   static char sbuf[20+1];
   static double frac = 0, sec = 0, min = 0;
-  
+
   frac = modf(seconds/60, &min);
   sec = seconds - (60 * min);
   sprintf(&sbuf[0], "%.0f:%02.0f", min, sec);
-  
+
   return string((const char*) sbuf);
 }
 
@@ -119,17 +123,17 @@ const string
 StopWatch::timeStr(const time_t& inTime /* = 0*/) {
 
   static char sbuf[20+1];
-  
+
   time_t usedTime = 0;
   if ( inTime == 0 ) {
     time(&usedTime);
   } else {
     usedTime = inTime;
   }
-  
+
   const tm *ltime = localtime(&usedTime);
   strftime(&sbuf[0], 20, "%H:%M:%S", ltime);
-  
+
   return string((const char*) sbuf);
 }
 
@@ -138,10 +142,10 @@ const string
 StopWatch::diffReal() {
 
   ostringstream buffer;
-  
-  buffer << "Elapsed time: " << diffSecondsReal() << " seconds."; 
-  
-  return buffer.str(); 
+
+  buffer << "Elapsed time: " << diffSecondsReal() << " seconds.";
+
+  return buffer.str();
 }
 
 
@@ -149,10 +153,10 @@ const string
 StopWatch::diffCPU() {
 
    ostringstream buffer;
-      
-   buffer << "Used CPU time: " << diffSecondsCPU() << " seconds."; 
-   
-   return buffer.str();   
+
+   buffer << "Used CPU time: " << diffSecondsCPU() << " seconds.";
+
+   return buffer.str();
 }
 
 
@@ -162,24 +166,24 @@ StopWatch::diffTimes() {
    ostringstream buffer;
    double sReal = diffSecondsReal();
    double sCPU = diffSecondsCPU();
-   
+
    buffer << "Times (elapsed / cpu): ";
 
-   if (sReal > 60.0) 
+   if (sReal > 60.0)
    {
      buffer << minutesAndSeconds(sReal) << "min ";
-     buffer << "(" << sReal << "sec) /";  
+     buffer << "(" << sReal << "sec) /";
    }
    else
    {
-     buffer << sReal << "sec / ";  
+     buffer << sReal << "sec / ";
    }
 
    buffer << sCPU << "sec = " << sReal/sCPU;
-   //buffer << " / ticks = " << (stopCPU - startCPU) 
+   //buffer << " / ticks = " << (stopCPU - startCPU)
    //<< "[" << stopCPU << " - " << startCPU << "]";
-	  
-   return buffer.str();  
+
+   return buffer.str();
 }
 
 
@@ -210,25 +214,25 @@ void
 RTFlag::initByString( const string &keyList ) {
 
    //  The string contains a comma separated list of
-   //  keywords which is inserted into a map. 
+   //  keywords which is inserted into a map.
 
    const char* sep = ",";
    int n = keyList.length();
-   
+
    if (n == 0) {
      return;
-   }  
+   }
 
    char* pbuf = new char[n+1];
    keyList.copy(pbuf,n);
    pbuf[n] = 0;
 
-   char* pkey = 0;  
+   char* pkey = 0;
    pkey=strtok(pbuf,sep);
 
    string key = string(pkey);
    flagMap[key] = true;
-    
+
    while ( (pkey=strtok(0,sep)) != NULL ) {
 
      key = string(pkey);
@@ -244,27 +248,27 @@ RTFlag::initByString( const string &keyList ) {
    delete [] pbuf;
 }
 
-bool 
-RTFlag::isActive( const string& key ) { 
-    
-  if ( (it=flagMap.find( key )) != flagMap.end() ) { 
+bool
+RTFlag::isActive( const string& key ) {
 
-    return it->second;  
-  } 
-  else { 
+  if ( (it=flagMap.find( key )) != flagMap.end() ) {
 
-    return false; 
+    return it->second;
+  }
+  else {
+
+    return false;
   }
 }
 
-void 
+void
 RTFlag::setFlag( const string& key, const bool value ) {
 
-  if ( (it=flagMap.find( key )) != flagMap.end() ) { 
+  if ( (it=flagMap.find( key )) != flagMap.end() ) {
 
     it->second = value;
-  } 
-  else { 
+  }
+  else {
 
     flagMap[key] = value;
     cerr << "New Flag added!" << endl;
@@ -298,11 +302,11 @@ ostream& operator << (ostream& os, const tab& f) {
 
 
 
-ostream& 
-tab::operator() (ostream& os) const 
+ostream&
+tab::operator() (ostream& os) const
 {
   for (int i=n; i!=0; i--) os.put(c);
-  return os; 
+  return os;
 }
 
 // The next operator is implemented in file "UtilFunctions.cpp"
@@ -313,21 +317,21 @@ ostream& operator << (ostream& os, const tab& f);
 The class below can be used as manipulator in streams. If the
 runtime flag "CMSG:Color" is set $os << color(red)$ will return
 a escape sequence which switches to color red on "some" terminals.
-Without the flag an empty string will be returned. 
+Without the flag an empty string will be returned.
 
 */
 
-void 
+void
 color::useColors(const bool value) {
 
-    if ( value ) 
+    if ( value )
     {
       n = 4; // must be changed when new colors are added
       col.resize(n);
-      col[normal % n] = "\033[0m"; 
+      col[normal % n] = "\033[0m";
       col[red % n] = "\033[31m";
       col[green % n] = "\033[32m";
-      col[blue % n] = "\033[34m"; 
+      col[blue % n] = "\033[34m";
     }
     else
     {
@@ -337,13 +341,13 @@ color::useColors(const bool value) {
         *it = "";
         it++;
       }
-    } 
+    }
   }
 
-ostream& color::operator() (ostream& os) const 
-{ 
-    os << col[c % n]; 
-    return os; 
+ostream& color::operator() (ostream& os) const
+{
+    os << col[c % n];
+    return os;
 }
 
 vector<string> color::col;
@@ -360,16 +364,16 @@ characters or if it is empty.
 
 */
 
-bool 
+bool
 isSpaceStr(const string& s)
 {
-  return s.find_first_not_of(" \t\v\n\r") == string::npos; 
+  return s.find_first_not_of(" \t\v\n\r") == string::npos;
 }
 
 size_t
 firstNonSpace(const string& s)
 {
-  return s.find_first_not_of(" \t\v\n\r"); 
+  return s.find_first_not_of(" \t\v\n\r");
 }
 
 string removeNewLines(const string& s)
@@ -385,11 +389,11 @@ string removeNewLines(const string& s)
     p2 = result.find_first_of('\n',p1);
 
     if (p2 != string::npos)
-      result.erase(p2,1);    
+      result.erase(p2,1);
 
     p1 = p2;
-  }  
-  return result;  
+  }
+  return result;
 }
 
 
@@ -405,16 +409,16 @@ string expandTabs(const string& s, const int n)
   {
     p2 = result.find_first_of('\t',p1);
 
-    if ( p2 != string::npos ) 
+    if ( p2 != string::npos )
     {
       result.erase(p2, 1);
       result.insert(p2, string(n,' '));
-    }  
+    }
 
     p1 = p2;
-  }  
-  return result;  
-} 
+  }
+  return result;
+}
 
 string translate(const string& s,  const string& from, const string& to)
 {
@@ -428,19 +432,19 @@ string translate(const string& s,  const string& from, const string& to)
   {
     p2 = result.find_first_of(from,p1);
 
-    if ( p2 != string::npos ) 
+    if ( p2 != string::npos )
     {
       result.replace(p2, 1, to);
       p1 = p2+1;
-    } 
-    else 
+    }
+    else
     {
       p1 = end;
     }
     //cout << p1 << endl;
-  }  
-  return result;  
-} 
+  }
+  return result;
+}
 
 string hexStr(const string& s)
 {
@@ -451,12 +455,12 @@ string hexStr(const string& s)
 
   while (p < end)
   {
-    res << hex << static_cast<int>( s[p] ) << " "; 
+    res << hex << static_cast<int>( s[p] ) << " ";
     p++;
     //cout << p << endl;
-  }  
-  return res.str();  
-} 
+  }
+  return res.str();
+}
 
 
 
@@ -470,7 +474,7 @@ Examples can be found in file "Tests/tcharutils.cpp".
 */
 
 string
-wordWrap( const int indent1, const int indent2, 
+wordWrap( const int indent1, const int indent2,
           const int textwidth, const string& s )
 {
   const bool trace = false;
@@ -486,23 +490,23 @@ wordWrap( const int indent1, const int indent2,
   // usable width for the first line
   size_t len = textwidth - indent1;
   int lines = 1;
-  
+
   string result="";
   size_t end = text.size();
   size_t p1 = 0;
   size_t lastbreak = 0;
-  
+
   while ( p1 < end )
   {
     if (trace)
       cout << "text[" << p1 << "]='" << text.substr(p1,20) << "...'" << endl;
-    
+
     if (lines > 1) {
       len = textwidth - indent2;
       indentStr = indent2Str;
       // do a line break
       result += "\n";
-    }  
+    }
 
     string substr="";
     // search a suitable wrap position
@@ -510,94 +514,94 @@ wordWrap( const int indent1, const int indent2,
     bool newLine = true;
     size_t p2 = end;
     size_t endPos = min(p1+len, end);
-   
+
     while ( !found /*&& (endPos >= (p1+(2*len/3)))*/ )
-    { 
+    {
       // check for a predefined linebreak
       p2 = text.find_first_of('\n', p1);
-      if ( p2 <= endPos ) 
-      { 
+      if ( p2 <= endPos )
+      {
         newLine = true;
         break;
-      }  
-      
+      }
+
       if ( (end - p1) <= len) // check if last line
-      { 
+      {
         newLine = true;
         p2 = end;
         break;
-      } 
-       
+      }
+
       newLine = false;
       p2 = text.find_last_of(wrapChars, endPos);
-      
+
       bool lastOk = (end-p2) >= len/3;
       // dont wrap if the next char is also a wrap char
-      if (lastOk && wrapChars.find(text[p2+1]) == string::npos) 
+      if (lastOk && wrapChars.find(text[p2+1]) == string::npos)
       {
         found = true;
-      }  
+      }
       else
-      { 
+      {
         endPos = p2-1;
-      }  
+      }
     }
 
     if (trace) {
       cout << "p1: " << p1 << endl;
       cout << "endPos: " << endPos << endl;
       cout << "p2: " << p2 << endl;
-    }  
-      
-    if ( !newLine && 
+    }
+
+    if ( !newLine &&
          ( (p2 == string::npos) || ((p2-p1+1) < 2*len/3) || (p2 <= p1) ))
     {
-      if (trace) 
+      if (trace)
         cout << "force break" << endl;
-        
-      // There is no suitable wrap char in the next len 
+
+      // There is no suitable wrap char in the next len
       // chars, hence we need to force a wrap. We split after
       // the first non-wrapChar below 4/5 of the textwidth
-      
-      size_t cutLen = 4*len/5; 
+
+      size_t cutLen = 4*len/5;
       endPos = p1+len;
       p2=endPos;
       while ( (p2 >= p1+cutLen) )
-      { 
+      {
         p2 = text.find_last_not_of(wrapChars, endPos);
         endPos = p2-1;
-      }  
+      }
       if (p2 == string::npos)
         p2 = p1+cutLen;
       substr = text.substr(p1,p2-p1);
       lastbreak = p2;
-    }  
+    }
     else
     {
-      if (trace) 
+      if (trace)
         cout << "soft break" << endl;
-      
-      assert( p2 <= (p1+len) );    
-      assert( p2 >= p1 );    
+
+      assert( p2 <= (p1+len) );
+      assert( p2 >= p1 );
       substr = text.substr(p1,p2-p1);
       lastbreak = p2;
     }
     if (trace) {
-      cout << "text[" << lastbreak << "]='" 
+      cout << "text[" << lastbreak << "]='"
            << text[lastbreak] << "'" << endl;
-    }  
+    }
     lines++;
 
-    // 
+    //
     if ( isspace(text[lastbreak]) ) {
       lastbreak++;
-    }  
+    }
     p1 = lastbreak;
     result += indentStr + substr;
     if (trace) {
-      cout << "substr: " << substr << endl;    
-      cout << "lastbreak: " << lastbreak << endl;    
-    }  
+      cout << "substr: " << substr << endl;
+      cout << "lastbreak: " << lastbreak << endl;
+    }
   }
   return result;
 }
@@ -615,14 +619,14 @@ wordWrap(const string& s1, const int textwidth, const string& s2)
 }
 
 string
-wordWrap( const string& s1, const int indent2, 
+wordWrap( const string& s1, const int indent2,
           const int textwidth, const string& s2 )
 {
   return s1 + wordWrap( s1.size(), indent2, textwidth, s2);
 }
 
 string
-wordWrap( const int textwidth, const string& s, 
+wordWrap( const int textwidth, const string& s,
           const int indent1/*=0*/, const int indent2/*=0*/ )
 {
   return wordWrap(indent1, indent2, textwidth, s);
@@ -640,15 +644,15 @@ string trim(const string& s, const string& ext/*=""*/)
   static const string ignoreChars=" \n\r\a\b\f\t\v"+ext;
   size_t start = s.find_first_not_of(ignoreChars);
   size_t end = s.find_last_not_of(ignoreChars);
-  
+
   if (start != string::npos && end != string::npos)
     return s.substr(start,end-start+1);
   if (start == string::npos && end != string::npos)
-    return s.substr(0, end+1);  
+    return s.substr(0, end+1);
   if (start != string::npos && end == string::npos)
-    return s.substr(start);  
+    return s.substr(start);
 
-  return s;  
+  return s;
 }
 
 bool expandVarRef(string& s)
@@ -663,7 +667,7 @@ bool expandVarRef(string& s)
     char* val = getenv( var.c_str() );
     if ( !val )
       val = "";
-   
+
     s.replace(pos1, pos2-pos1+1, val);
     //cout << "Variable: " << var << endl;
     //cout << "Value   : " <<  val << endl;
@@ -681,7 +685,7 @@ string expandVar(const string& s)
     t = translate(t, "/", "\\");
   else
     t = translate(t, "\\", "/");
-  return t; 
+  return t;
 }
 
 // append characters if necessary
@@ -691,13 +695,13 @@ string padStr(const string& s, size_t n, const char ch/*=' '*/)
     return s + string( n - s.length(), ch );
   else
     return s;
-} 
+}
 
 
 bool hasSuffix(const string& suf, const string& s)
 {
   return (s.rfind(suf) != string::npos);
-}  
+}
 
 
 bool removeSuffix(const string& suf, string& s)
@@ -708,13 +712,13 @@ bool removeSuffix(const string& suf, string& s)
     return true;
   }
   return false;
-}  
+}
 
 
 bool hasPrefix(const string& pre, const string& s)
 {
   return ( s.find(pre) == 0);
-}  
+}
 
 
 bool removePrefix(const string& pre, string& s)
@@ -725,5 +729,5 @@ bool removePrefix(const string& pre, string& s)
     return true;
   }
   return false;
-}  
+}
 
