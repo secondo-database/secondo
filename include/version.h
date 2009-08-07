04ffcc1727fb6be8e32bd892059f4d883e80e8bc
behr <>
2009-08-07 12:20:34 +0000

    Version changed

--- include/version.h
+++ include/version.h
@@ -9,7 +9,7 @@ release-<MAJOR><MINOR>.
 #define SEC_VERSION_H
 
 #define SECONDO_VERSION_MAJOR      2
-#define SECONDO_VERSION_MINOR      8
-#define SECONDO_VERSION_REVISION   4
+#define SECONDO_VERSION_MINOR      9
+#define SECONDO_VERSION_REVISION   0
 
 #endif
