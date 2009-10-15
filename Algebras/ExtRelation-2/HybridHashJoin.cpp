b825c1203d792c256a37f49dce4589bf40fce4d0
sjung <>
2009-10-15 18:18:14 +0000

    Update of source code documentation

--- Algebras/ExtRelation-2/HybridHashJoin.cpp
+++ Algebras/ExtRelation-2/HybridHashJoin.cpp
@@ -20,7 +20,7 @@ along with SECONDO; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ----
 
-1 Implementation File Sort.cpp
+1 Implementation File HybridHashJoin.cpp
 
 June 2009, Sven Jungnickel. Initial version
 
