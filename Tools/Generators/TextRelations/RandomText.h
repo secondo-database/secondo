5a674a675d3e48bdd5bf49dbb463245fe1e672ab
jandt <>
2010-01-28 14:03:39 +0000

    Generatoren für Zufallstext Relationen von Herrn Jungnickel.

---
+++ Tools/Generators/TextRelations/RandomText.h
@@ -0,0 +1,45 @@
+/*
+November 2009, S.Jungnickel
+
+This file contains functions for random text generation.
+
+*/   
+
+#include "math.h"
+
+using namespace std;
+
+/*
+Creates a random string of length ~l~. Possible characters are
+[0-9][A-Z][a-z] 
+
+*/
+
+string randomText(int l)
+{
+  string s;
+
+  for (int j = 0; j < l; j++) 
+  { 
+    int c;
+
+    switch ( rand() % 3 )
+    {
+      case 0:
+  c = rand() % 10 + 48; // 0-9
+  break;
+      case 1:
+  c = rand() % 26 + 65; // A-Z
+  break;
+      case 2:
+  c = rand() % 26 + 97; // a-z
+  break;
+    }
+
+    s.push_back(c);
+  }
+
+  return s;
+}
+
+
