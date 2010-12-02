68f43a52a16e0a0f2b21abce9948894b773ca7e1
duentgen <>
2010-12-02 13:22:26 +0000

    Initial version

---
+++ Javagui/viewer/hoese/BackgroundListener.java
@@ -0,0 +1,11 @@
+package viewer.hoese;
+
+import viewer.hoese.BackgroundChangedEvent;
+
+/**
+ * @author Christian Duentgen
+ * 
+ */
+public interface BackgroundListener {
+	public void handleBackgroundChangedEvent(BackgroundChangedEvent evt);
+}
