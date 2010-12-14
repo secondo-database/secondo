0192cd156124cf38e47ee5b821d5e439af9ec4f4
behr <>
2010-12-14 14:45:30 +0000

    Error message changed.

--- Javagui/viewer/hoese/ImgLoader.java
+++ Javagui/viewer/hoese/ImgLoader.java
@@ -10,6 +10,9 @@ public class ImgLoader implements ObjectLoader<CachedImage>{
         try{
           return new CachedImage(javax.imageio.ImageIO.read(f));
         } catch(Exception e){ 
+          //e.printStackTrace();
+          System.err.println("problem in reading image from "+f);
+          tools.Reporter.debug(e);
           return null;
         }   
      }   
