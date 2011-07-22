a96d90a83e3f2c1cd2b82cca28105d2b23d15b3e
xu <>
2011-07-22 10:44:11 +0000

    new implementation of triangulation

--- Algebras/TransportationMode/Triangulate.h
+++ Algebras/TransportationMode/Triangulate.h
@@ -1200,5 +1200,223 @@ public:
 };
 
 
+/* 
+class ExceptAssert used in ASSERT macro
+
+*/
+
+class ExceptAssert : public Except
+{   
+public:
+    ExceptAssert()          {};
+    ExceptAssert( MGString com, MGString fname, MGInt line) 
+        : Except( com, fname, line) {};
+        
+    virtual ~ExceptAssert() {};
+    
+    virtual MGInt   GetExType()     const { return EX_ASSERT_CODE;}
+    virtual MGString    GetExPrefix()   const { return EX_ASSERT_STR;}
+};
+
+
+/* 
+class InternalException
+  for handling abnormal events specific for mesh gen. program
+
+*/
+
+class ExceptInter : public Except
+{
+public:
+    ExceptInter()           {};
+    ExceptInter( MGString com, MGString fname, MGInt line) 
+        : Except( com, fname, line) {};
+        
+    virtual ~ExceptInter()  {};
+    
+    virtual MGInt   GetExType()     const { return EX_INTERNAL_CODE;}
+    virtual MGString    GetExPrefix()   const { return EX_INTERNAL_STR;}
+};
+
+
+/* 
+class ExceptRExp
+  for handling regular expresion exceptions
+
+*/
+
+class ExceptRExp : public Except
+{   
+public:
+    ExceptRExp()                {};
+    ExceptRExp( MGString com, MGString fname, MGInt line) 
+        : Except( com, fname, line) {};
+        
+    virtual ~ExceptRExp()   {};
+    
+    virtual MGInt   GetExType()     const { return EX_REXP_CODE;}
+    virtual MGString    GetExPrefix()   const { return EX_REXP_STR;}
+};
+
+
+
+
+
+
+/* 
+macros for throwing Exceptions 
+
+*/
+
+
+#ifdef assert
+    // if compiler provides a assert macro in "assert.h"
+    #define ASSERT assert
+#else
+ // assert macro which does not depend on compiler; uses exception ExceptAssert
+    #ifdef _DEBUG
+        #define ASSERT(f) \
+        { \
+        if (!(f)) \
+  throw new ExceptAssert( "assertion failed: '" #f "'", THIS_FILE, __LINE__ ); \
+        }
+    #else
+        #define ASSERT(f) \
+        { \
+        }
+    #endif // _DEBUG
+#endif // assert
+
+
+#define THROW_ALLOC(f) \
+    { \
+    if (!(f)) \
+     throw new ExceptMem( "allocation error: '" #f "'", THIS_FILE, __LINE__ ); \
+    }
+
+#define THROW_MEMORY(f) \
+    { \
+        throw new ExceptMem( f, THIS_FILE, __LINE__ ); \
+    }
+
+#define THROW_FILE(f1, f2) \
+    { \
+        throw new ExceptFile( f1, f2, THIS_FILE, __LINE__ ); \
+    }
+
+#define THROW_MATH(f) \
+    { \
+        throw new ExceptMath( f, THIS_FILE, __LINE__ ); \
+    }
+
+#define THROW_INTERNAL(f) \
+    { \
+        throw new ExceptInter( f, THIS_FILE, __LINE__ ); \
+    }
+
+#define THROW_REGEXP(f) \
+    { \
+        throw new ExceptRExp( f, THIS_FILE, __LINE__ ); \
+    }
+
+
+/*
+rectangle structure 
+
+*/
+class HRect
+{
+public:
+    HRect() {}
+    HRect( const MGFloat& xmin, const MGFloat& ymin,
+          const MGFloat& xmax, const MGFloat& ymax) 
+          : mvMin( Vect2D(xmin,ymin)), mvMax( Vect2D(xmax,ymax))    {}
+
+    ~HRect()    {}
+
+    void ExportTEC( FILE *f);
+
+    bool    IsInside( const Vect2D& vct) const;
+    bool    IsOverlapping( const HRect& rec) const;
+    
+    Vect2D  Center()                { return (mvMin + mvMax)/2.0;}
+
+    const Vect2D&   VMin() const    { return mvMin;}
+          Vect2D&   rVMin()         { return mvMin;}
+          
+    const Vect2D&   VMax() const    { return mvMax;}
+          Vect2D&   rVMax()         { return mvMax;}
+
+    const MGFloat&  XMin() const    { return mvMin.X();}
+    const MGFloat&  YMin() const    { return mvMin.Y();}
+    const MGFloat&  XMax() const    { return mvMax.X();}
+    const MGFloat&  YMax() const    { return mvMax.Y();}
+          MGFloat&  rXMin()         { return mvMin.rX();}
+          MGFloat&  rYMin()         { return mvMin.rY();}
+          MGFloat&  rXMax()         { return mvMax.rX();}
+          MGFloat&  rYMax()         { return mvMax.rY();}
+
+    
+protected:
+    Vect2D  mvMin;
+    Vect2D  mvMax;
+};
+
+
+inline bool HRect::IsInside( const Vect2D& vct) const
+{
+    if ( vct.X() <= mvMax.X() && vct.X() >= mvMin.X() &&
+         vct.Y() <= mvMax.Y() && vct.Y() >= mvMin.Y() )
+
+        return true;
+    else
+        return false;
+}
+
+inline bool HRect::IsOverlapping( const HRect& rec) const
+{
+    if ( rec.IsInside( Vect2D( XMin(), YMin() )) ||
+         rec.IsInside( Vect2D( XMax(), YMin() )) ||
+         rec.IsInside( Vect2D( XMin(), YMax() )) ||
+         rec.IsInside( Vect2D( XMax(), YMax() )) )
+    {
+        return true;
+    }
+    else if ( IsInside( Vect2D( rec.XMin(), rec.YMin() )) ||
+              IsInside( Vect2D( rec.XMax(), rec.YMin() )) ||
+              IsInside( Vect2D( rec.XMin(), rec.YMax() )) ||
+              IsInside( Vect2D( rec.XMax(), rec.YMax() )) )
+    {
+        return true;
+    }
+    else if ( rec.XMin() > XMin() && rec.XMax() < XMax() &&
+              rec.YMin() < YMin() && rec.YMax() > YMax() )
+    {
+        return true;
+    }
+    else if ( rec.XMin() < XMin() && rec.XMax() > XMax() &&
+              rec.YMin() > YMin() && rec.YMax() < YMax() )
+    {
+        return true;
+    }
+    else
+        return false;
+}
+
+
+
+
+inline void HRect::ExportTEC( FILE *f)
+{
+    fprintf( f, "VARIABLES = \"X\",\"Y\"\n");
+    fprintf( f, "ZONE I=%d, F=POINT\n", 5);
+    fprintf( f, "%lg %lg\n", mvMin.X(), mvMin.Y() );
+    fprintf( f, "%lg %lg\n", mvMax.X(), mvMin.Y() );
+    fprintf( f, "%lg %lg\n", mvMax.X(), mvMax.Y() );
+    fprintf( f, "%lg %lg\n", mvMin.X(), mvMax.Y() );
+    fprintf( f, "%lg %lg\n", mvMin.X(), mvMin.Y() );
+}
+
+
 
 #endif
