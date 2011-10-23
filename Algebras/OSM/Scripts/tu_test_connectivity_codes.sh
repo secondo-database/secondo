769c07b4bb525f3ce141abae3e114aec7f1da606
uchdorf <>
2011-10-23 11:13:01 +0000

    some minor changes

--- Algebras/OSM/Scripts/tu_test_connectivity_codes.sh
+++ Algebras/OSM/Scripts/tu_test_connectivity_codes.sh
@@ -5,14 +5,24 @@ inp='TuTestConnectivityCodes.sec.tmpl'
 outp="TuTestConnectivityCodes.sec"
 bin_dir_path="${SECONDO_BUILD_DIR}/bin"
 script_dir_path="${SECONDO_BUILD_DIR}/Algebras/OSM/Scripts"
+inp_file_path="${script_dir_path}/${inp}"
+outp_file_path="${script_dir_path}/${outp}"
+
+export_dir_path="${script_dir_path}/Exported"
+road1_dual='TRUE'
+road2_dual='TRUE'
 while (test ${i} -lt 65536)
 do
+   export_file_path="${export_dir_path}/${i}.txt"
    echo ${i}
    expr1="s#<Road1Dual>#${road1_dual}#g"
    expr2="s#<Road2Dual>#${road2_dual}#g"
    expr3="s#<ConnectivityCode>#${i}#g"
-   sed -E ${expr1} "${script_dir_path}/${inp}"| sed -E  ${expr2} | 
-      sed -E  ${expr3} > "${script_dir_path}/${outp}"
-   ${bin_dir_path}/SecondoTTYNT -i "${script_dir_path}/TuTestConnectivityCode.sec"
+   expr4="s#<ExportFile>#${export_file_path}#g"
+   sed -E ${expr1} "${inp_file_path}"| sed -E  ${expr2} | 
+      sed -E  ${expr3} | sed -E  ${expr4} > ${outp_file_path} 
+   ${bin_dir_path}/SecondoTTYNT -i ${outp_file_path}
    let i=$i+1
 done
+
+sh -c 'for i in `ls Exported/*.txt`; do echo "$i `tail -n 1 ${i}|cut -c 6-36`"|sed "s# #,#g"; done'>ConnectivityCodesVsTransitions.csv
