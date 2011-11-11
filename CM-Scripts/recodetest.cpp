aa161e34490c5910818b4a1da22b1bbee9a7cef7
behr <>
2011-11-11 13:50:33 +0000

    Script and test programm for checking of the presense of recode.h

---
+++ CM-Scripts/recodetest.cpp
@@ -0,0 +1,16 @@
+/*
+
+Some code for checking the presense of the recode library
+
+
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <recode.h>
+
+int main(int argc, char** argv){
+
+   return 3;
+}
