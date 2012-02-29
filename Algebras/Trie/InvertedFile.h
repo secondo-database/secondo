383a23af228ed495e24ac2041858b9c68fccdb10
behr <>
2012-02-29 08:40:53 +0000

    Change to use VTRie2 implementation

--- Algebras/Trie/InvertedFile.h
+++ Algebras/Trie/InvertedFile.h
@@ -33,7 +33,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "ListUtils.h"
 #include "StringUtils.h"
 #include "Trie.h"
-#include "VTrie.h"
+#include "VTrie2.h"
 
 #include "LRU.h"
 
