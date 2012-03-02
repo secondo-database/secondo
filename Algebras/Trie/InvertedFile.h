e9ea063e9961c6911be62092079e7ad74d323aca
behr <>
2012-03-02 15:25:53 +0000

    Wrong include removed

--- Algebras/Trie/InvertedFile.h
+++ Algebras/Trie/InvertedFile.h
@@ -33,7 +33,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "ListUtils.h"
 #include "StringUtils.h"
 #include "VTrie2.h"
-#include "MMTrie.h"
+//#include "MMTrie.h"
 
 #include "LRU.h"
 
