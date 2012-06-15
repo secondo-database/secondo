9283a997e56ea9eae10a3d448b023026390ff6b6
nidzwetzki <>
2012-06-15 21:31:07 +0000

    First revision

---
+++ Algebras/CostEstimation/ExtRelationAlgebraCostEstimation.h
@@ -0,0 +1,258 @@
+/*
+----
+This file is part of SECONDO.
+
+Copyright (C) 2004, University in Hagen, Department of Computer Science,
+Database Systems for New Applications.
+
+SECONDO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SECONDO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SECONDO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+----
+
+//paragraph [1] Title: [{\Large \bf \begin{center}] [\end{center}}]
+//paragraph [10] Footnote: [{\footnote{] [}}]
+//[TOC] [\tableofcontents]
+//[_] [\_]
+//[&] [\&]
+//[x] [\ensuremath{\times}]
+//[->] [\ensuremath{\rightarrow}]
+//[>] [\ensuremath{>}]
+//[<] [\ensuremath{<}]
+//[ast] [\ensuremath{\ast}]
+
+*/
+
+/*
+[1] ExtRelationAlgebraCostEstimation 
+
+Jun, 2012. First Revision - Jan Kristof Nidzwetzki
+
+[TOC]
+
+0 Description
+
+This file provides some CostEstimationClasses for the ExtRelationAlgebra. 
+
+Jun 2012, JKN, First version of this file
+
+*/
+
+/*
+0.1 Defines
+
+*/
+
+#ifndef COST_EST_EXT_RELATION_ALG_H
+#define COST_EST_EXT_RELATION_ALG_H
+
+#define DEBUG false 
+
+/*
+1.0 Prototyping
+
+Local info for operator
+
+*/
+
+
+/*
+1.1 The class ~ItHashJoinCostEstimation~ provides cost estimation
+    capabilities for the operator itHashJoin
+
+*/
+class LoopJoinCostEstimation : public CostEstimation 
+{
+
+public:
+    LoopJoinCostEstimation()
+    {    
+       pli = new ProgressLocalInfo();
+    }    
+
+/*
+1.2 Free local datastructures
+
+*/
+  virtual ~LoopJoinCostEstimation() {
+     if(pli) {
+        delete pli;
+     }
+  };
+
+  virtual int requestProgress(Word* args, ProgressInfo* pRes, void* localInfo, 
+    bool argsAvialable) {
+
+     // no progress info available => cancel
+     if(! argsAvialable) {
+         return CANCEL;
+     }
+
+
+     if (qp->RequestProgress(args[0].addr, &p1)
+       && qp->RequestProgress(args[1].addr, &p2)) 
+      {     
+        pli->SetJoinSizes(p1, p2);
+
+        // Read memory for operator in bytes
+        size_t maxmem = qp->GetMemorySize(supplier) * 1024 * 1024;
+        
+
+          pRes->CopySizes(pli);
+
+          return YIELD;
+       }
+
+   // default: send cancel
+   return CANCEL;
+}
+
+
+/*
+1.3 getCosts
+
+Returns the estimated time in ms for given arguments.
+
+*/
+virtual bool getCosts(const size_t NoTuples1, const size_t sizeOfTuple1,
+                      const size_t NoTuples2, const size_t sizeOfTuple2,
+                      const double memoryMB, double &costs) const{
+
+         
+     return true;
+}
+
+
+/*
+1.4 Calculate the sufficent memory for this operator.
+
+*/
+double calculateSufficientMemory(size_t NoTuples1, size_t sizeOfTuple1) const {
+
+}
+
+/*
+1.5 Get Linear Params
+Input: 
+NoTuples1, sizeOfTuple1
+NoTuples2, sizeOfTuple2,
+
+Output:
+sufficientMemory = sufficientMemory for this operator with the given 
+                   input
+
+timeAtSuffMemory = Time for the calculation with sufficientMemory
+
+timeAt16MB - Time for the calculation with 16MB Memory
+
+*/
+   virtual bool getLinearParams(
+            size_t NoTuples1, size_t sizeOfTuple1,
+            size_t NoTuples2, size_t sizeOfTuple2,
+            double& sufficientMemory, double& timeAtSuffMemory,
+            double& timeAt16MB )  const { 
+      
+      sufficientMemory=calculateSufficientMemory(NoTuples2, sizeOfTuple2);
+      
+      getCosts(NoTuples1, sizeOfTuple1, NoTuples2, sizeOfTuple2, 
+        sufficientMemory, timeAtSuffMemory);
+
+      getCosts(NoTuples1, sizeOfTuple1, NoTuples2, sizeOfTuple2, 
+        16, timeAt16MB);
+      
+      return true;
+   }
+
+/*
+1.6 getFunction
+
+This function approximates the costfunction by an parametrizable
+function. Allowed types are:
+
+1: linear function
+2: a / x
+
+*/
+   virtual bool getLinearParams(
+            size_t NoTuples1, size_t sizeOfTuple1,
+            size_t NoTuples2, size_t sizeOfTuple2,
+            int& functionType,
+            double& sufficientMemory, double& timeAtSuffMemory,
+            double& timeAt16MB,
+            double& a, double& b, double& c, double& d) const {
+       functionType=1;
+       a=0;b=0;c=0;d=0;
+       return getLinearParams(NoTuples1, sizeOfTuple1,
+                              NoTuples2, sizeOfTuple2,
+                              sufficientMemory, timeAtSuffMemory, 
+                              timeAt16MB);  
+  }  
+   
+
+/*
+1.8 Setter for stream1Exhausted
+
+*/
+  void setStream1Exhausted(bool exhausted) {
+      stream1Exhausted = exhausted;
+  }
+
+/*
+1.9 Setter for stream2Exhausted
+
+*/
+  void setStream2Exhausted(bool exhausted) {
+      stream2Exhausted = exhausted;
+  }
+
+
+/*
+1.10 Update processed tuples in stream1
+
+*/
+   void processedTupleInStream1() {
+      readStream1++;
+   }
+
+/*
+1.11 Update processed tuples in stream2
+
+*/
+    void processedTupleInStream2() {
+       readStream2++;
+    }
+
+/*
+1.19 init our class
+
+*/
+  virtual void init(Word* args, void* localInfo)
+  {
+    returned = 0;
+    stream1Exhausted = false;
+    stream2Exhausted = false;
+    readStream1 = 0;
+    readStream2 = 0;
+  }
+
+private:
+  ProgressLocalInfo *pli;   // Local Progress info
+  ProgressInfo p1, p2;      // Progress info for stream 1 / 2
+  bool stream1Exhausted;    // is stream 1 exhaused?
+  bool stream2Exhausted;    // is stream 2 exhaused?
+  size_t readStream1;       // processed tuple in stream1
+  size_t readStream2;       // processes tuple in stream2
+};
+
+
+#endif
