d1f0c7ba9dd8f9f00a18b6e027e6cd52fdcef0cc
nidzwetzki <>
2012-08-29 11:41:06 +0000

    Changed type int -> double

--- Tools/ProgressWatcher/src/progresswatcher/FileWatcherWorker.java
+++ Tools/ProgressWatcher/src/progresswatcher/FileWatcherWorker.java
@@ -125,7 +125,7 @@ public class FileWatcherWorker implements Runnable {
 			// Get Progress valued
 			int time = Integer.parseInt(matcher.group(1));
 			double card = Double.parseDouble(matcher.group(2));
-			int estTime = Integer.parseInt(matcher.group(3));
+			double estTime = Double.parseDouble(matcher.group(3));
 			
 			NumberFormat format = NumberFormat.getInstance(Locale.GERMAN);
 
