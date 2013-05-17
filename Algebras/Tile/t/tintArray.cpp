0998008c996ebc08eba7bc0bad9f3a1a1163feb1
zacher <>
2013-05-17 12:33:47 +0000

    Anpassung fuer globale Konstanten

--- Algebras/Tile/t/tintArray.cpp
+++ Algebras/Tile/t/tintArray.cpp
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */ 
 
-#include "../Defines.h"
+#include "../Constants.h"
 #include "tintArray.h"
 #include "TypeConstructor.h"
 #include "Symbols.h"
