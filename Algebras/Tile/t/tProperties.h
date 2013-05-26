edd6ae0b8d1f92d5993243926b0e5dbd379c3b8c
zacher <>
2013-05-26 09:10:28 +0000

    Implementierung der Template Klasse tProperties

---
+++ Algebras/Tile/t/tProperties.h
@@ -0,0 +1,58 @@
+/*
+This file is part of SECONDO.
+
+Copyright (C) 2011, University in Hagen, Department of Computer Science,
+Database Systems for New Applications.
+
+SECONDO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SECONDO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SECONDO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+#ifndef TILEALGEBRA_TPROPERTIES_H
+#define TILEALGEBRA_TPROPERTIES_H
+
+namespace TileAlgebra
+{
+
+/*
+declaration of template class tProperties
+
+*/
+
+template <class Type>
+class tProperties
+{
+  public:
+
+  typedef Type ImplementationType;
+  static int GetDimensionSize();
+  static int GetFlobElements();
+  static SmiSize GetFlobSize();
+  static std::string GetTypeName(); 
+  static Type GetUndefinedValue();
+  static Type GetValue(const NList& rNList);
+  static bool IsUndefinedValue(const Type& rType);
+  static bool IsValidValueType(const NList& rNList);
+  static NList ToNList(const Type& rType);
+};
+
+/*
+implementation of template class tProperties
+
+*/
+
+}
+
+#endif // TILEALGEBRA_TPROPERTIES_H
