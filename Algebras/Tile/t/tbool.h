5e7bad2a54cf8912e7d1764ac491d704283ea31f
zacher <>
2013-05-30 14:10:02 +0000

    Implementierung des Datentyps tbool

--- Algebras/Tile/t/tbool.h
+++ Algebras/Tile/t/tbool.h
@@ -66,7 +66,7 @@ implementation of template class tProperties<char>
 int tProperties<char>::GetDimensionSize()
 {
   int dimensionSize = static_cast<unsigned int>
-                      (std::sqrt((WinUnix::getPageSize() - sizeof(Grid2)) /
+                      (std::sqrt((WinUnix::getPageSize() - sizeof(grid2)) /
                        sizeof(char)));
 
   return dimensionSize;
