b0005b4e65e45723c1d17f337bc4c21be33b5736
zacher <>
2013-06-07 16:25:18 +0000

    Implementierung des Datentyps mtbool

--- Algebras/Tile/mt/mtbool.h
+++ Algebras/Tile/mt/mtbool.h
@@ -26,6 +26,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "mt.h"
 #include "mtProperties.h"
 #include "../Properties/Propertiesbool.h"
+#include "RectangleAlgebra.h"
 
 namespace TileAlgebra
 {
@@ -48,6 +49,7 @@ class mtProperties<char>
   public:
 
   typedef Properties<char> TypeProperties;
+  typedef Rectangle<3> bboxType;
   typedef mtgrid gridType;
   typedef mtbool mtType;
   static int GetDimensionSize();
