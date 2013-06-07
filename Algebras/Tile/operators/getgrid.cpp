562030dbed0719c56846281fb0bb99e88fb939bd
zacher <>
2013-06-07 12:12:03 +0000

    Implementierung des getgrid Operators

---
+++ Algebras/Tile/operators/getgrid.cpp
@@ -0,0 +1,124 @@
+/*
+This file is part of SECONDO.
+
+Copyright (C) 2013, University in Hagen, Department of Computer Science,
+Database Systems for New Applications.
+
+SECONDO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SECONDO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SECONDO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+#include "getgrid.h"
+#include "../t/tint.h"
+#include "../t/treal.h"
+#include "../t/tbool.h"
+#include "../t/tstring.h"
+#include "../mt/mtint.h"
+#include "../mt/mtreal.h"
+#include "../mt/mtbool.h"
+#include "../mt/mtstring.h"
+
+namespace TileAlgebra
+{
+
+/*
+definition of getgrid functions
+
+*/
+
+ValueMapping getgridFunctions[] =
+{
+  getgridFunction<tint, tProperties<int> >,
+  getgridFunction<treal, tProperties<double> >,
+  getgridFunction<tbool, tProperties<char> >,
+  getgridFunction<tstring, tProperties<std::string> >,
+  getgridFunction<mtint, mtProperties<int> >,
+  getgridFunction<mtreal, mtProperties<double> >,
+  getgridFunction<mtbool, mtProperties<char> >,
+  getgridFunction<mtstring, mtProperties<std::string> >,
+  0
+};
+
+/*
+definition of getgrid select function
+
+*/
+
+int getgridSelectFunction(ListExpr arguments)
+{
+  int nSelection = -1;
+
+  if(arguments != 0)
+  {
+    NList argumentsList(arguments);
+    NList argument1 = argumentsList.first();
+    const int TYPE_NAMES = 8;
+    const std::string TYPE_NAMES_ARRAY[TYPE_NAMES] =
+    {
+      tint::BasicType(),
+      treal::BasicType(),
+      tbool::BasicType(),
+      tstring::BasicType(),
+      mtint::BasicType(),
+      mtreal::BasicType(),
+      mtbool::BasicType(),
+      mtstring::BasicType()
+    };
+
+    for(int i = 0; i < TYPE_NAMES; i++)
+    {
+      if(argument1.isSymbol(TYPE_NAMES_ARRAY[i]))
+      {
+        nSelection = i;
+        break;
+      }
+    }
+  }
+
+  return nSelection;
+}
+
+/*
+definition of getgrid type mapping function
+
+*/
+
+ListExpr getgridTypeMapping(ListExpr arguments)
+{
+  ListExpr type = NList::typeError("Expecting a t or a mt type.");
+
+  NList argumentsList(arguments);
+  NList argument1 = argumentsList.first();
+
+  if(argument1 == NList(tint::BasicType()) ||
+     argument1 == NList(treal::BasicType()) ||
+     argument1 == NList(tbool::BasicType()) ||
+     argument1 == NList(tstring::BasicType()))
+  {
+    type = NList(tgrid::BasicType()).listExpr();
+  }
+  
+  if(argument1 == NList(mtint::BasicType()) ||
+     argument1 == NList(mtreal::BasicType()) ||
+     argument1 == NList(mtbool::BasicType()) ||
+     argument1 == NList(mtstring::BasicType()))
+  {
+    type = NList(mtgrid::BasicType()).listExpr();
+  }
+
+  return type;
+}
+
+}
---
+++ Algebras/Tile/operators/getgrid.h
@@ -0,0 +1,112 @@
+/*
+This file is part of SECONDO.
+
+Copyright (C) 2013, University in Hagen, Department of Computer Science,
+Database Systems for New Applications.
+
+SECONDO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SECONDO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SECONDO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+#ifndef TILEALGEBRA_GETGRID_H
+#define TILEALGEBRA_GETGRID_H
+
+#include "AlgebraTypes.h"
+#include "Operator.h"
+#include "QueryProcessor.h"
+
+namespace TileAlgebra
+{
+
+/*
+declaration of getgrid functions
+
+*/
+
+extern ValueMapping getgridFunctions[];
+
+/*
+declaration of getgrid select function
+
+*/
+
+int getgridSelectFunction(ListExpr arguments);
+
+/*
+declaration of getgrid type mapping function
+
+*/
+
+ListExpr getgridTypeMapping(ListExpr arguments);
+
+/*
+definition of getgrid Operator Info structure
+
+*/
+
+struct getgridInfo : OperatorInfo
+{
+  getgridInfo()
+  {
+    name      = "getgrid";
+    signature = "tT -> tgrid";
+    appendSignature("mtT -> mtgrid");
+    syntax    = "getgrid(_)";
+    meaning   = "Returns the grid of a t or mt type.";
+  }
+};
+
+/*
+definition of template getgrid function
+
+*/
+
+template <typename Type, typename Properties>
+int getgridFunction(Word* pArguments,
+                    Word& rResult,
+                    int message,
+                    Word& rLocal,
+                    Supplier supplier)
+{
+  int nRetVal = 0;
+
+  if(qp != 0 &&
+     pArguments != 0)
+  {
+    Type* pType = static_cast<Type*>(pArguments[0].addr);
+
+    if(pType != 0)
+    {
+      rResult = qp->ResultStorage(supplier);
+
+      if(rResult.addr != 0)
+      {
+        typename Properties::gridType* pResult =
+        static_cast<typename Properties::gridType*>(rResult.addr);
+        
+        if(pResult != 0)
+        {
+          *pResult = pType->GetGrid();
+        }
+      }
+    }
+  }
+
+  return nRetVal;
+}
+
+}
+
+#endif // TILEALGEBRA_GETGRID_H
