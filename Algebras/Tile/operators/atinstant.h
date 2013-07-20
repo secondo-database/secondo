6e05237cd95588c8c335705e83b58624456592cc
zacher <>
2013-07-20 10:48:26 +0000

    Implementierung des atinstant Operators

--- Algebras/Tile/operators/atinstant.h
+++ Algebras/Tile/operators/atinstant.h
@@ -27,7 +27,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "Operator.h"
 #include "QueryProcessor.h"
 #include "DateTime.h"
-#include "../Types.h"
+#include "../Types/Types.h"
 
 namespace TileAlgebra
 {
