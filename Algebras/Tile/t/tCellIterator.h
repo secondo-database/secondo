de428350a70a3a6311924beb3f7a10c9e0e39de8
zacher <>
2013-07-20 10:55:37 +0000

    Implementierung der Template Klasse tCellIterator

--- Algebras/Tile/t/tCellIterator.h
+++ Algebras/Tile/t/tCellIterator.h
@@ -24,7 +24,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #ifndef TILEALGEBRA_TCELLITERATOR_H
 #define TILEALGEBRA_TCELLITERATOR_H
 
-#include "../Index.h"
+#include "../Index/Index.h"
 #include "../grid/tgrid.h"
 
 namespace TileAlgebra
