de6d0642df46a9b22e6da737163c9ca7dc437e13
zacher <>
2013-07-20 10:55:47 +0000

    Implementierung der Template Klasse t

--- Algebras/Tile/t/t.h
+++ Algebras/Tile/t/t.h
@@ -29,7 +29,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "Attribute.h"
 #include "../grid/tgrid.h"
 #include "../../Tools/Flob/Flob.h"
-#include "../Index.h"
+#include "../Index/Index.h"
 #include "RectangleAlgebra.h"
 #include "DateTime.h"
 
