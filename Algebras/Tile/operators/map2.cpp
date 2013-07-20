47ae63b5cc9b04247802143baa6611fa8fd1c08f
zacher <>
2013-07-20 10:52:37 +0000

    Implementierung des map2 Operators

--- Algebras/Tile/operators/map2.cpp
+++ Algebras/Tile/operators/map2.cpp
@@ -21,8 +21,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 #include "map2.h"
-#include "../Index.h"
-#include "../Types.h"
+#include "../Index/Index.h"
+#include "../Types/Types.h"
 #include "../grid/tgrid.h"
 #include "../grid/mtgrid.h"
 #include "../t/tint.h"
