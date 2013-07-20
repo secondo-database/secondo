9b13fb955635ae8f7d3dfb6c13cb9b69d09a96f0
zacher <>
2013-07-20 10:49:08 +0000

    Implementierung des atrange Operators

--- Algebras/Tile/operators/atrange.h
+++ Algebras/Tile/operators/atrange.h
@@ -28,7 +28,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "QueryProcessor.h"
 #include "DateTime.h"
 #include "RectangleAlgebra.h"
-#include "../Types.h"
+#include "../Types/Types.h"
 
 namespace TileAlgebra
 {
