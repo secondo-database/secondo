6cb64aaa58b2b5336d8767853d537c1f93d535bd
zacher <>
2013-07-20 10:47:36 +0000

    Implementierung der Template Klasse mtCellIterator

--- Algebras/Tile/mt/mtCellIterator.h
+++ Algebras/Tile/mt/mtCellIterator.h
@@ -24,7 +24,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #ifndef TILEALGEBRA_MTCELLITERATOR_H
 #define TILEALGEBRA_MTCELLITERATOR_H
 
-#include "../Index.h"
+#include "../Index/Index.h"
 #include "../grid/mtgrid.h"
 
 namespace TileAlgebra
