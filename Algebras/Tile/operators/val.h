263ca6dbab133eaaf5f272a0c3c5176bf4fa371c
zacher <>
2013-07-20 10:54:49 +0000

    Implementierung des val Operators

--- Algebras/Tile/operators/val.h
+++ Algebras/Tile/operators/val.h
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "AlgebraTypes.h"
 #include "Operator.h"
 #include "QueryProcessor.h"
-#include "../Types.h"
+#include "../Types/Types.h"
 
 namespace TileAlgebra
 {
