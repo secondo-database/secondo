383908210d65458b589d0502d58a81707508b336
issa <>
2013-07-22 13:04:21 +0000

    *** empty log message ***

---
+++ Algebras/HybridTrajectory/HybridTrajectory.spec
@@ -0,0 +1,3 @@
+operator getHybridPartition alias GETHYBRIDPARTITION pattern _ op
+operator atinstant alias ATINSTANT pattern _ infixop _
+operator atperiods alias ATPERIODS pattern _ infixop _
--- Algebras/HybridTrajectory/HybridTrajectoryAlgebra.cpp
+++ Algebras/HybridTrajectory/HybridTrajectoryAlgebra.cpp
@@ -1,4 +1,4 @@
-/*
+/*                                                                       
 ----
 This file is part of SECONDO.
 
@@ -18,6 +18,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with SECONDO; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 ----
 
 //paragraph [1] Title: [{\Large \bf \begin {center}] [\end {center}}]
@@ -25,11 +26,14 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 [1] Source File of the Hybrid Trajectory Algebra
 
-Started July 2013, Hamza Issa\'{e}s
+Started  2013 , Hamza Issa\'{e}s
+
+
 
 [TOC]
+
 \section{Overview}
-This algebra includes the operators ~by~ and ~atinstant~.
+This algebra includes the operators ~by~ and ~atperiods~.
 
 \section{Defines and Includes}
 
@@ -46,9 +50,13 @@ This algebra includes the operators ~by~ and ~atinstant~.
 #include "Stream.h"
 #include "TemporalUnitAlgebra.h"
 #include "../NestedRelation/NestedRelationAlgebra.h"
+
+#include "DivisionStream.h"
+#include "HybridTrajectoryAlgebra.h"
 #include <string>
 #include <vector>
-#include "DivisionStream.h"
+
+
 extern NestedList* nl;
 extern QueryProcessor *qp;
 extern AlgebraManager *am;
@@ -83,7 +91,7 @@ ListExpr lastindexList=nl->TheEmptyList();
           bool first=true;
     	  newAttributelist =nl->OneElemList(
 nl->TwoElemList(
-		nl->SymbolAtom("AtInstant"),
+		nl->SymbolAtom("Instant"),
     	nl->SymbolAtom( Instant::BasicType())));
     	  lastnewAttributelist=newAttributelist;
     	  while (!(nl->IsEmpty(attributeList))){
@@ -94,24 +102,24 @@ ListExpr attrtype=nl->Second(singleAttribute);
     		  string newattrname=attrname;
     		  ListExpr newattrtype=attrtype;
     		  int inttoappend =0;
-    		  if( nl->IsEqual( attrtype, MBool::BasicType() ) ){
-    			  newattrtype=nl->SymbolAtom( IBool::BasicType() );
+if( nl->IsEqual( attrtype, MBool::BasicType() ) ){
+newattrtype=nl->SymbolAtom( CcBool::BasicType() );
     			  inttoappend =1;
     		  }
-    		  if( nl->IsEqual( attrtype, MInt::BasicType() ) ){
-    			  newattrtype=nl->SymbolAtom( IInt::BasicType() );
+if( nl->IsEqual( attrtype, MInt::BasicType() ) ){
+newattrtype=nl->SymbolAtom( CcInt::BasicType() );
     			  inttoappend=2;
     		  }
     		  if( nl->IsEqual( attrtype, MReal::BasicType() ) ){
-    			  newattrtype=nl->SymbolAtom( IReal::BasicType() );
+newattrtype=nl->SymbolAtom( CcReal::BasicType() );
     			  inttoappend=3;
     		  }
     		  if( nl->IsEqual( attrtype, MPoint::BasicType() ) ){
-    			  newattrtype=nl->SymbolAtom( IPoint::BasicType() );
+newattrtype=nl->SymbolAtom( Point::BasicType() );
     			  inttoappend=4;
     		  }
     		  if( nl->IsEqual( attrtype, MString::BasicType() ) ){
-newattrtype=nl->SymbolAtom( IString::BasicType() );
+newattrtype=nl->SymbolAtom( CcString::BasicType() );
     			  inttoappend=5;
     		  }
     		  lastnewAttributelist =nl->Append(
@@ -245,26 +253,26 @@ int ArelAtInstant( Word* args, Word& result, int message,
     	  MPoint* mp=(MPoint*)t->GetAttribute(scanIndex);
     	  Intime<Point>* pResult = new Intime<Point>();
     	  mp->AtInstant(* instant, *pResult);
-    	  atuple->PutAttribute(scanIndex+1,pResult);
+		  atuple->PutAttribute(scanIndex+1,&pResult->value);
     }
     else if(type==2){
     	MInt* mint=(MInt*)t->GetAttribute(scanIndex);
     	Intime<CcInt>* pResult = new Intime<CcInt>();
     	mint->AtInstant(* instant, *pResult);
-    	atuple->PutAttribute(scanIndex+1,pResult);
+    	atuple->PutAttribute(scanIndex+1,&pResult->value);
     }
     else if(type==1){
     	MBool* mbool=(MBool*)t->GetAttribute(scanIndex);
     	Intime<CcBool>* pResult = new Intime<CcBool>();
     	mbool->AtInstant(* instant, *pResult);
-    	atuple->PutAttribute(scanIndex+1,pResult);
+    	atuple->PutAttribute(scanIndex+1,&pResult->value);
     }
     else if(type==3){
 
     	MReal* mreal=(MReal*)t->GetAttribute(scanIndex);
     	Intime<CcReal>* pResult = new Intime<CcReal>();
     	mreal->AtInstant(* instant, *pResult);
-    	atuple->PutAttribute(scanIndex+1,pResult);
+    	atuple->PutAttribute(scanIndex+1,&pResult->value);
 
     }
     else if(type==5){
@@ -272,7 +280,7 @@ int ArelAtInstant( Word* args, Word& result, int message,
      	MString* mstring=(MString*)t->GetAttribute(scanIndex);
      	Intime<CcString>* pResult = new Intime<CcString>();
      	mstring->AtInstant(* instant, *pResult);
-     	atuple->PutAttribute(scanIndex+1,pResult);
+     	atuple->PutAttribute(scanIndex+1,&pResult->value);
      }
     else{
     	atuple->CopyAttribute(scanIndex, t, scanIndex+1);
@@ -396,6 +404,7 @@ nl->Second(nl->Second(arg1))));
 outList =nl->ThreeElemList(nl->SymbolAtom(Symbol::APPEND()),
 nl->OneElemList(indexList),
 arelstrcut);
+//cout <<"djskdjskdjskdj"<<nl->ToString(outList)<<endl;
 return outList;
 
     }
@@ -424,6 +433,8 @@ int ArelAtPeriod( Word* args, Word& result, int message,
 	Supplier son;
 	/*Take care about memory leak*/
 	AttributeRelation* arel=(AttributeRelation*) args[0].addr;
+
+
 	Periods* period=   (Periods*)args[1].addr;
 	Relation* r=Relation::GetRelation( arel->getRelId() );
 	ListExpr resultType=qp->GetType(s);
@@ -576,7 +587,8 @@ string t2; nl->WriteToString(t2, second);
 NList resTupleType =NList(NList("Raw"),NList(t1)).enclose();
 resTupleType.append(NList(NList("Symbolic"),NList(t2)));
 NList resType =
-NList(NList(Symbol::STREAM()),NList(NList(Tuple::BasicType()),resTupleType));
+NList(NList(Symbol::STREAM()),
+      NList(NList(Tuple::BasicType()),resTupleType));
 return resType.listExpr();
 
 }
@@ -597,7 +609,8 @@ int ByValueMappingVM( Word* args, Word& result, int message,
       mo1 = static_cast<M1*>(args[0].addr);
       M2* mo2;
       mo2 = static_cast<M2*>(args[1].addr);
-      li = new DivisionStream<M1,U1,M2,U2>(mo1,mo2,GetTupleResultType(s));
+      li = new 
+       DivisionStream<M1,U1,M2,U2>(mo1,mo2,GetTupleResultType(s));
       local.setAddr(li);
       return 0;
     }
@@ -728,6 +741,450 @@ Operator by(
 
 
 /*end of implmenetation of by operator*/
+
+
+/*start implementation of getFullRefinement*/
+
+ListExpr
+getFullRefinmentTypeMapping( ListExpr args )
+{
+	if ( nl->ListLength( args ) == 1 )
+	{
+		ListExpr arg1 = nl->First( args );
+		if(listutils::isRelDescription(arg1,false)){
+
+		ListExpr attributeList =nl->Second(nl->Second(arg1));
+		ListExpr newAttributelist=nl->TheEmptyList();
+		
+		ListExpr lastnewAttributelist=nl->TheEmptyList();
+		
+        
+		ListExpr index=nl->TheEmptyList();
+
+		ListExpr lastIndex=nl->TheEmptyList();
+          
+		  bool first=true;
+		  while (!(nl->IsEmpty(attributeList))){
+
+			  ListExpr singleAttribute =nl->First(attributeList);
+			  attributeList=nl->Rest(attributeList);
+string attrname=nl->SymbolValue(nl->First(singleAttribute));
+			  ListExpr attrtype=nl->Second(singleAttribute);
+    		  string newattrname=attrname;
+    		  ListExpr newattrtype=attrtype;
+    		  
+    		  if( nl->IsEqual( attrtype, MBool::BasicType() ) ){
+newattrtype=nl->SymbolAtom( UBool::BasicType() );
+    			  
+    		  }
+    		  else if( nl->IsEqual( attrtype, MInt::BasicType() ) ){
+newattrtype=nl->SymbolAtom( UInt::BasicType() );
+    			  
+    		  }
+    		  else if( nl->IsEqual( attrtype, MReal::BasicType() ) ){
+newattrtype=nl->SymbolAtom( UReal::BasicType() );
+    			  
+    		  }
+    		  else if( nl->IsEqual( attrtype, MPoint::BasicType() ) ){
+newattrtype=nl->SymbolAtom( UPoint::BasicType() );
+    			  
+    		  }
+    		  else if( nl->IsEqual( attrtype, MString::BasicType() ) ){
+newattrtype=nl->SymbolAtom( UString::BasicType() );
+    			  
+    		  }
+
+			  else
+			  return nl->SymbolAtom( Symbol::TYPEERROR() );
+
+    		  if(first){
+
+				  newAttributelist=nl->OneElemList(
+        nl->TwoElemList(nl->SymbolAtom(attrname), newattrtype));
+				  lastnewAttributelist=newAttributelist;
+    			  
+index=nl->OneElemList(nl->StringAtom(nl->SymbolValue(attrtype)));
+				  lastIndex=index;
+				  first=false;
+
+
+    		  }
+    		  else{
+				  			  
+lastnewAttributelist =nl->Append(lastnewAttributelist,
+       nl->TwoElemList(nl->SymbolAtom(attrname), newattrtype));  	   
+       lastIndex =nl->Append(lastIndex,
+        nl->StringAtom(nl->SymbolValue(attrtype))); 
+			  }
+    	  }
+    	  
+		      	  ListExpr arelstrcut=nl->TwoElemList(
+    			  nl->SymbolAtom(Symbol::STREAM()),
+    			  nl->TwoElemList(
+         nl->SymbolAtom(Tuple::BasicType()),newAttributelist));
+
+		      	  ListExpr outList =nl->ThreeElemList(
+    			  nl->SymbolAtom(Symbol::APPEND()),
+    			  nl->OneElemList(index),
+    			  arelstrcut);
+
+		  //cout <<"djskdjskdjskdj"<<nl->ToString(outList)<<endl;
+    	  return outList;
+		
+	}
+		
+	
+	}
+	
+	return nl->SymbolAtom( Symbol::TYPEERROR() );
+}
+
+
+int
+getFullRefinmentSelect( ListExpr args )
+{
+  ListExpr arg1 = nl->First( args );
+
+  if(listutils::isRelDescription(arg1, false))
+	  return 0;
+
+  return -1;
+}
+
+
+OperatorInfo getFullRefinmentOperatorInfo(
+"getHybridPartition",
+"(rel(T)) -> stream(uT); T is a tuple containing moving"
+"stuff only, uT is tuple containning unit of moving thing"
+"at given periods",
+"rel getHybridPartition",
+"divide first tuple of a relation containg a stuff of moving point"
+"to stream of tuple where each one contains the moving"
+"units to a restricted periods",
+""
+);
+
+
+
+
+ 
+
+
+BaseClass::BaseClass(){
+
+}
+
+void BaseClass::incrementIndex(){
+
+	this->scaningIndex++;
+}
+
+void BaseClass::resetIndex(){
+
+	this->scaningIndex=0;
+}
+
+
+
+void FullRefinment::fillmaps(){
+
+		 int index=0;
+		 std::vector<string>::iterator iter;
+for (iter = this->inType.begin(); iter != this->inType.end(); ++iter)
+		 {
+			 string type=*iter;
+
+			 //cout <<"Type:"<<type<<endl;
+			 if(type.compare(MPoint::BasicType())==0){
+
+				 GeneralMapping<MPoint,UPoint>* m=
+new GeneralMapping<MPoint,UPoint>((MPoint*)this->tuple->GetAttribute(index));
+				 this->totals.insert(std::make_pair(index,m));
+			 }
+			 else
+				 if(type.compare(MString::BasicType())==0){
+
+GeneralMapping<MString,UString>* m=new 
+GeneralMapping<MString,UString>((MString*)this->tuple->GetAttribute(index));
+this->totals.insert(std::make_pair(index,m));
+				 }
+			 else 
+				 if(type.compare(MBool::BasicType())==0){
+					 GeneralMapping<MBool,UBool>* m=new 
+GeneralMapping<MBool,UBool>((MBool*)this->tuple->GetAttribute(index));
+this->totals.insert(std::make_pair(index,m));
+				 }
+			 else 
+				 if(type.compare(MInt::BasicType())==0){
+					 GeneralMapping<MInt,UInt>* m=new 
+GeneralMapping<MInt,UInt>((MInt*)this->tuple->GetAttribute(index));
+this->totals.insert(std::make_pair(index,m));
+				 }
+			 else 
+				 if(type.compare(MReal::BasicType())==0){
+					 GeneralMapping<MReal,UReal>* m=new 
+GeneralMapping<MReal,UReal>((MReal*)this->tuple->GetAttribute(index));
+this->totals.insert(std::make_pair(index,m));
+				 }
+
+			 index ++;
+		 }	 
+}
+
+FullRefinment::FullRefinment(Tuple* args,vector<string> inputType,
+ ListExpr outputTupleExpression){
+
+		this->tuple=args;
+		this->ote=outputTupleExpression;
+		this->numberofAttribute=nl->ListLength(ote);
+		this->inType=inputType;
+		this->fillmaps();
+		this->firsttime=true;
+		this->fillmaps();
+
+}
+
+
+void FullRefinment::getNext(Tuple** result){
+
+
+		
+		 	 Instant mininst;
+			 bool minisclosed;
+
+			Instant endinst;
+			bool endisclosed;
+
+
+			std::map<int, BaseClass*>::iterator iter;
+std::pair<int, BaseClass*> firstEntry=*this->totals.begin() ;
+			BaseClass* first=firstEntry.second;
+
+		    
+			if(this->firsttime==true){
+				first->resetIndex();
+			 first->getNextStart(mininst,minisclosed,true);
+			iter = this->totals.begin()++;
+			for (iter; iter != this->totals.end(); ++iter)	
+			{
+
+			iter->second->resetIndex();
+			 Instant inst;
+			 bool isclosed;
+			 iter->second->getNextStart(inst,isclosed,true);
+			 if(inst<mininst){
+				 mininst=inst;
+				 minisclosed=isclosed;
+			 }
+			 else if(inst==mininst)
+				 minisclosed=minisclosed||isclosed;
+			}
+
+			this->firsttime=false;
+
+		}
+		else
+		{
+		
+		 
+         bool firstmindone=false;
+		 iter=this->totals.begin();
+		 for ( iter;iter != this->totals.end(); ++iter)	
+		 {
+			if(iter->second->isFinished())
+				 continue;
+
+
+			 
+			 int res;
+			 if(firstmindone==false){
+
+res=iter->second->getNextStart(mininst,minisclosed,false);
+				firstmindone=true;
+			 }
+			 else{
+			 
+			Instant inst;
+			bool isclosed;
+			res=iter->second->getNextStart(inst,isclosed,false);
+				 if(inst<mininst){
+				 mininst=inst;
+				 minisclosed=isclosed;
+			 
+				 }
+			 
+				 else if(inst==mininst)
+				 
+					 minisclosed=minisclosed||isclosed;
+			 }
+			 
+			 if(res==1)
+				 break;
+
+		 }
+
+		}
+
+
+			bool finishfirst=false;
+			iter=this->totals.begin();
+			for (iter; iter != this->totals.end(); ++iter)	
+			{
+iter->second->setLastStartInstant(mininst,minisclosed);
+
+			
+				if(iter->second->isFinished())
+				 
+					continue;
+
+
+
+
+				
+						
+				if(finishfirst==false){
+				
+iter->second->getNearestEnd(endinst,endisclosed);
+					finishfirst=true;
+				}
+				else{
+				Instant inst;
+				bool isclosed;
+
+				    iter->second->getNearestEnd(inst,isclosed);
+					if(inst<endinst){
+				 endinst=inst;
+				 endisclosed=isclosed;
+			
+					}
+			 
+					else if(inst==endinst){
+				 endisclosed=endisclosed&&isclosed;
+			 
+					}
+				}
+
+
+			}
+
+
+
+
+
+*result=new Tuple(this->ote);
+for (iter = this->totals.begin(); 
+iter != this->totals.end(); ++iter)
+{
+iter->second->
+ setLastfinishInstant(endinst,endisclosed);
+				Attribute* tem;
+				iter->second->Get(&tem);
+				(*result)->PutAttribute(iter->first,tem);
+				iter->second->makeIncrementifOK();
+			}
+
+		
+			 
+		
+}
+
+bool FullRefinment::haveMoreUnits(){
+		 
+
+std::map<int, BaseClass*>::iterator iter;
+for (iter = this->totals.begin()
+ ; iter != this->totals.end(); ++iter)
+		 {
+			 if(iter->second->isFinished()==false)
+				 return true;
+		 }
+		 return false;	 
+}
+
+FullRefinment::~FullRefinment(){
+		
+std::map<int, BaseClass*>::iterator iter;
+for (iter = this->totals.begin()
+; iter != this->totals.end(); ++iter)
+		 {
+			 delete iter->second;
+		 }
+		 
+	 }
+/*take the rel and produce the stream of tuple*/
+int getHybridRefinmentVM( Word* args, Word& result,
+int message,Word& local, Supplier s )
+{
+
+
+  GenericRelation* r;
+  GenericRelationIterator* rit;
+  FullRefinment* li;
+  switch( message )
+  {
+    case OPEN:{
+      if(local.addr){
+        delete static_cast<FullRefinment*>(local.addr);
+        local.setAddr(0);
+      }
+      r = static_cast<GenericRelation*>(args[0].addr);
+	  rit = r->MakeScan();
+	  Tuple *t = rit->GetNextTuple();
+	  vector<string> vec;
+	  Word elem(Address(0));
+	   Supplier son ;
+	  for(int scanIndex=0;scanIndex<t->GetNoAttributes() ;scanIndex++){
+		  
+	    son = qp->GetSupplier(args[1].addr, scanIndex);
+        qp->Request(son, elem);
+        vec.push_back (((CcString*)elem.addr)->GetValue());
+	  }
+	  li = new FullRefinment(t,vec,nl->Second(GetTupleResultType(s)));
+      local.setAddr(li);
+      return 0;
+    }
+    case REQUEST:{
+      if(!local.addr){
+        return CANCEL;
+      }
+      li = static_cast<FullRefinment*>(local.addr);
+      Tuple* t = NULL;
+      if(li->haveMoreUnits()){
+        li->getNext(&t);
+        if(t){
+          result.setAddr(t);
+          return YIELD;
+        }
+      }
+      return CANCEL;
+    }
+    case CLOSE:{
+      if(local.addr){
+        delete static_cast<FullRefinment*>(local.addr);
+        local.setAddr(0);
+      }
+      return 0;
+    }
+    default:{
+      cerr << __PRETTY_FUNCTION__ << "Unknown message = " << message << "."
+           << endl;
+      return -1;
+    }
+  } // end switch
+  cerr << __PRETTY_FUNCTION__ << "Unknown message = " << message << "."
+       << endl;
+  return -1;
+
+}
+
+ValueMapping getFullRefinementValueMapping[] = {getHybridRefinmentVM};
+Operator getFullRefinement(
+  getFullRefinmentOperatorInfo,
+  getFullRefinementValueMapping,
+  getFullRefinmentSelect,
+  getFullRefinmentTypeMapping
+);
+
 class HybridTrajectoryAlgebra : public Algebra {
   public:
 	HybridTrajectoryAlgebra() : Algebra() {
@@ -735,6 +1192,7 @@ class HybridTrajectoryAlgebra : public Algebra {
 		AddOperator(&temporalatinstant);
 		AddOperator(&temporalatperiods);
 		AddOperator(&by);
+		AddOperator(&getFullRefinement);
 
 	}
 
@@ -750,6 +1208,3 @@ Algebra* InitializeHybridTrajectoryAlgebra(NestedList *nlRef,
                                              QueryProcessor *qpRef) {
   return new hyt::HybridTrajectoryAlgebra;
 }
-
-
-
