0b1fa1962ac7e5195b3387b672aba43ad20c8841
behr <>
2013-08-08 08:50:14 +0000

    Version set to 3.4.0

--- include/version.h
+++ include/version.h
@@ -9,7 +9,7 @@ release-<MAJOR><MINOR>.
 #define SEC_VERSION_H
 
 #define SECONDO_VERSION_MAJOR      3
-#define SECONDO_VERSION_MINOR      3
+#define SECONDO_VERSION_MINOR      4
 #define SECONDO_VERSION_REVISION   0
 
 #endif
