523def761f715e88627961f2a8d84e82bdce574a
floheinz <>
2013-11-27 14:50:02 +0000

    *** empty log message ***

--- Algebras/RegionInterpolation2/lua.cpp
+++ Algebras/RegionInterpolation2/lua.cpp
@@ -6,23 +6,43 @@
 #include <vector>
 #include <lua5.1/lua.hpp>
 
-static lua_State *l = NULL;
+static lua_State *L = NULL;
 
-void luaInit (void) {
-    if (l != NULL)
-        lua_close(l);
-    l = lua_open();
-    luaopen_base(l);
-    luaL_loadfile(l, "/tmp/mf.lua");
+int luaInit (void) {
+    if (L != NULL)
+        lua_close(L);
+    L = luaL_newstate();
+    luaL_openlibs(L);
+    int st = luaL_loadfile(L, "/tmp/mf.lua");
+    if (st) {
+        cerr << "Error parsing LUA-file: " << lua_tostring(L, -1) << "\n";
+        return -1;
+    }
+    
+    st = lua_pcall(L, 0, 0, 0);
+    if (st) {
+        cerr << "Error running LUA-file: " << lua_tostring(L, -1) << "\n";
+        return -1;
+    }
+    
+    return 0;
 }
 
 vector<pair<Reg *, Reg *> > _matchFacesLua(vector<Reg> *src, vector<Reg> *dst) {
     vector<pair<Reg *, Reg *> > ret;
     
-    lua_getglobal(l, "matchFaces");
-    lua_pushinteger(l, src->size());
-    lua_pushinteger(l, dst->size());
-    lua_call(l, 2, 1);
+    if (!L) {
+        int st = luaInit();
+        if (st < 0)
+            return ret;
+    }
+    
+    lua_getglobal(L, "matchFaces");
+    lua_pushinteger(L, src->size());
+    lua_pushinteger(L, dst->size());
+    cerr << "Calling LUA START\n";
+    lua_call(L, 2, 1);
+    cerr << "Calling LUA END\n";
     
     return ret;
 }
