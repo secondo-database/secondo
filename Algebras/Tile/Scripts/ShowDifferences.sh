79fcdb3093546712fc0dc1c33f596ca447625e01
zacher <>
2013-11-28 18:40:58 +0000

    *** empty log message ***

--- Algebras/Tile/Scripts/ShowDifferences.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-clear
-diff -r /home/secondo/secondo/Algebras/Tile "/media/USB STICK/Studium/Sommersemester 2013/Abschlussarbeit/Implementierung/secondo/Algebras/Tile"
-diff -r /home/secondo/secondo/Algebras/Raster2 "/media/USB STICK/Studium/Sommersemester 2013/Abschlussarbeit/Implementierung/secondo/Algebras/Raster2"
-diff -r /home/secondo/secondo/Javagui "/media/USB STICK/Studium/Sommersemester 2013/Abschlussarbeit/Implementierung/secondo/Javagui"
