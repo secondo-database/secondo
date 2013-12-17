61350827b3e896ce1c76e443233d126aec4d48ce
valdes <>
2013-12-17 11:19:25 +0000

    SecondoPostGIS Converter added to repository

---
+++ Tools/SecondoPostGIS/startup.bat
@@ -0,0 +1,2 @@
+REM Use this file to start SecondoPostGIS Converter on Windows OS
+java -Djava.util.Arrays.useLegacyMergeSort=true -XX:+AggressiveHeap -jar Hjort.jar
---
+++ Tools/SecondoPostGIS/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#Use this file to start SecondoPostGIS Converter on Linux and Mac OS
+java -Djava.util.Arrays.useLegacyMergeSort=true -XX:+AggressiveHeap -jar Hjort.jar
