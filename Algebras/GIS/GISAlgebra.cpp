9c0f6f7935f391b6cfd103c889c8f1ef6d3bf89c
jstehmann <>
2014-03-25 20:54:16 +0000

    Initial revision

---
+++ Algebras/GIS/GISAlgebra.cpp
@@ -0,0 +1,157 @@
+/*
+This file is part of SECONDO.
+
+Copyright (C) 2013, University in Hagen, Department of Computer Science,
+Database Systems for New Applications.
+      
+SECONDO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+  
+SECONDO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SECONDO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+/*
+//[_] [\_]
+//[TOC] [\tableofcontents]
+//[Title] [ \title{GIS Algebra} \author{Jana Stehmann} \maketitle]
+//[times] [\ensuremath{\times}]
+//[->] [\ensuremath{\rightarrow}]
+
+*/
+
+/*
+GIS Algebra implements operators for raster terrain analysis
+
+The following operators are available:
+
+atinstant
+atlocation
+
+*/
+
+/*
+SECONDO includes
+
+*/
+
+#include "Algebra.h"
+#include "NestedList.h"
+#include "QueryProcessor.h"
+#include "GISAlgebra.h"
+
+/*
+GISAlgebra operators includes
+
+*/
+
+#include "slope.h"
+//#include "operators/atlocation.h"
+
+/*
+extern declarations
+
+*/
+
+extern NestedList* nl;
+extern QueryProcessor* qp;
+
+/*
+Method InitializeGISAlgebra initializes global variables nl of type NestedList
+an qp of type QueryProcessor.
+
+author: Jana Stehmann
+parameters: pNestedList - a pointer to a NestedList object
+            pQueryProcessor - a pointer to global QueryProcessor object
+return value: a pointer to GIS Algebra
+exceptions: -
+
+*/
+
+extern "C" Algebra* InitializeGISAlgebra(NestedList* pNestedList,
+                                          QueryProcessor* pQueryProcessor)
+{
+  Algebra* pAlgebra = 0;
+
+  nl = pNestedList;
+  assert(nl != 0);
+
+  qp = pQueryProcessor;
+  assert(qp != 0);
+
+  pAlgebra = new GISAlgebra::GISAlgebra();
+  assert(pAlgebra != 0);
+
+  return pAlgebra;
+}
+
+/*
+declaration of namespace GISAlgebra
+
+*/
+
+namespace GISAlgebra
+{
+
+/*
+Constructor GISAlgebra initializes GIS Algebra by adding type constructors
+of GIS Algebra datatypes and by adding operators to GIS Algebra.
+
+author: Jana Stehmann
+parameters: -
+return value: -
+exceptions: -
+
+*/
+
+GISAlgebra::GISAlgebra()
+            :Algebra()
+{
+  /*
+  Type Constructors
+
+  */
+
+  // AddTypeConstructor(new TypeConstructor(tintArray::GetTypeConstructor()),
+  //                    true);
+  // AddTypeConstructor(new TypeConstructor(tintFlob::GetTypeConstructor()),
+  //                    true);
+  //AddTypeConstructor(new TypeConstructor(tgrid::GetTypeConstructor()), true);
+  
+
+  /*
+  Operators
+
+  */
+
+  AddOperator(slopeInfo(), slopeFuns, slopeSelectFun, slopeTypeMap);
+  //AddOperator(atlocationInfo(), atlocationFunctions, atlocationSelectFunction,
+  //            atlocationTypeMappingFunction);
+  
+}
+
+/*
+Destructor deinitializes GIS Algebra.
+
+author: Jana Stehmann
+parameters: -
+return value: -
+exceptions: -
+
+*/
+
+GISAlgebra::~GISAlgebra()
+{
+  
+}
+
+}
---
+++ Algebras/GIS/slope.cpp
@@ -0,0 +1,140 @@
+/*
+This file is part of SECONDO.
+
+Copyright (C) 2011, University in Hagen, Department of Computer Science,
+Database Systems for New Applications.
+
+SECONDO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SECONDO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SECONDO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+#include "slope.h"
+#include "../Raster2/sint.h"
+#include "../Raster2/sreal.h"
+
+#ifndef M_PI
+const double M_PI = acos( -1.0 );
+#endif
+
+namespace GISAlgebra {
+
+  template <typename T>
+  int slopeFun(Word* args, Word& result, 
+                     int message, Word& local, Supplier s)
+  {
+    result = qp->ResultStorage(s);
+
+    CcReal* factor = static_cast<CcReal*>(args[1].addr);
+    raster2::sint* sint_out = static_cast<raster2::sint*>(result.addr);
+    raster2::sint* sint_in = static_cast<raster2::sint*>(args[0].addr);
+
+    raster2::grid2 grid = sint_in->getGrid();
+
+    double zFactor = factor->GetValue();
+    double cellsize = grid.getLength();
+
+    sint_out->setGrid(grid);
+
+    Rectangle<2> bbox = sint_in->bbox();
+
+    raster2::RasterIndex<2> from = grid.getIndex(bbox.MinD(0), bbox.MinD(1));
+    raster2::RasterIndex<2> to = grid.getIndex(bbox.MaxD(0), bbox.MaxD(1));
+
+    for (raster2::RasterIndex<2> index=from; index < to; 
+                                             index.increment(from, to))
+    {
+	// Mitte
+        double e = sint_in->get(index);
+
+        double a = sint_in->get((int[]){index[0] - 1, index[1] + 1});
+        if (sint_in->isUndefined(a)){a = e;}
+        double b = sint_in->get((int[]){index[0], index[1] + 1});
+        if (sint_in->isUndefined(b)){b = e;}
+        double c = sint_in->get((int[]){index[0] + 1, index[1] + 1});
+        if (sint_in->isUndefined(c)){c = e;}
+        double d = sint_in->get((int[]){index[0] - 1, index[1]});
+        if (sint_in->isUndefined(d)){d = e;}
+        double f = sint_in->get((int[]){index[0] + 1, index[1]});
+        if (sint_in->isUndefined(f)){f = e;}
+        double g = sint_in->get((int[]){index[0] - 1, index[1] - 1});
+        if (sint_in->isUndefined(g)){g = e;}
+        double h = sint_in->get((int[]){index[0], index[1] - 1});
+        if (sint_in->isUndefined(h)){h = e;}
+        double i = sint_in->get((int[]){index[0] + 1, index[1] - 1});
+        if (sint_in->isUndefined(i)){i = e;}
+
+        // Delta bestimmen
+        double dzdx = ((c + 2*f + i) - (a + 2*d + g)) / (8*cellsize*zFactor);
+        double dzdy = ((g + 2*h + i) - (a + 2*b + c)) / (8*cellsize*zFactor);
+
+        // Slope berechnen
+        double slope = atan(sqrt(pow(dzdx,2) + pow(dzdy,2))) * 180/M_PI;
+
+        sint_out->set(index, slope);
+    }
+
+    return 0;
+  }
+
+  ValueMapping slopeFuns[] =
+  {
+    slopeFun<raster2::sint>,
+    slopeFun<raster2::sreal>,
+    0
+  };
+
+  int slopeSelectFun(ListExpr args)
+  {
+    int nSelection = -1;
+    
+    NList type(args);
+
+    if (type.first() == NList(raster2::sint::BasicType()))
+    {
+      nSelection = 0;
+    }
+    
+    else if (type.first() == NList(raster2::sreal::BasicType()))
+    {
+      nSelection = 1;
+    }
+
+    return nSelection;
+  }
+
+  ListExpr slopeTypeMap(ListExpr args)
+  {
+    NList type(args);
+
+    if(type.length() != 2)
+    {
+      return NList::typeError("two arguments required");
+    }
+
+    if (type == NList(raster2::sint::BasicType(), CcReal::BasicType())) 
+    {
+      return NList(raster2::sint::BasicType()).listExpr();
+    }
+
+    else if(type == NList(raster2::sreal::BasicType(), 
+                          CcReal::BasicType())) 
+    {
+      return NList(raster2::sreal::BasicType()).listExpr();
+    }
+    
+    return NList::typeError
+           ("Expecting an sint or sreal and a double (zfactor).");
+  }
+}
