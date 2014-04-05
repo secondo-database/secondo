520fa213cf9d277e100dc8a75c6edeb6106ff67b
gueting <>
2014-04-05 16:50:07 +0000

    First version of rewriting SQL queries describing atomic operations on moving objects into such operations.

---
+++ Optimizer/operatorSQL.pl
@@ -0,0 +1,153 @@
+/*
+Translation of SQL Queries into Moving Object Operators
+
+*/
+
+
+:- op(940, fx, from).
+:- op(950, xfx, where).
+:- op(960, xfx, select).
+:- op(980, xfx, =>>).
+:- op(800, xfx, atinstant).
+:- op(800, xfx, atperiods).
+:- op(800, xfx, at).
+:- op(970, fx, exists).
+:- op(800, xfx, present).
+:- op(800, xfx, passes).
+
+
+
+Join select Project =>> Term :-
+  Join =>> r(Name, Term1, Type1),
+  s(Name, Term1, Type1, Project) =>> Term.
+
+
+
+/*
+Translation of Joins
+
+For the moment, we allow to consider from the result of the join only the projection to the first argument, hence projections can later be applied only to that part of the join.
+
+*/
+
+from [X, Y] where X:t = Y:t =>> r(X, X atinstant Y, intime(T)) :-
+  type(X, moving(T)),
+  type(Y, instant).
+
+from [X, Y] where X:t = Y:t =>> r(X, X atperiods Y, moving(T)) :-
+  type(X, moving(T)),
+  type(Y, periods).
+
+from [X, Y] where X:pos = Y:pos =>> r(X, X at Y, moving(XType)) :-
+  type(X, moving(XType)),
+  type(Y, YType),
+  member(XType, [point, region]),
+  member(YType, [point, line, region]).
+
+from [X, Y] where X:v = Y:v =>> r(X, X at Y, moving(XType)) :-
+  type(X, moving(XType)),
+  type(Y, range(XType)),
+  member(XType, [int, real, bool, string]).
+
+
+
+exists (from [X, Y] where X:t = Y:t select _) =>> X present Y :-
+  type(X, moving(_)),
+  type(Y, YType),
+  member(YType, [instant, periods]).
+
+exists (from [X, Y] where X:pos = Y:pos select _) =>> X passes Y :-
+  type(X, moving(XType)),
+  type(Y, YType),
+  member(XType, [point, region]),
+  member(YType, [point, line, region]).
+
+exists (from [X, Y] where X:v = Y:v select _) =>> X passes Y :-
+  type(X, moving(XType)),
+  type(Y, range(XType)),
+  member(XType, [int, real, bool, string]).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Translation of Projections
+
+*/
+
+s(Name, Term, intime(_), Name) =>> Term.
+
+s(Name, Term, intime(_), Name:t) =>> inst(Term).
+
+s(Name, Term, intime(T), Name:pos) =>> val(Term) :-
+  member(T, [point, region]).
+
+s(Name, Term, intime(T), Name:v) =>> val(Term) :-
+  member(T, [int, real, bool, string]).
+
+
+s(Name, Term, moving(_), Name) =>> Term.
+
+s(Name, Term, moving(_), Name:t) =>> deftime(Term).
+
+s(Name, Term, moving(point), Name:pos) =>> trajectory(Term).
+
+s(Name, Term, moving(region), Name:pos) =>> traversed(Term).
+
+s(Name, Term, moving(T), Name:v) =>> rangevalues(Term) :-
+  member(T, [int, real, bool, string]).
+
+
+
+
+
+type(mi, moving(int)).
+type(mr, moving(real)).
+type(mb, moving(bool)).
+type(ms, moving(string)).
+
+type(mp, moving(point)).
+type(mre, moving(region)).
+
+type(i, instant).
+type(pe, periods).
+
+type(p, point).
+type(l, line).
+type(re, region).
+
+type(ira, range(int)).
+
+
+
+type(train7, moving(point)).
+type(mehringdamm, point).
+type(tiergarten, region).
+type(six30, instant).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
