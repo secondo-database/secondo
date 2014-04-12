8988283eedf030cfa97c08e9fc6c74633cb91f06
gueting <>
2014-04-12 18:27:52 +0000

    Added rewriting of SQL queries.

--- Optimizer/operatorSQL.pl
+++ Optimizer/operatorSQL.pl
@@ -7,6 +7,7 @@ Translation of SQL Queries into Moving Object Operators
 :- op(960, xfx, from).
 :- op(950, xfx, where).
 :- op(950, fx, select).
+:- op(930, xfy,  as).
 :- op(980, xfx, =>>).
 :- op(800, xfx, atinstant).
 :- op(800, xfx, atperiods).
@@ -16,10 +17,6 @@ Translation of SQL Queries into Moving Object Operators
 :- op(800, xfx, passes).
 
 
-
-% Join select Project =>> Term :-
-%   Join =>> r(Name, Term1, Type1),
-%   s(Name, Term1, Type1, Project) =>> Term.
 
 
 
@@ -48,31 +45,31 @@ _ from [X, Y] where X:t = Y:t =>> r(X, X atperiods Y, moving(T)) :-
 _ from [X, Y] where X:pos = Y:pos =>> r(X, X at Y, moving(XType)) :-
   type(X, moving(XType)),
   type(Y, YType),
-  member(XType, [point, region]),
-  member(YType, [point, line, region]).
+  memberchk(XType, [point, region]),
+  memberchk(YType, [point, line, region]).
 
 _ from [X, Y] where X:v = Y:v =>> r(X, X at Y, moving(XType)) :-
   type(X, moving(XType)),
   type(Y, range(XType)),
-  member(XType, [int, real, bool, string]).
+  memberchk(XType, [int, real, bool, string]).
 
 
 
 exists (get _ from [X, Y] where X:t = Y:t) =>> X present Y :-
   type(X, moving(_)),
   type(Y, YType),
-  member(YType, [instant, periods]).
+  memberchk(YType, [instant, periods]).
 
 exists (get _ from [X, Y] where X:pos = Y:pos) =>> X passes Y :-
   type(X, moving(XType)),
   type(Y, YType),
-  member(XType, [point, region]),
-  member(YType, [point, line, region]).
+  memberchk(XType, [point, region]),
+  memberchk(YType, [point, line, region]).
 
 exists (get _ from [X, Y] where X:v = Y:v) =>> X passes Y :-
   type(X, moving(XType)),
   type(Y, range(XType)),
-  member(XType, [int, real, bool, string]).
+  memberchk(XType, [int, real, bool, string]).
 
 
 
@@ -86,10 +83,10 @@ s(Name, Term, intime(_), Name) =>> Term.
 s(Name, Term, intime(_), Name:t) =>> inst(Term).
 
 s(Name, Term, intime(T), Name:pos) =>> val(Term) :-
-  member(T, [point, region]).
+  memberchk(T, [point, region]).
 
 s(Name, Term, intime(T), Name:v) =>> val(Term) :-
-  member(T, [int, real, bool, string]).
+  memberchk(T, [int, real, bool, string]).
 
 
 s(Name, Term, moving(_), Name) =>> Term.
@@ -101,7 +98,7 @@ s(Name, Term, moving(point), Name:pos) =>> trajectory(Term).
 s(Name, Term, moving(region), Name:pos) =>> traversed(Term).
 
 s(Name, Term, moving(T), Name:v) =>> rangevalues(Term) :-
-  member(T, [int, real, bool, string]).
+  memberchk(T, [int, real, bool, string]).
 
 
 
@@ -130,10 +127,82 @@ type(train7, moving(point)).
 type(mehringdamm, point).
 type(tiergarten, region).
 type(six30, instant).
+type(trip, moving(point)).
+
+
+/*
+Rewriting SQL queries.
+
+Example:
+
+select val(trip atinstant six30) as atsix30 
+from trains 
+where trip passes mehringdamm
+
+can be written as
+
+select (get trip:pos from [trip, six30] where trip:t = six30:t) as atsix30
+from trains
+where exists(get trip from [trip, mehringdamm] where trip:pos = mehringdamm:pos)
+
+
+*/
+
+moSQL(select Attrs from Rels where Preds, 
+  	select Attrs4 from Rels where Preds4) :-
+  makeList(Attrs, Attrs2),
+  moAttrs(Attrs2, Attrs3),
+  makeNoList(Attrs3, Attrs4),
+  makeList(Preds, Preds2),
+  moPreds(Preds2, Preds3),
+  makeNoList(Preds3, Preds4).
+
+moAttrs([], []).
+
+moAttrs([Attr | Attrs], [Attr2 | Attrs2]) :-
+  moAttr(Attr, Attr2),
+  moAttrs(Attrs, Attrs2).
+
+moAttr(Term as Name, Term2 as Name) :-
+  moTerm(Term, Term2),
+  !.
+
+moAttr(X, X).
 
 
 
+moTerm(X, Y) :-
+  X =>> Y,		% Translation
+  !.
 
+moTerm(X, X).
+
+
+
+moPreds([], []).
+
+moPreds([Pred | Preds], [Pred2 | Preds2]) :-
+  moPred(Pred, Pred2),
+  moPreds(Preds, Preds2).
+
+
+moPred(exists(X), Y) :-
+  exists(X) =>> Y,	% Translation
+  !.
+
+moPred(X, X).
+
+
+
+makeList(L, L) :- is_list(L).
+
+makeList(L, [L]) :- not(is_list(L)).
+
+
+makeNoList([X], X) :- 
+  !.
+
+makeNoList(L, L).
 
 
 
