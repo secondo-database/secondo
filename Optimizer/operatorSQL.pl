d8a7d9d0b495eed1fbd909a6c8ce839fb38ff688
gueting <>
2014-04-25 19:32:11 +0000

    Completed integration of operatorSQL into optimizer.

--- Optimizer/operatorSQL.pl
+++ Optimizer/operatorSQL.pl
@@ -8,7 +8,7 @@ Translation of SQL Queries into Moving Object Operators
 :- op(940, fx, exists).
 
 
-	/*
+	/* defined in Secondo
 :- op(960, xfx, from).
 :- op(950, xfx, where).
 :- op(950, fx, select).
@@ -21,12 +21,30 @@ Translation of SQL Queries into Moving Object Operators
 :- op(800, xfx, passes).
 	*/
 
+/*
+Translation of SQL queries describing moving object operators, such as
 
+----	
+    get train7:pos from [train7, six30] where train7:t = six30:t
+
+	=>> val(train7 atinstant six30)
+
+    get train7:pos from [train7, thecenter] where train7:pos = thecenter:pos
+
+	=>> trajectory(train7 at thecenter)
+
+    exists (get * from [train7, mehringdamm] where train7:pos = mehringdamm:pos)
+
+	=>> train7 passes mehringdamm
+----
+
+*/ 
 
 
 get Project from Join =>> Term :-
   _ from Join =>> r(Name, Term1, Type1),
-  s(Name, Term1, Type1, Project) =>> Term.
+  s(Name, Term1, Type1, Project) =>> Term,
+  !.
 
 
 
@@ -66,13 +84,15 @@ _ from X =>> r(X, X, XType) :-
 exists (get _ from [X, Y] where X:t = Y:t) =>> X present Y :-
   type(X, moving(_)),
   type(Y, YType),
-  memberchk(YType, [instant, periods]).
+  memberchk(YType, [instant, periods]),
+  !.
 
 exists (get _ from [X, Y] where X:pos = Y:pos) =>> X passes Y :-
   type(X, moving(XType)),
   type(Y, YType),
   memberchk(XType, [point, region]),
-  memberchk(YType, [point, line, region]).
+  memberchk(YType, [point, line, region]),
+  !.
 
 exists (get _ from [X, Y] where X:v = Y:v) =>> X passes Y :-
   type(X, moving(XType)),
@@ -112,8 +132,10 @@ s(Name, Term, moving(T), Name:v) =>> rangevalues(Term) :-
   memberchk(T, [int, real, bool, string]).
 
 
+/*
+Hard-coded types of objects used for testing.
 
-
+*/
 
 type(mi, moving(int)).
 type(mr, moving(real)).
@@ -133,34 +155,94 @@ type(re, region).
 type(ira, range(int)).
 
 
+% type(train7, moving(point)).
+% type(mehringdamm, point).
+% type(tiergarten, region).
+% type(six30, instant).
+% type(trip, moving(point)).
 
-type(train7, moving(point)).
-type(mehringdamm, point).
-type(tiergarten, region).
-type(six30, instant).
-type(trip, moving(point)).
+/*
+Determine type of database objects or attributes
+
+*/
+
+type(X, Type) :-
+  secondoCatalogInfo(X, _, _, [ShortType]),
+  fullTypeName(ShortType, Type),
+  !.
+
+type(X:Y, Type) :-	% renamed attributes
+  queryRelation(Rel, X),
+  secondoCatalogInfo(Rel, _, _, [[rel, [tuple, AttributeList]]]),
+  dcName2externalName(Rel:Y, Name),
+  checkAttr(Name, AttributeList, ShortType),
+  fullTypeName(ShortType, Type).
+
+type(Y, Type) :-	% normal attributes
+  queryRelation(Rel, *),
+  secondoCatalogInfo(Rel, _, _, [[rel, [tuple, AttributeList]]]),
+  dcName2externalName(Rel:Y, Name),
+  checkAttr(Name, AttributeList, ShortType),
+  fullTypeName(ShortType, Type).
 
 
+/*
+Check whether an attribute with a given name (external spelling) exists in an attribute list as retrieved from ~SecondoCatalogInfo~.
+
+*/
+
+checkAttr(_, [] , _) :-
+  fail.
+
+checkAttr(Name, [[Name, Type] | _], Type) :- 
+  !.
+
+checkAttr(Name, [ _ | AttrList], Type) :-
+  checkAttr(Name, AttrList, Type).
+
+/*
+Translate short type names like ~mpoint~ to long versions like moving(point).
+
+*/
+
+fullTypeName(mpoint, moving(point)) :- !.
+fullTypeName(mregion, moving(region)) :- !.
+fullTypeName(mint, moving(int)) :- !.
+fullTypeName(mreal, moving(real)) :- !.
+fullTypeName(mbool, moving(bool)) :- !.
+fullTypeName(mstring, moving(string)) :- !. 
+
+fullTypeName(X, X).
+
+
+
+
+
+ 
 /*
 Rewriting SQL queries.
 
 Example:
 
-select val(trip atinstant six30) as atsix30 
-from trains 
-where trip passes mehringdamm
+----	select [id, 
+	  (get trip:pos from [trip, six30] where trip:t = six30:t) as atsix30]
+	from trains
+	where 
+	  [exists(get * from [trip, mehringdamm] where trip:pos = mehringdamm:pos),
+  	  exists(get * from [trip, six30] where trip:t = six30:t)]
+----
 
-can be written as
+can be rewritten into
 
-select (get trip:pos from [trip, six30] where trip:t = six30:t) as atsix30
-from trains
-where exists(get trip from [trip, mehringdamm] where trip:pos = mehringdamm:pos)
-
+----	select [id, val(trip atinstant six30) as atsix30]
+	from trains where [trip passes mehringdamm, trip present six30]
+----
 
 */
 
 moSQL(select Attrs from Rels where Preds, 
   	select Attrs4 from Rels where Preds4) :-
+  prepareRels(Rels),
   makeList(Attrs, Attrs2),
   moAttrs(Attrs2, Attrs3),
   makeNoList(Attrs3, Attrs4),
@@ -171,6 +253,7 @@ moSQL(select Attrs from Rels where Preds,
 
 moSQL(select Attrs from Rels, 
   	select Attrs4 from Rels) :-
+  prepareRels(Rels),
   makeList(Attrs, Attrs2),
   moAttrs(Attrs2, Attrs3),
   makeNoList(Attrs3, Attrs4),
@@ -178,7 +261,29 @@ moSQL(select Attrs from Rels,
 
 moSQL(X, X).
 
+:- dynamic queryRelation/2.
 
+
+prepareRels(Rels) :-
+  retractall(queryRelation(_, _)),
+  makeList(Rels, Rels2),
+  prepareRels2(Rels2).
+
+prepareRels2([]).
+
+prepareRels2([Rel as X | Rels]) :-
+  !,
+  assert(queryRelation(Rel, X)),
+  prepareRels2(Rels).
+
+prepareRels2([Rel | Rels]) :-
+  assert(queryRelation(Rel, *)),
+  prepareRels2(Rels).
+  
+/*
+Translate derived attributes in select clause.
+
+*/
 
 moAttrs([], []).
 
@@ -193,21 +298,22 @@ moAttr(Term as Name, Term2 as Name) :-
 moAttr(X, X).
 
 
-
 moTerm(X, Y) :-
   X =>> Y,		% Translation
   !.
 
 moTerm(X, X).
 
+/*
+Translate operations in the where clause.
 
+*/
 
 moPreds([], []).
 
 moPreds([Pred | Preds], [Pred2 | Preds2]) :-
   moPred(Pred, Pred2),
   moPreds(Preds, Preds2).
-
 
 moPred(exists(X), Y) :-
   exists(X) =>> Y,	% Translation
@@ -216,12 +322,8 @@ moPred(exists(X), Y) :-
 moPred(X, X).
 
 
-
-
 % makeList defined elsewhere
-
 % makeList(L, L) :- is_list(L).
-
 % makeList(L, [L]) :- not(is_list(L)).
 
 
