2f23af6facb180639efb27ec3d97f8561447dd4b
gueting <>
2014-05-29 17:03:51 +0000

    Reimplemented itHashJoin and itSpatialJoin cost functions right here. Added treatment of symmjoin.

--- Optimizer/costs2014.pl
+++ Optimizer/costs2014.pl
@@ -28,38 +28,40 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 [10] Memory-Aware Cost Estimation
 
+[toc]
+
 1 The Costs of Terms
 
-----	cost(+Term, +Sel, +Pred, +Res, +Mem, -Size, -NAttrs, 
-          -TupleSize, -Cost)
+----	cost(+Term, +Sel, +Pred, +Res, +Mem, -Card, -NAttrs, 
+          -TSize, -Cost)
 ----
 
 The cost of an executable ~Term~ representing a predicate ~Pred~ with
-selectivity ~Sel~ is ~Cost~ and the size of the result (number of tuples) is ~Size~. The resulting tuples have ~NAttrs~ attributes and tuple size terms ~TupleSize~ = sizeTerm(MemoryFix, DiskCore, DiskLOB). The result will belong to node ~Res~. If the root operator of the ~Term~ uses a memory buffer, the amount of available memory is ~Mem~ (in MB).
+selectivity ~Sel~ is ~Cost~ and the cardinality of the result (number of tuples) is ~Card~. The resulting tuples have ~NAttrs~ attributes and tuple size terms ~TSize~ = sizeTerm(MemoryFix, DiskCore, DiskLOB). The result will belong to node ~Res~. If the root operator of the ~Term~ uses a memory buffer, the amount of available memory is ~Mem~ (in MB).
 
 This is evaluated recursively descending into the term. When the operator
 realizing the predicate (e.g. ~filter~) is encountered, the selectivity ~Sel~ is
-used to determine the size of the result. It is assumed that only a single operator of this kind occurs within the term.
+used to determine the size of the result. Usually only a single operator of this kind occurs within the term. However, with the filter and refine technique (used e.g. for spatial join), selectivity needs to be split into bounding-box selectivity and remaining selectivity.
 
 
 1.1 Arguments
 
 */
 
-cost(rel(Rel, _), _, _, _, _, Size, NAttrs, TupleSize, 0) :-
-  card(Rel, Size),
-  tupleSizeSplit(Rel, TupleSize),
+cost(rel(Rel, _), _, _, _, _, Card, NAttrs, TSize, 0) :-
+  card(Rel, Card),
+  tupleSizeSplit(Rel, TSize),
   getRelAttrList(Rel, OrigAttrs, _),
   length(OrigAttrs, NAttrs).
   
 
-cost(res(N), _, _, _, _, Size, NAttrs, TupleSize, 0) :-
-  resultSize(N, Size),
-  nodeTupleSize(N, TupleSize),
+cost(res(N), _, _, _, _, Card, NAttrs, TSize, 0) :-
+  resultSize(N, Card),
+  nodeTupleSize(N, TSize),
   nodeNAttrs(N, NAttrs).
 
 /*
-1.2 feed, feedproject, rename
+1.2 feed, feedproject
 
 Cost constant definitions at the end of the file.
 
@@ -69,27 +71,29 @@ Cost constant definitions at the end of the file.
 	% original constants with machine factor 3.35 multiplied by
 	% 1.33 / 3.35 = 0.4 to adapt to currrent machine
 
-cost(feed(X), Sel, Pred, Res, Mem, Size, NAttrs, TupleSize, Cost) :-
-  cost(X, Sel, Pred, Res, Mem, Size, NAttrs, TupleSize, Cost1),
+cost(feed(X), Sel, Pred, Res, Mem, Card, NAttrs, TSize, Cost) :-
+  cost(X, Sel, Pred, Res, Mem, Card, NAttrs, TSize, Cost1),
   feedC(PerTuple, PerAttr),
-  Cost is Cost1 + Size * (PerTuple + PerAttr * NAttrs).
+  Cost is Cost1 + Card * (PerTuple + PerAttr * NAttrs).
 
 
 
-% feedprojectC(0.0033, 0.0003, 0.000006).  % PerTuple, PerAttr, PerByte [ms]
-	% constants adapted to this machine, keeping relative sizes
-	% the same
+% feedprojectC(0.0033, 0.0003, 0.000006).  
+	% PerTuple, PerAttr, PerByte [ms]
+	% constants adapted to this machine, keeping relative
+	% sizes the same
 
 cost(feedproject(rel(Rel, _), Project), _, _, _, _, 
-	Size, NAttrs, TupleSize, Cost) :-
-  cost(rel(Rel, _), _, _, _, _, Size, _, _, Cost1),
+	Card, NAttrs, TSize, Cost) :-
+  cost(rel(Rel, _), _, _, _, _, Card, _, _, Cost1),
   getRelAttrList(Rel, OrigAttrs, _),
   attributes(Project, AList),
-  projectAttrList(OrigAttrs, AList, _, TupleSize),
+  projectAttrList(OrigAttrs, AList, _, TSize),
   length(AList, NAttrs),
-  TupleSize = sizeTerm(MemoryFix, _, _),
+  TSize = sizeTerm(MemoryFix, _, _),
   feedprojectC(PerTuple, PerAttr, PerByte),
-  Cost is Cost1 + Size * (PerTuple + PerAttr * NAttrs + PerByte * MemoryFix).
+  Cost is Cost1 + Card * 
+    (PerTuple + PerAttr * NAttrs + PerByte * MemoryFix).
 
 
 
@@ -133,7 +137,7 @@ Relation Roads	(Auxiliary objects:)
 
 
 	Cardinality:   735683
-	Avg.TupleSize: 1416.3 = sizeTerm(416, 291.568, 1124.74)
+	Avg.TSize: 1416.3 = sizeTerm(416, 291.568, 1124.74)
 ----
 
 Evaluating a cheap predicate:
@@ -192,23 +196,19 @@ We will proceed as follows. We set a threshold value of 0.2 ms, corresponding to
 
 % filterC(0.00111).	% PerTuple [ms]
 
-cost(filter(X, _), Sel, Pred, Res, Mem, Size, 
-	NAttrs, TupleSize, Cost) :-  
-  cost(X, 1.0, Pred, Res, Mem, SizeX, NAttrs, TupleSize, CostX),
+cost(filter(X, _), Sel, Pred, Res, Mem, Card, 
+	NAttrs, TSize, Cost) :-  
+  cost(X, 1.0, Pred, Res, Mem, Card1, NAttrs, TSize, Cost1),
   getPET(Pred, _, ExpPET),   % fetch stored predicate evaluation time
   filterC(PerTuple),
   ( (ExpPET =< 0.2) -> (PET is 0.0); PET is ExpPET),
-  Size is SizeX * Sel,
-  Cost is CostX + SizeX * (PerTuple + PET).
-
-
-
-
+  Card is Card1 * Sel,
+  Cost is Cost1 + Card1 * (PerTuple + PET).
 
 
 
 /*
-1.4 Rename
+1.4 rename
 
 Cost is linear in the number of tuples, does not depend on the number of attributes or tuple size as just one pointer for the tuple is passed to the next operator.
 
@@ -231,10 +231,10 @@ has running times 3.65, 3.58, 3.23 seconds, average 3.49. Hence the cost of rena
 
 % renameC(0.000367). 	% PerTuple
 
-cost(rename(X, _), Sel, Pred, Res, Mem, Size, NAttrs, TupleSize, Cost) :-
-  cost(X, Sel, Pred, Res, Mem, Size, NAttrs, TupleSize, Cost1),
+cost(rename(X, _), Sel, Pred, Res, Mem, Card, NAttrs, TSize, Cost) :-
+  cost(X, Sel, Pred, Res, Mem, Card, NAttrs, TSize, Cost1),
   renameC(PerTuple),
-  Cost is Cost1 + Size * PerTuple.
+  Cost is Cost1 + Card * PerTuple.
   
   
 
@@ -253,17 +253,19 @@ Iterative HashJoin reads the tuples from its first input stream into a hash tabl
 
 For the cost formula we use the following notations:
 
-  * $M$: the available memory, in MB
+  * $Mem$: the available memory, in MB
 
-  * $P$: the number of partitions where $P = \lceil(p_1.Card \cdot p_1.Size) / (M * 1024 * 1024)\rceil$.
+  * $Card_i$: the cardinalities of the first / second input stream
 
-  * $p_1.Card, p_2.Card$: the cardinalities of the first / second input stream
+  * $NAttrs_i$: the numbers of attributes of the first / second argument stream
 
-  * $p_i.Size$: the tuple sizes of the first / second input stream. Tuple sizes are taken from the ~MemoryFix~ component of the size terms.
+  * $Size_i$: the tuple sizes in memory of the first / second input stream. Tuple sizes are taken from the ~MemoryFix~ component of the ~sizeTerm~.
 
-  * $p_i.noAttrs$: the numbers of attributes of the first / second argument stream
+  * $Card$: the cardinality of the output.
 
-  * $pRes.Card$: the cardinality of the output.
+  * $NAttrs$: the number of attributes of the output
+
+  * $P$: the number of partitions where $P = \lceil(Card_1 \cdot Size_1) / (Mem * 1024 * 1024)\rceil$.
 
   * $u, v, w, x, y$: constants with the following meanings (denoted as $uHashJoin$ etc. in a global table of constants): 
 
@@ -283,123 +285,216 @@ The cost for iterative hash join then is the following:
 \begin{array}{lll}
 C_{itHashJoin}  & = & 
 \left\{ \begin{array}{ll}
-	p_1.Card \cdot u + p2.Card \cdot v & \mbox{if $P$ = 1} 	\\
-	p_1.Card \cdot u	& \mbox{otherwise}		\\
-	+ p_2.Card \cdot p_2.Size \cdot w  &  			\\
-	+ (P - 1) \cdot p_2.Card \cdot p_2.Size \cdot x &     	\\
+	Card_1 \cdot u + Card_2 \cdot v & \mbox{if $P$ = 1} 	\\
+	Card_1 \cdot u	+ Card_2 \cdot Size_2 \cdot w & \mbox{otherwise} \\
+	+ (P - 1) \cdot Card_2 \cdot Size_2 \cdot x &     	\\
 	\end{array}
 \right. \\
-& & + pRes.Card \cdot y \cdot (p_1.noAttrs + p_2.noAttrs)
+& & + Card \cdot y \cdot NAttrs
 \end{array}
 \]
 
-These costs are reflected in the following cost function provided by ~ExtRelation2AlgebraCostEstimation.h~.
+These costs are reflected in the following cost function.
 
 
 */
 
-% attrC(0.00042).	% PerAttr [ms], cost per attribute in result tuple
+% attrC(0.00042).	% Cost per attribute in result tuple. in ms.
+% itHashJoinC(0.00836, 0.09739, 0.007518, 0.00457) % see above
 
-
-cost(itHashJoin(X, Y, _, _), Sel, Pred, Res, Mem, Size, NAttrs, 
-	TupleSize, Cost) :-
-  cost(X, 1, Pred, Res, Mem, SizeX, NAttrsX, sizeTerm(MemX, _, _), CostX), 
-  cost(Y, 1, Pred, Res, Mem, SizeY, NAttrsY, sizeTerm(MemY, _, _), CostY),
-  getOpIndexes(itHashJoin, [algebra, all], _, AlId, OpId, _),
-  getCosts([AlId, OpId, 0], 
-    [SizeX, MemX, NAttrsX], [SizeY, MemY, NAttrsY], Sel, Mem, C1),
-  Size is SizeX * SizeY * Sel,
+cost(itHashJoin(Arg1, Arg2, _, _), Sel, Pred, Res, Mem, 
+	Card, NAttrs, TSize, Cost) :-
+  cost(Arg1, 1, Pred, Res, Mem, Card1, _, sizeTerm(Size1, _, _), Cost1), 
+  cost(Arg2, 1, Pred, Res, Mem, Card2, _, sizeTerm(Size2, _, _), Cost2),
+  Card is Card1 * Card2 * Sel,
   nodeNAttrs(Res, NAttrs),
-  nodeTupleSize(Res, TupleSize),
-  attrC(PerAttr),
-  C2 is Size * NAttrs * PerAttr,
-  Cost is CostX + CostY + C1 + C2.
-  
-  
-/*
-1.6 gracehashjoin
-
-Cost function used from ~ExtRelation2AlgebraCostEstimation.h~.
-
-*/
-
-cost(gracehashjoin(X, Y, _, _, _), Sel, Pred, Res, Mem, Size, NAttrs, 
-	TupleSize, Cost) :-
-  cost(X, 1, Pred, Res, Mem, SizeX, NAttrsX, sizeTerm(MemX, _, _), CostX), 
-  cost(Y, 1, Pred, Res, Mem, SizeY, NAttrsY, sizeTerm(MemY, _, _), CostY),
-  getOpIndexes(gracehashjoin, [algebra, all], _, AlId, OpId, _),
-  getCosts([AlId, OpId, 0], 
-    [SizeX, MemX, NAttrsX], [SizeY, MemY, NAttrsY], Sel, Mem, C1),
-  Size is SizeX * SizeY * Sel,
-  nodeNAttrs(Res, NAttrs),
-  nodeTupleSize(Res, TupleSize),
-  attrC(PerAttr),
-  C2 is Size * NAttrs * PerAttr,
-  Cost is CostX + CostY + C1 + C2.
-  
-/*
-1.7 hybridhashjoin
-
-Cost function used from ~ExtRelation2AlgebraCostEstimation.h~.
-
-*/
-
-cost(hybridhashjoin(X, Y, _, _, _), Sel, Pred, Res, Mem, 
-	Size, NAttrs, TupleSize, Cost) :-
-  cost(X, 1, Pred, Res, Mem, SizeX, NAttrsX, sizeTerm(MemX, _, _), CostX), 
-  cost(Y, 1, Pred, Res, Mem, SizeY, NAttrsY, sizeTerm(MemY, _, _), CostY),
-  getOpIndexes(hybridhashjoin, [algebra, all], _, AlId, OpId, _),
-  getCosts([AlId, OpId, 0], 
-    [SizeX, MemX, NAttrsX], [SizeY, MemY, NAttrsY], Sel, Mem, C1),
-  Size is SizeX * SizeY * Sel,
-  nodeNAttrs(Res, NAttrs),
-  nodeTupleSize(Res, TupleSize),
-  attrC(PerAttr),
-  C2 is Size * NAttrs * PerAttr,
-  Cost is CostX + CostY + C1 + C2.
-  
-/*
-1.8 sortmergejoin
-
-Cost function used from ~ExtRelation2AlgebraCostEstimation.h~.
-
-*/
-
-cost(sortmergejoin(X, Y, _, _), Sel, Pred, Res, Mem, Size, NAttrs, 
-	TupleSize, Cost) :-
-  cost(X, 1, Pred, Res, Mem, SizeX, NAttrsX, sizeTerm(MemX, _, _), CostX), 
-  cost(Y, 1, Pred, Res, Mem, SizeY, NAttrsY, sizeTerm(MemY, _, _), CostY),
-  getOpIndexes(sortmergejoin, [algebra, all], _, AlId, OpId, _),
-  getCosts([AlId, OpId, 0], 
-    [SizeX, MemX, NAttrsX], [SizeY, MemY, NAttrsY], Sel, Mem, C1),
-  Size is SizeX * SizeY * Sel,
-  nodeNAttrs(Res, NAttrs),
-  nodeTupleSize(Res, TupleSize),
-  attrC(PerAttr),
-  C2 is Size * NAttrs * PerAttr,
-  Cost is CostX + CostY + C1 + C2.
+  nodeTupleSize(Res, TSize),
+  P is 1 + ((Card1 * Size1) div (Mem * 1024 * 1024)), % # of partitions
+  itHashJoinC(U, V, W, X),
+  ( P = 1 
+    -> C1 is Card1 * U + Card2 * V
+    ;  C1 is Card1 * U 
+         + Card2 * Size2 * W
+         + (P - 1) * Card2 * Size2 * X
+  ),
+  attrC(PerAttr), 			% Cost depending on result size,
+  C2 is Card * NAttrs * PerAttr,	% the same for all joins
+  Cost is Cost1 + Cost2 + C1 + C2.
   
 
 /*
-1.9 itSpatialJoin
+1.5 itSpatialJoin
 
-Cost function used from ~MMRTreeAlgebraCostEstimation.h~.
+The task is to join tuples from two input streams that have overlapping bounding boxes for some attribute.
+
+This operator works in the same way as ~itHashJoin~. The only difference is that instead of a hash table, a main memory R-tree is used. Probes search with a rectangle on this R-tree. We can therefore use the same cost function, only the constants will be different.
+
+\[
+\begin{array}{lll}
+C_{itSpatialJoin}  & = & 
+\left\{ \begin{array}{ll}
+	Card_1 \cdot u + Card_2 \cdot v & \mbox{if $P$ = 1} 	\\
+	Card_1 \cdot u	+ Card_2 \cdot Size_2 \cdot w & \mbox{otherwise} \\
+	+ (P - 1) \cdot Card_2 \cdot Size_2 \cdot x &     	\\
+	\end{array}
+\right. \\
+& & + Card \cdot y \cdot NAttrs
+\end{array}
+\]
 
 */
 
-cost(itSpatialJoin(X, Y, _, _), Sel, Pred, Res, Mem, 
-	Size, NAttrs, TupleSize, Cost) :-
-  cost(X, 1, Pred, Res, Mem, SizeX, NAttrsX, sizeTerm(MemX, _, _), CostX), 
-  cost(Y, 1, Pred, Res, Mem, SizeY, NAttrsY, sizeTerm(MemY, _, _), CostY),
-  getOpIndexes(itSpatialJoin, [algebra, all], _, AlId, OpId, _),
-  getCosts([AlId, OpId, 0], 
-    [SizeX, MemX, NAttrsX], [SizeY, MemY, NAttrsY], Sel, Mem, C1),
-  Size is SizeX * SizeY * Sel,
+
+% itSpatialJoinC(0.059, 0.125, 0.006, 0.001) % see above
+
+cost(itSpatialJoin(Arg1, Arg2, _, _), Sel, Pred, Res, Mem, 
+	Card, NAttrs, TSize, Cost) :-
+  cost(Arg1, 1, Pred, Res, Mem, Card1, _, sizeTerm(Size1, _, _), Cost1), 
+  cost(Arg2, 1, Pred, Res, Mem, Card2, _, sizeTerm(Size2, _, _), Cost2),
+  Card is Card1 * Card2 * Sel,
   nodeNAttrs(Res, NAttrs),
-  nodeTupleSize(Res, TupleSize),
-  attrC(PerAttr),
-  C2 is Size * NAttrs * PerAttr,
-  Cost is CostX + CostY + C1 + C2.
+  nodeTupleSize(Res, TSize),
+  P is 1 + ((Card1 * Size1) div (Mem * 1024 * 1024)), % # of partitions
+  itSpatialJoinC(U, V, W, X),
+  ( P = 1 
+    -> C1 is Card1 * U + Card2 * V
+    ;  C1 is Card1 * U 
+         + Card2 * Size2 * W
+         + (P - 1) * Card2 * Size2 * X
+  ),
+  attrC(PerAttr), 			% Cost depending on result size,
+  C2 is Card * NAttrs * PerAttr,	% the same for all joins
+  Cost is Cost1 + Cost2 + C1 + C2.
+  
+
+
+/*
+1.7 symmjoin
+
+Symmjoin is the fallback method for join as it is able to process an arbitrary join predicate. It is a symmetric version of a nested-loop join, considering all pairs of tuples from the two argument streams and evaluating the predicate for them. It works roughly as follows:
+
+  1 For the two input streams, initialize two buffers ~A~ and ~B~ as empty.
+
+  2 While not both input streams are exhausted, do:
+
+    1 If stream 1 is not empty, take the first tuple $t_1$ from stream 1 and put it into buffer ~A~. For each tuple $t_2$ in buffer ~B~ evaluate the predicate on the pair $(t_1, t_2)$.
+
+    2 If stream 2 is not empty, take the first tuple $t_2$ from stream 2 and put it into buffer ~B~. For each tuple $t_1$ in buffer ~A~ evaluate the predicate on the pair $(t_1, t_2)$.
+
+Hence the procedure alternates between the two input streams and therefore is called symmetric. One advantage is that there is no blocking time as for standard nested loop which first loads one entire argument into the buffer. Instead, result tuples can be reported immediately.
+
+Obviously the cost is quadratic, or more precisely, the product of the argument sizes. When the available memory is exceeded, there is some change in cost as tuples need to be written to disk. However, we do not need to model this in the cost function, as the quadratic behaviour dominates the cost. Except in rare cases, ~symmjoin~ will only be used if no other join technique is available, because it is so expensive.
+
+Similar as for filter, we need to distinguish between cheap and expensive predicates. Again we introduce a threshold cost and add the cost for evaluating the predicate only if the measured cost exceeds the threshold.
+
+Let ~ExpPET~ denote the predicate evaluation time measured in the selectivity query and ~th~ the threshold that determines whether we have an expensive predicate. The constant ~u~ denotes the cost for processing one pair of tuples with a cheap predicate.
+
+The cost function is:
+
+\[
+\begin{array}{lll}
+PET & = &
+\left\{ \begin{array}{ll}
+	ExpPET  & \mbox{if $ExpPET > th$} 	\\
+	0 & \mbox{otherwise} \\
+	\end{array}
+\right. \\
+%
+& & \\
+%
+C_{symmjoin}  & = & 
+Card_1 * Card_2 * (u + PET) \\
+& & + Card \cdot y \cdot NAttrs
+\end{array}
+\]
+
+1.7.1 Determining Constants
+
+We use database ~opt~.
+
+----	let plzEven = plz feed filter[(.PLZ mod 2) = 0] consume
+
+	let plzOdd = plz feed filter[(.PLZ mod 2) = 1] consume
+----
+
+The two relations ~plzEven~ and ~plzOdd~ have cardinalities 19773 and 21494, respectively. The query 
+
+----	query plzEven feed {p1} plzOdd feed {p2} 
+	itHashJoin[PLZ_p1, PLZ_p2] count
+----
+
+has result 0 and runs in 0.12 seconds.
+
+----	query plzEven feed head[A] {p1} 
+	plzOdd feed head[B] {p2} 
+	symmjoin[.PLZ_p1 = ..PLZ_p2] {0.00000001, 0.01} count
+----
+
+for all parameters A and B has result 0 and has running times:
+
+B = 1000:
+
+  * A = 1000: 3.04 sec
+
+  * A = 2000: 6.05 sec
+
+  * A = 3000: 9.07 sec
+
+  * A = 4000: 12.05 sec
+
+B = 2000:
+
+  * A = 1000: 6.05 sec
+
+  * A = 2000: 12.07 sec
+
+  * A = 3000: 18.12 sec
+
+  * A = 4000: 24.19 sec
+
+One can observe the product cost very well. Clearly the cost is 3 secs per million pairs processed, hence the constant ~u~ is $u = 3000 / 1000000$ = 0.003 ms.
+
+To determine the cost for an expensive predicate, we use the database berlintest.
+
+----	query Trains feed {t1} Trains feed {t2} 
+	symmjoin[.Id_t1 = ..Id_t2] count
+----
+
+The result is 562, the number of trains, and the running times are 0.99, 0.96, 0.97 secs.The prediction using the constant above is 562 * 562 * 0.003 = 947.532 ms which is close.
+
+----	query Trains feed head[200] {t1} Trains feed head[B] {t2} 
+	symmjoin[sometimes(distance(.Trip_t1, ..Trip_t2) < 50.0)] count
+----
+
+  * B =  50: 10.67 sec
+
+  * B = 100: 16.08
+
+  * B = 150: 23.58
+
+  * B = 200: 36.93
+
+The cost per predicate evaluation is about 8 secs per 10000 pairs, that is, 0.8 ms.
+
+For this predicate, the optimizer determines in the sample query a cost of 0.76 ms. For a cheap predicate like comparing two numbers it is 0.0035. We use a threshold of 0.01 ms to distinguish an expensive predicate.
+
+*/
+% symmjoinC(0.003).	% Cost per tuple pair for cheap preds in ms
+
+cost(symmjoin(Arg1, Arg2, _), Sel, Pred, Res, Mem, 
+	Card, NAttrs, TSize, Cost) :-
+  cost(Arg1, 1, Pred, Res, Mem, Card1, _, _, Cost1), 
+  cost(Arg2, 1, Pred, Res, Mem, Card2, _, _, Cost2),
+  Card is Card1 * Card2 * Sel,
+  nodeNAttrs(Res, NAttrs),
+  nodeTupleSize(Res, TSize),
+  symmjoinC(U),
+  getPET(Pred, _, ExpPET),   % fetch stored predicate evaluation time
+  ( (ExpPET =< 0.01) -> (PET is 0.0); PET is ExpPET),
+  C1 is Card1 * Card2 * (U + PET),
+  attrC(PerAttr), 			% Cost dep. on result size,
+  C2 is Card * NAttrs * PerAttr,	% the same for all joins
+  Cost is Cost1 + Cost2 + C1 + C2.
 
 
 
@@ -414,7 +509,8 @@ feedC(0.0011, 0.0004). 	% PerTuple, PerAttr [ms]
 	% original constants with machine factor 3.35 multiplied by
 	% 1.33 / 3.35 = 0.4 to adapt to currrent machine
 
-feedprojectC(0.0033, 0.0003, 0.000006).  % PerTuple, PerAttr, PerByte [ms]
+feedprojectC(0.0033, 0.0003, 0.000006). 
+	% PerTuple, PerAttr, PerByte [ms]
 	% constants adapted to this machine, keeping relative sizes
 	% the same
 
@@ -422,7 +518,14 @@ filterC(0.00111).	% PerTuple [ms]
 
 renameC(0.000367). 	% PerTuple
 
-attrC(0.00042).	% PerAttr [ms], cost per attribute in result tuple
+itHashJoinC(0.00836, 0.09739, 0.007518, 0.00457).
+	% u, v, w, x constants for itHashJoin, see above
+
+itSpatialJoinC(0.059, 0.125, 0.006, 0.001).
+
+symmjoinC(0.003).
+
+attrC(0.00012).	% PerAttr [ms], cost per attribute in result tuple
 
 
 
