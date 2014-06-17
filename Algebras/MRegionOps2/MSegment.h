8a938fb62c78dbbeda5773384c7d5e7e6436cdbc
schroeer <>
2014-06-17 14:41:53 +0000

    implementation create intersection segments

--- Algebras/MRegionOps2/MSegment.h
+++ Algebras/MRegionOps2/MSegment.h
@@ -1,6 +1,43 @@
 /*
+----
+This file is part of SECONDO.
 
-*/ 
+Copyright (C) 2013, University in Hagen, 
+Faculty of Mathematics and Computer Science,
+Database Systems for New Applications.
+
+SECONDO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SECONDO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SECONDO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+----
+
+//paragraph [1] Title: [{\Large \bf \begin {center}] [\end {center}}]
+//[TOC] [\tableofcontents]
+//[ue] [\"u]
+//[ae] [\"a]
+//[oe] [\"o]
+
+[1] File Refinement2.h
+
+This file contains classes and functions for use within the operators intersection and inside
+
+[TOC]
+
+1 Helper classes and methods
+
+1.1 Some forward declaration of helper methods
+
+*/
 
 #ifndef MSEGMENT_H_
 #define MSEGMENT_H_
