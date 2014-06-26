c4d866ab8144ce7775f8ce42f338512cafd83765
nidzwetzki <>
2014-06-26 09:22:29 +0000

    Improved start cassandra script

--- Algebras/Cassandra/tools/start_cassandra.sh
+++ Algebras/Cassandra/tools/start_cassandra.sh
@@ -7,15 +7,36 @@
 #
 #######################################
 
+
 # Cassandra Nodes
 nodes="node1 node2 node3 node4 node5 node6"
 
 # Cassandra binary
-cassandrabin="/opt/psec/nidzwetzki/cassandra/apache-cassandra-2.0.7/bin/cassandra"
+cassandradir="/opt/psec/nidzwetzki/cassandra/apache-cassandra-2.0.7"
 
 
 for node in $nodes; do
+   
+   echo -n "Starting Cassandra on Node $node " 
+   ssh $node "source .secondorc; $cassandradir/bin/cassandra > /dev/null" &
+   echo "  [ Done ]"
 
-   ssh $node $cassandrabin &
+done
 
+echo -e "\n\n\n\n\n"
+
+echo "Wait for cassandra nodes to become ready...."
+sleep 5
+
+while [ true ]; do
+ 
+ $cassandradir/bin/nodetool ring
+
+ ring=$($cassandradir/bin/nodetool ring)
+
+ if [ $(echo $ring | grep Down | wc -l) -eq 0 ]; then
+    break
+ fi
+ 
+ sleep 5;
 done
--- Algebras/Cassandra/tools/stop_cassandra.sh
+++ Algebras/Cassandra/tools/stop_cassandra.sh
@@ -16,6 +16,8 @@ cassandrabin="/opt/cassandra/apache-cassandra-2.0.7/bin/cassandra"
 
 for node in $nodes; do
 
-   ssh $node "ps ux | grep cassandra | grep -v grep | grep -v stop_cassandra | awk {'print \$2'} | xargs kill"
+   echo -n "Killing Cassandra on node $node"
+   ssh $node "ps ux | grep cassandra | grep -v grep | grep -v stop_cassandra | awk {'print \$2'} | xargs kill 2> /dev/null"
+   echo "  [ Done ]"
 
 done
