18e008c0248f3ef6ffe66b3a94c9edc1194c2040
nidzwetzki <>
2014-09-13 08:38:43 +0000

    Fixed comment

--- Algebras/Cassandra/CassandraAlgebra.cpp
+++ Algebras/Cassandra/CassandraAlgebra.cpp
@@ -32,7 +32,7 @@
  0 Overview
 
  This file contains an algebra with 11 operators. With these operators
- you can interact with cassandra. With cspread can export SECONDO
+ you can interact with cassandra. With cspread you can export SECONDO
  tuples into cassandra, with ccollect you can import these tuples back 
  into SECONDO. Also, the algebra contains operators for distributed
  query processing like cqueryexecute and cquerywait.
