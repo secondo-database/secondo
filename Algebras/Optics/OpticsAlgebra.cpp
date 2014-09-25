3041d118b5c58fa746c017ee09e49a75c4e5bac9
mhaug <>
2014-09-25 17:51:59 +0000

    *** empty log message ***

--- Algebras/Optics/OpticsAlgebra.cpp
+++ Algebras/Optics/OpticsAlgebra.cpp
@@ -47,6 +47,8 @@ This file contains the implementation of the OpticsAlgebra.
 #include "Optics.h"
 #include "StandardTypes.h"
 #include "SpatialAlgebra.h"
+#include "MMRTreeAlgebra.h"
+#include "MMRTree.h"
 
 #include <limits.h>
 #include <float.h>
@@ -86,10 +88,10 @@ Struct ~opticsResult~ to save the ordering of algorithm.
   bool hasNext() { return init && (it != tupleIds->end()); }
  };
 /*
-Type mapping method ~opticsType~
+Type mapping method ~opticsTM~
 
 */
- ListExpr opticsType( ListExpr args )
+ ListExpr opticsTM( ListExpr args )
  {
   if(!DistfunReg::isInitialized())
   {
@@ -184,10 +186,10 @@ Type mapping method ~opticsType~
      ,newAttrList)));
  }
 /*
-Value mapping method ~opticsFun~
+Value mapping method ~opticsVM~
 
 */
- int opticsFun(Word* args, Word& result, int message, Word& local, Supplier s)
+ int opticsVM(Word* args, Word& result, int message, Word& local, Supplier s)
  {
   opticsResult* info = (opticsResult*) local.addr;
 
@@ -206,7 +208,7 @@ Value mapping method ~opticsFun~
     string config;
         
     //Optics instance
-    Optics optics;
+    Optics<0> optics;
 
     Tuple* tup;
     TupleType* resultTupleType;
@@ -284,7 +286,9 @@ Value mapping method ~opticsFun~
      tp->AppendTuple(newTuple);
      tup->DeleteIfAllowed();
     }
-            
+    
+    stream.close();
+    
     if(info)
     {
      delete info;
@@ -340,6 +344,242 @@ Value mapping method ~opticsFun~
   return 0;
  }
 /*
+Type mapping method ~opticsRTM~
+
+*/
+ ListExpr opticsRTM( ListExpr args )
+ {    
+  if(nl->ListLength(args)!=2)
+  {
+   ErrorReporter::ReportError("two element expected");
+   return nl->TypeError();
+  }
+
+  ListExpr stream = nl->First(args);
+
+  if(!Stream<Tuple>::checkType(nl->First(args)))
+  {
+     return listutils::typeError("stream(Tuple) expected");
+  }
+
+  //Check the arguments
+  ListExpr arguments = nl->Second(args);
+
+  if(nl->ListLength(arguments)!=3)
+  {
+   ErrorReporter::ReportError("non conform list (three arguments expected)");
+   return nl->TypeError();
+  }
+  
+  if(!CcReal::checkType(nl->Second(arguments)))
+  {
+     return listutils::typeError("arg2 is not a real (Eps)");
+  }
+  
+  if(!CcInt::checkType(nl->Third(arguments)))
+  {
+     return listutils::typeError("arg3 is not an int (MinPts)");
+  }
+  
+  //Check the attribute name, is it in the tuple list
+  ListExpr attrList = nl->Second(nl->Second(stream));
+  ListExpr attrType;
+  string attrName = nl->SymbolValue(nl->First(nl->Second(args)));
+  int found = FindAttribute(attrList, attrName, attrType);
+  if(found == 0)
+  {
+   ErrorReporter::ReportError("Attribute "
+    + attrName + " is not a member of the tuple");
+   return nl->TypeError();
+  }
+  
+  if( !Rectangle<2>::checkType(attrType)
+   && !Rectangle<3>::checkType(attrType)
+   && !Rectangle<4>::checkType(attrType)
+   && !Rectangle<8>::checkType(attrType) )
+  {
+     return listutils::typeError("Attribute " + attrName + " not of type " 
+      + Rectangle<2>::BasicType() + ", " 
+      + Rectangle<3>::BasicType() + ", " 
+      + Rectangle<4>::BasicType() + " or " 
+      + Rectangle<8>::BasicType() );
+  }
+  
+  //Copy attrlist to newattrlist
+  attrList             = nl->Second(nl->Second(stream));
+  ListExpr newAttrList = nl->OneElemList(nl->First(attrList));
+  ListExpr lastlistn   = newAttrList;
+  
+  attrList = nl->Rest(attrList);
+  
+  while(!(nl->IsEmpty(attrList)))
+  {
+     lastlistn = nl->Append(lastlistn,nl->First(attrList));
+     attrList = nl->Rest(attrList);
+  }
+
+  lastlistn = nl->Append(lastlistn
+   ,nl->TwoElemList(nl->SymbolAtom("CoreDist")
+    ,nl->SymbolAtom(CcReal::BasicType())));
+  lastlistn = nl->Append(lastlistn
+   ,nl->TwoElemList(nl->SymbolAtom("ReachDist")
+    ,nl->SymbolAtom(CcReal::BasicType())));
+  lastlistn = nl->Append(lastlistn
+   ,nl->TwoElemList(nl->SymbolAtom("Processed")
+    ,nl->SymbolAtom(CcBool::BasicType())));
+  lastlistn = nl->Append(lastlistn
+   ,nl->TwoElemList(nl->SymbolAtom("Eps")
+    ,nl->SymbolAtom(CcReal::BasicType())));
+
+   return nl->ThreeElemList(nl->SymbolAtom(Symbol::APPEND())
+   ,nl->OneElemList(nl->IntAtom(found-1))
+   ,nl->TwoElemList(nl->SymbolAtom(Symbol::STREAM())
+   ,nl->TwoElemList(nl->SymbolAtom(Tuple::BasicType())
+     ,newAttrList)));
+ }
+/*
+Value mapping method ~opticsRVM~
+
+*/
+ template <int dim>
+ int opticsRVM(Word* args, Word& result, int message, Word& local, Supplier s)
+ {
+  opticsResult* info = (opticsResult*) local.addr;
+
+  switch (message)
+  {
+   case OPEN :
+   {
+    //Default values
+    double defEps = 0.0;
+    int defMinPts = 0;
+    int attrCnt   = 0;
+    int idxData   = -1;
+    int minLeafs  = 2;
+    int maxLeafs  = 8;
+        
+    //Optics instance
+    Optics<dim> optics;
+
+    Tuple* tup;
+    TupleType* resultTupleType;
+    ListExpr resultType;
+    TupleBuffer* tp;
+    long maxMem;
+    Word argument;
+    Supplier son;
+    
+    qp->Open(args[0].addr);
+    
+    //set the result type of the tuple
+    resultType = GetTupleResultType(s);
+    resultTupleType = new TupleType(nl->Second(resultType));
+    Stream<Tuple> stream(args[0]);
+    
+    //set the given eps
+    son = qp->GetSupplier(args[1].addr, 1);
+    qp->Request(son, argument);
+    defEps = ((CcReal*)argument.addr)->GetRealval();
+    
+    //set the given minPts
+    son = qp->GetSupplier(args[1].addr, 2);
+    qp->Request(son, argument);
+    defMinPts = ((CcInt*)argument.addr)->GetIntval();
+    
+    //set the index of the attribute in the tuple
+    idxData = static_cast<CcInt*>(args[2].addr)->GetIntval();
+    
+    mmrtree::RtreeT<dim, TupleId> rtree(minLeafs, maxLeafs);
+    
+    stream.open();
+    
+    maxMem = qp->FixedMemory();
+    tp = new TupleBuffer(maxMem);
+    
+    while((tup = stream.request()) != 0)
+    {
+     Tuple *newTuple = new Tuple(resultTupleType);
+     
+     //Copy data from given tuple to the new tuple
+     attrCnt = tup->GetNoAttributes();
+     for( int i = 0; i < attrCnt; i++ ) 
+     {
+      newTuple->CopyAttribute( i, tup, i);
+     }
+ 
+     //Initialize the result tuple with default values
+     CcReal coreDist(-1.0);
+     newTuple->PutAttribute( attrCnt, ((Attribute*) &coreDist)->Clone());
+     CcReal reachDist(-1.0);
+     newTuple->PutAttribute( attrCnt+1, ((Attribute*) &reachDist)->Clone());
+     CcBool processed(true, false);
+     newTuple->PutAttribute( attrCnt+2, ((Attribute*) &processed)->Clone());
+     CcReal eps(defEps);
+     newTuple->PutAttribute( attrCnt+3, ((Attribute*) &eps)->Clone());
+     
+     tp->AppendTuple(newTuple);
+     tup->DeleteIfAllowed();
+    }
+    
+    stream.close();
+    
+    if(info)
+    {
+     delete info;
+    }
+    
+    info = new opticsResult(tp);
+
+    GenericRelationIterator* relIter = tp->MakeScan();
+    Tuple* obj;
+    
+    while((obj = relIter->GetNextTuple()))
+    {
+     TupleId objId = relIter->GetTupleId();
+     Rectangle<dim>* attr = (Rectangle<dim>*) obj->GetAttribute(idxData);
+     
+     if(attr->IsDefined())
+     {
+      rtree.insert(*attr, objId);
+     }
+     
+     obj->DeleteIfAllowed();
+    }
+    
+    optics.initialize(&rtree, tp, idxData, attrCnt, attrCnt+1, attrCnt+2);
+    
+    //Start the optics ordering
+    optics.order(defEps, defMinPts, info->tupleIds);
+    
+    info->initialize();
+    local.setAddr(info);
+    
+    return 0;
+   }
+   case REQUEST :
+   {
+    if(info->hasNext())
+    {
+     TupleId tid = info->next();
+     Tuple* outTup = info->buffer->GetTuple(tid, false);
+     outTup->DeleteIfAllowed();
+     result = SetWord(outTup);
+     return YIELD;
+    }
+    else
+    {
+     return CANCEL;
+    }
+   }
+   case CLOSE :
+   {
+    return 0;
+   }
+  }
+  
+  return 0;
+ }
+/*
 Struct ~opticsInfo~
 
 */
@@ -355,6 +595,61 @@ Struct ~opticsInfo~
    }
  };
 /*
+Struct ~opticsInfoR~
+
+*/
+ struct opticsInfoR : OperatorInfo
+ {
+   opticsInfoR() : OperatorInfo()
+   {
+     name      = "opticsR";
+     signature = "stream(Tuple) -> stream(Tuple)";
+     syntax    = "_ optics [list]";
+     meaning   = "Order points to identify the cluster structure";
+     example   = "query Kneipen feed extend[B : bbox(.GeoData)]" 
+"opticsR[B, 1000.0, 5] consume";
+   }
+ };
+/*
+Selection method ~opticsRRecSL~
+
+*/
+ int opticsRRecSL(ListExpr args)
+ {
+  ListExpr attrList = nl->Second(nl->Second(nl->First(args)));
+  ListExpr attrType;
+  string attrName = nl->SymbolValue(nl->First(nl->Second(args)));
+  int found = FindAttribute(attrList, attrName, attrType);
+  assert(found > 0);
+   
+  if(Rectangle<2>::checkType(attrType))
+  {
+   return 0;
+  }
+  else if(Rectangle<3>::checkType(attrType))
+  {
+   return 1;
+  }
+  else if(Rectangle<4>::checkType(attrType))
+  {
+   return 2;
+  }
+  else if(Rectangle<8>::checkType(attrType))
+  {
+   return 3;
+  }
+   
+  return -1;
+ }
+ 
+ ValueMapping opticsRRecVM[] = 
+ {
+    opticsRVM<2>
+   ,opticsRVM<3>
+   ,opticsRVM<4>
+   ,opticsRVM<8>
+ };
+/*
 Algebra class ~ClusterOpticsAlgebra~
 
 */
@@ -363,7 +658,8 @@ Algebra class ~ClusterOpticsAlgebra~
   public:
    ClusterOpticsAlgebra() : Algebra()
    {
-    AddOperator(opticsInfo(), opticsFun, opticsType);
+    AddOperator(opticsInfo(), opticsVM, opticsTM);
+    AddOperator(opticsInfoR(), opticsRRecVM, opticsRRecSL, opticsRTM);
    }
 
    ~ClusterOpticsAlgebra() {};
