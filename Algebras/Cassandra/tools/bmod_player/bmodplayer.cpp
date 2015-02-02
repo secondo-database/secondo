bf7e8b0e914cbe3e7fdb98968bbb824554dcc530
nidzwetzki <>
2015-02-02 14:27:58 +0000

    Updated bmodplayer

--- Algebras/Cassandra/tools/bmod_player/bmodplayer.cpp
+++ Algebras/Cassandra/tools/bmod_player/bmodplayer.cpp
@@ -30,6 +30,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -51,6 +54,90 @@ struct Statistics {
    size_t discarded; 
 };
 
+class InputData {
+   
+public:   
+    size_t moid;
+    size_t tripid;
+    tm time;
+    float x;
+    float y;
+};
+
+
+void handleCSVLine(vector<InputData*> &data, vector<std::string> &lineData) {
+   InputData *inputdata1 = new InputData;
+   InputData *inputdata2 = new InputData;
+   
+   data.push_back(inputdata1);
+   data.push_back(inputdata2);
+   
+   inputdata1 -> moid = atoi(lineData[0].c_str());
+   inputdata2 -> moid = atoi(lineData[0].c_str());
+   
+   inputdata1 -> tripid = atoi(lineData[1].c_str());
+   inputdata2 -> tripid = atoi(lineData[1].c_str());
+   
+   cout << lineData[2] << endl;
+   
+   // 2007-06-08 08:32:26.781
+   struct tm tm1;
+   struct tm tm2;
+   
+   if (! strptime(lineData[2].c_str(), "%Y-%m-%d %H:%M:%S.", &tm1)) {
+      cerr << "Unable to parse start date: " << lineData[2] << endl;
+   }
+   
+   if (! strptime(lineData[3].c_str(), "%Y-%m-%d %H:%M:%S.", &tm2)) {
+      cerr << "Unable to parse end date: " << lineData[3] << endl;
+   }
+   
+   inputdata1 -> time = tm1;
+   inputdata2 -> time = tm2;
+   
+   inputdata1 -> x = atof(lineData[4].c_str());
+   inputdata1 -> y = atof(lineData[5].c_str());
+   
+   inputdata2 -> x = atof(lineData[6].c_str());
+   inputdata2 -> y = atof(lineData[7].c_str());
+}
+
+bool parseInputData(string &filename, vector<InputData*> &data) {
+   
+   if( access( filename.c_str(), F_OK ) == -1 ) {
+      cerr << "Unable to open Input file: " << filename << endl;
+      return false;
+   }
+   
+   string line;
+   ifstream myfile(filename.c_str());
+   
+   if (! myfile.is_open()) {
+      cerr << "Unable to open file: " << filename << endl;
+      return false;
+   }
+   
+   while ( getline (myfile,line) ) {
+          
+      vector<std::string> lineData;
+      stringstream lineStream(line);
+      string cell;
+
+      while(getline(lineStream,cell,',')) {
+         lineData.push_back(cell);
+      }
+                    
+      if(lineData.size() != 8) {
+         cerr << "Invalid line: " << line << " skipping" << endl;
+         continue;
+      }
+          
+      handleCSVLine(data, lineData);
+   }
+   
+   myfile.close();
+   return true;
+}
 
 void printHelpAndExit(char *progName) {
    cerr << "Usage: " << progName << " -i <inputfile>";
@@ -103,6 +190,15 @@ int main(int argc, char *argv[]) {
    
    parseParameter(argc, argv, configuration);
    
+   vector<InputData*> inputData;
+   
+   bool result = parseInputData(configuration->inputfile, inputData);
+   
+   if(! result) {
+      cerr << "Unable to parse input data" << endl;
+      exit(-1);
+   }
+   
    for(size_t i = 0; i < 100; i++) {
       cout << "\r\033[2K" << "Sec: " << i;
       cout << " \033[1m Trips:\033[0m " << statistics -> trips;
@@ -120,6 +216,16 @@ int main(int argc, char *argv[]) {
    if(configuration != NULL) {
       delete configuration;
       configuration = NULL;
+   }
+   
+   while(! inputData.empty()) {
+      InputData *entry = inputData.back();
+      inputData.pop_back();
+      
+      if(entry != NULL) {
+         delete entry;
+         entry = NULL;
+      }
    }
    
    return EXIT_SUCCESS;
