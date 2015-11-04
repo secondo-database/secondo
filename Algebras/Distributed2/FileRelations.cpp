f1e991b3669b951bd85a645fa279feef1dd4248c
gueting <>
2015-11-04 11:25:52 +0000

    edit includes.

--- Algebras/Distributed2/FileRelations.cpp
+++ Algebras/Distributed2/FileRelations.cpp
@@ -32,6 +32,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
 
+#include <boost/thread.hpp>
+#include <boost/date_time.hpp>
 #include "FileRelations.h"
 boost::mutex nlparsemtx;
 
