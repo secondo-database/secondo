47ff18cd249ba0cedf7ca25bee18c14577c0e16f
behr <>
2016-08-04 12:27:25 +0000

    set to version 4.0.0

--- include/version.h
+++ include/version.h
@@ -8,8 +8,8 @@ release-<MAJOR><MINOR>.
 #ifndef SEC_VERSION_H
 #define SEC_VERSION_H
 
-#define SECONDO_VERSION_MAJOR      3
-#define SECONDO_VERSION_MINOR      4
+#define SECONDO_VERSION_MAJOR      4
+#define SECONDO_VERSION_MINOR      0
 #define SECONDO_VERSION_REVISION   0
 
 #endif
