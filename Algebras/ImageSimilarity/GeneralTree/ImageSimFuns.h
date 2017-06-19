8c5d377fcadacbdb3851fa19bc956f9444f4b90a
mloris <>
2017-06-19 18:19:19 +0000

    *** empty log message ***

--- Algebras/ImageSimilarity/GeneralTree/ImageSimFuns.h
+++ Algebras/ImageSimilarity/GeneralTree/ImageSimFuns.h
@@ -78,11 +78,40 @@ double euclidDist(FeatureSignatureTuple ist1,
 	return sqrt(tmpRes);	
 }
 
-// ground distance used by SQFD
-static double f_s(FeatureSignatureTuple fst1, FeatureSignatureTuple fst2)
+double euclidDist(FeatureSignatureTuple ist1,
+                    FeatureSignatureTuple ist2)
+{
+	double tmpRes = 0.0;
+	
+	tmpRes += pow(ist1.weight - ist2.weight, 2);
+	tmpRes += pow(ist1.centroid.x - ist2.centroid.x, 2);
+	tmpRes += pow(ist1.centroid.y - ist2.centroid.y, 2);
+	tmpRes += pow(ist1.centroid.r - ist2.centroid.r, 2);
+	tmpRes += pow(ist1.centroid.g - ist2.centroid.g, 2);
+	tmpRes += pow(ist1.centroid.b - ist2.centroid.b, 2);
+	tmpRes += pow(ist1.centroid.coarseness 
+				- ist2.centroid.coarseness, 2);
+	tmpRes += pow(ist1.centroid.contrast - ist2.centroid.contrast, 2);
+	return tmpRes;	
+}
+
+// similarity function used by SQFD
+static double f_s(FeatureSignatureTuple fst1, 
+            FeatureSignatureTuple fst2)
 {	
 	double res = 1.0 / (1.0 + euclidDist(fst1, fst2));	
 	return res;
+}
+
+
+static double f_s_gauss(FeatureSignatureTuple fst1, 
+            FeatureSignatureTuple fst2)
+{	
+    const double e = std::exp(1.0);
+    const double alpha  = -0.9;
+    return pow(e, alpha * euclidDistSquare(fst1, fst2));
+	//double res = 1.0 / (1.0 + euclidDist(fst1, fst2));	
+	//return res;
 }
 
 
