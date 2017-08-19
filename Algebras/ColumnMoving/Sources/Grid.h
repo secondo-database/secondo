2b91085c9a091eadca7fd8ff96ca24a6845f4964
jkalz <>
2017-08-19 14:50:35 +0000

    add

--- Algebras/ColumnMoving/Sources/Grid.h
+++ Algebras/ColumnMoving/Sources/Grid.h
@@ -24,84 +24,341 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #pragma once
 
-#include "SimpleAttrArray.h"
-#include "StandardTypes.h"
+#include "Array.h"
 
-namespace ColumnMovingAlgebra
-{
-  class BoolEntry
-  {
+namespace ColumnMovingAlgebra {
+
+  template<int dim, int blockSize, class Value>
+  class Grid {
   public:
-    enum Values { UNDEFINED, DEFINED_FALSE, DEFINED_TRUE };
-    
-    typedef CcBool AttributeType;
 
-    static const bool isPrecise = true;
+    struct Mbr {
+      double l[dim], h[dim];
+      Mbr unite(const Mbr & b);
+    };
 
-    BoolEntry()
-    {
-    }
+    struct PositionMbr {
+      int l[dim], h[dim];
+      bool operator != (const PositionMbr & b) const;
+    };
 
-    BoolEntry(Values value) :
-      m_Value(value)
-    {
-    }
+    struct IVector {
+      int p[dim];
+      IVector() = default;
+      IVector(PositionMbr & pmbr);
+      bool operator != (const IVector & b) const;
+    };
 
-    BoolEntry(const CcBool &value) :
-      m_Value(value.IsDefined() ? 
-        (value.GetValue() ? DEFINED_TRUE : DEFINED_FALSE) : 
-        UNDEFINED)
-    {
-    }
+    struct DVector {
+      double s[dim];
+    };
 
-    bool IsDefined() const
-    {
-      return m_Value != UNDEFINED;
-    }
+    Grid(IVector splits, DVector cellSize);
+    Grid(CRelAlgebra::Reader& source);
 
-    int Compare(const BoolEntry &value) const
-    {
-      int d = this->m_Value - value.m_Value;
-      if (d != 0)
-        return d < 0 ? -1 : 1;
-        
-      return 0;
-    }
+    void save(CRelAlgebra::Writer &target);
+    int savedSize();
 
-    int Compare(const CcBool &value) const
-    {
-      BoolEntry b(value);
-      return Compare(b);
-    }
+    void clear();
+    void addRow();
+    void removeRow();
+    void add(Mbr mbr, Value value);
+    set<Value> selection(Mbr mbr);
+    Mbr mbr();
 
-    bool Equals(const BoolEntry &value) const
-    {
-      return Compare(value) == 0;
-    }
+  private:
+    struct Block {
+      int nextBlock;
+      int count;
+      Value entries[blockSize];
+    };
 
-    bool Equals(const CcBool &value) const
-    {
-      return Compare(value) == 0;
-    }
+    Array<Block> m_Blocks;
+    DVector m_CellSize;
+    IVector m_Splits;
+    Array<int> m_Log;
+    Array<int> m_Row;
+    Mbr m_Mbr;
+    bool m_MbrValid = false;
 
-    size_t GetHash() const
-    {
-      return m_Value;
-    }
-
-    CcBool *GetAttribute(bool clone = true) const
-    {
-      return new CcBool(m_Value != UNDEFINED, m_Value == DEFINED_TRUE);
-    }
-
-    operator bool() const
-    {
-      return m_Value == DEFINED_TRUE;
-    }
-
-    Values m_Value;
+    void translateMbrToPositionMbr(Mbr & mbr, PositionMbr & positionMbr);
+    int blockIndex(IVector position);
+    void createSelection(PositionMbr & pmbr, IVector & position, int iDim, 
+                         set<Value> & selection);
+    void add(PositionMbr & pmbr, IVector & position, int iDim, Value & value);
   };
 
-  typedef CRelAlgebra::SimpleFSAttrArray<BoolEntry> Bools;
-  typedef CRelAlgebra::SimpleFSAttrArrayIterator<BoolEntry> BoolsIterator;
+
+
+  template<int dim, int blockSize, class Value>
+  inline typename Grid<dim, blockSize, Value>::Mbr 
+  Grid<dim, blockSize, Value>::Mbr::unite(const Mbr & b)
+  {
+    Mbr r;
+
+    for (int i = 0; i < dim; i++) {
+      r.l[i] = min(l[i], b.l[i]);
+      r.h[i] = max(h[i], b.h[i]);
+    }
+
+    return r;
+  }
+
+
+
+  template<int dim, int blockSize, class Value>
+  inline bool Grid<dim, blockSize, Value>::PositionMbr::operator!=(
+    const PositionMbr & b) const
+  {
+    for (int i = 0; i < dim; i++)
+      if (l[i] != b.l[i] || h[i] != b.h[i])
+        return false;
+
+    return true;
+  }
+
+
+
+  template<int dim, int blockSize, class Value>
+  inline bool Grid<dim, blockSize, Value>::IVector::operator!=(
+    const IVector & b) const
+  {
+    for (int i = 0; i < dim; i++)
+      if (p[i] != b.p[i])
+        return false;
+
+    return true;
+  }
+
+
+
+  template<int dim, int blockSize, class Value>
+  inline Grid<dim, blockSize, Value>::IVector::IVector(PositionMbr & pmbr)
+  {
+    for (int i = 0; i < dim; i++)
+      p[i] = pmbr.l[i];
+  }
+
+
+
+
+  template<int dim, int blockSize, class Value>
+  inline Grid<dim, blockSize, Value>::Grid(IVector splits, DVector cellSize) :
+    m_CellSize(cellSize),
+    m_Splits(splits)
+  {
+    int cells = 1;
+    for (int i = 0; i < dim; i++) {
+      cells *= m_Splits.p[i];
+      m_Mbr.l[i] = m_Mbr.h[i] = 0.0;
+    }
+    
+    m_Blocks.resize(cells);
+    for (int i = 0; i < cells; i++) {
+      m_Blocks[i].nextBlock = -1;
+      m_Blocks[i].count = 0;
+    }
+  }
+
+  template<int dim, int blockSize, class Value>
+  inline Grid<dim, blockSize, Value>::Grid(CRelAlgebra::Reader& source)
+  {
+    m_Blocks.load(source);
+    source.ReadOrThrow(reinterpret_cast<char*>(&m_Splits), 
+                       sizeof(m_Splits));
+    source.ReadOrThrow(reinterpret_cast<char*>(&m_CellSize), 
+                       sizeof(m_CellSize));
+    source.ReadOrThrow(reinterpret_cast<char*>(&m_Mbr), 
+                       sizeof(m_Mbr));
+    m_Row.load(source);
+    m_Log.load(source);
+  }
+
+  template<int dim, int blockSize, class Value>
+  inline void Grid<dim, blockSize, Value>::save(CRelAlgebra::Writer &target)
+  {
+    m_Blocks.save(target);
+    target.WriteOrThrow(reinterpret_cast<char*>(&m_Splits), 
+                        sizeof(m_Splits));
+    target.WriteOrThrow(reinterpret_cast<char*>(&m_CellSize), 
+                        sizeof(m_CellSize));
+    target.WriteOrThrow(reinterpret_cast<char*>(&m_Mbr), 
+                        sizeof(m_Mbr));
+    m_Row.save(target);
+    m_Log.save(target);
+  }
+
+  template<int dim, int blockSize, class Value>
+  inline int Grid<dim, blockSize, Value>::savedSize() 
+  {
+    return	m_Blocks.savedSize() +
+        sizeof(m_Splits) +
+        sizeof(m_CellSize) +
+        sizeof(m_Mbr) +
+        m_Row.savedSize() +
+        m_Log.savedSize();
+  }
+
+  template<int dim, int blockSize, class Value>
+  inline void Grid<dim, blockSize, Value>::clear()
+  {
+    m_Blocks.clear();
+    m_Log.clear();
+    m_Row.clear();
+    m_MbrValid = false;
+  }
+  
+  template<int dim, int blockSize, class Value>
+  inline void Grid<dim, blockSize, Value>::add(Mbr mbr, Value value)
+  {
+    if (!m_MbrValid)
+      m_Mbr = mbr;
+    else
+      m_Mbr = m_Mbr.unite(mbr);
+
+    m_MbrValid = true;
+
+    PositionMbr pmbr;
+    translateMbrToPositionMbr(mbr, pmbr);
+    IVector p(pmbr);
+    add(pmbr, p, 0, value);
+  }
+
+  template<int dim, int blockSize, class Value>
+  inline void Grid<dim, blockSize, Value>::addRow()
+  {
+    m_Row.push_back(m_Log.size());
+  }
+
+  template<int dim, int blockSize, class Value>
+  inline void Grid<dim, blockSize, Value>::removeRow()
+  {
+    while (static_cast<int>(m_Log.size()) > m_Row.back()) {
+      int bi = m_Log.back();
+      Block & b = m_Blocks[bi];
+
+      if (b.count > 1) {
+        b.count--;
+      } else {
+        assert(b.nextBlock == static_cast<int>(m_Blocks.size()) - 1);
+        b = m_Blocks[b.nextBlock];
+        m_Blocks.pop_back();
+      }
+
+      m_Log.pop_back();
+    }
+
+    m_Row.pop_back();
+  }
+
+  template<int dim, int blockSize, class Value>
+  inline set<Value> Grid<dim, blockSize, Value>::selection(Mbr mbr)
+  {
+    PositionMbr pmbr;
+    translateMbrToPositionMbr(mbr, pmbr);
+
+    IVector p = IVector();
+    set<Value> s;
+    createSelection(pmbr, p, 0, s);
+
+    return s;
+  }
+
+  template<int dim, int blockSize, class Value>
+  inline typename Grid<dim, blockSize, Value>::Mbr 
+  Grid<dim, blockSize, Value>::mbr()
+  {
+    return m_Mbr;
+  }
+
+  template<int dim, int blockSize, class Value>
+  inline void Grid<dim, blockSize, Value>::translateMbrToPositionMbr(
+    Mbr & mbr, PositionMbr & positionMbr)
+  {
+    for (int i = 0; i < dim; i++) {
+      int s = static_cast<int>(mbr.l[i] / m_CellSize.s[i]);
+      int e = static_cast<int>(mbr.h[i] / m_CellSize.s[i]);
+      
+      positionMbr.l[i] = (s % m_Splits.p[i] + m_Splits.p[i]) % m_Splits.p[i];
+
+      for (int j = 0; j < m_Splits.p[i] - 1 && s != e; j++)
+        s++;
+
+      positionMbr.h[i] = (s % m_Splits.p[i] + m_Splits.p[i]) % m_Splits.p[i];
+    }
+  }
+
+  template<int dim, int blockSize, class Value>
+  inline int Grid<dim, blockSize, Value>::blockIndex(IVector position)
+  {
+    int p = 0;
+    for (int i = 0; i < dim; i++)
+      p = p * m_Splits.p[i] + position.p[i];
+
+    return p;
+  }
+
+  template<int dim, int blockSize, class Value>
+  inline void Grid<dim, blockSize, Value>::createSelection(PositionMbr & pmbr, 
+    IVector & position, int iDim, set<Value> & selection)
+  {
+    if (iDim < dim) {
+      position.p[iDim] = pmbr.l[iDim];
+      do {
+        createSelection(pmbr, position, iDim + 1, selection);
+
+        if (position.p[iDim] == pmbr.h[iDim])
+          return;
+
+        position.p[iDim] = (position.p[iDim] + 1) % m_Splits.p[iDim];
+      } while (true);
+    } else {
+      int iB = blockIndex(position);
+
+      while (iB > -1) {
+        Block & b = m_Blocks[iB];
+
+        for (int i = 0; i < b.count; i++)
+          selection.insert(b.entries[i]);
+
+        iB = b.nextBlock;
+      }
+    }
+  }
+
+  template<int dim, int blockSize, class Value>
+  inline void Grid<dim, blockSize, Value>::add(PositionMbr & pmbr, 
+    IVector & position, int iDim, Value & value)
+  {
+    if (iDim < dim) {
+      position.p[iDim] = pmbr.l[iDim]; 
+      do {
+        add(pmbr, position, iDim + 1, value);
+
+        if (position.p[iDim] == pmbr.h[iDim])
+          return;
+
+        position.p[iDim] = (position.p[iDim] + 1) % m_Splits.p[iDim];
+      } while (true);
+    } else {
+      int iB = blockIndex(position);
+      Block & b = m_Blocks[iB];
+
+      if (b.count > 0 && (b.entries[b.count - 1]) == value)
+        return;
+
+      if (b.count == blockSize) {
+        m_Blocks.push_back(b);
+        Block & nb = m_Blocks[iB];
+        nb.count = 1;
+        nb.entries[0] = value;
+        nb.nextBlock = m_Blocks.size() - 1;
+      } else {
+        b.entries[b.count] = value;
+        b.count++;
+      }
+
+      m_Log.push_back(iB);
+    }
+  }
 }
