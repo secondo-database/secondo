7a19c298ab59bf4ca9526612225d2118808e320a
jkalz <>
2017-08-19 15:05:11 +0000

    add

--- Algebras/ColumnMoving/Sources/MPoints.h
+++ Algebras/ColumnMoving/Sources/MPoints.h
@@ -24,84 +24,183 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #pragma once
 
-#include "SimpleAttrArray.h"
-#include "StandardTypes.h"
+#include <memory>
+#include "AttrArray.h"
+#include "Ints.h"
+#include "DefTimes.h"
+#include "Bools.h"
+#include "IPoints.h"
+#include "MPointsData.h"
+#include "Grid.h"
 
 namespace ColumnMovingAlgebra
 {
-  class BoolEntry
+  class MPoints : public CRelAlgebra::AttrArray
   {
   public:
-    enum Values { UNDEFINED, DEFINED_FALSE, DEFINED_TRUE };
+    MPoints();
+    MPoints(CRelAlgebra::Reader& source);
+    MPoints(CRelAlgebra::Reader& source, size_t rowsCount);
+    MPoints(const MPoints &array, 
+            const CRelAlgebra::SharedArray<const size_t> &filter);
+    virtual ~MPoints() { }
+
+    virtual AttrArray* Filter(CRelAlgebra::SharedArray<const size_t> 
+                              filter) const;
+
+    virtual size_t GetCount() const;
+    virtual size_t GetSize() const;
+    virtual Attribute *GetAttribute(size_t row, bool clone = true) const;
+
+    virtual void Save(CRelAlgebra::Writer &target, 
+                      bool includeHeader = true) const;
+
+    virtual void Append(const CRelAlgebra::AttrArray & array, size_t row);
+    virtual void Append(Attribute & value);
+    virtual void Remove();
+    virtual void Clear();
+
+    virtual bool IsDefined(size_t row) const;
+
+    virtual int  Compare(size_t rowA, const AttrArray& arrayB, 
+                               size_t rowB) const;
+    virtual int  CompareAlmost(size_t rowA, const AttrArray &arrayB, 
+                               size_t rowB) const;
+    virtual bool Equals(size_t rowA, const AttrArray &arrayB, 
+                               size_t rowB) const;
+    virtual bool EqualsAlmost(size_t rowA, const AttrArray &arrayB, 
+                               size_t rowB) const;
+
+    virtual int  Compare(size_t row, Attribute &value) const;
+    virtual int  CompareAlmost(size_t row, Attribute &value) const;
+    virtual bool Equals(size_t row, Attribute &value) const;
+    virtual bool EqualsAlmost(size_t row, Attribute &value) const;
+
+    virtual size_t GetHash(size_t row) const;
+
+    void present(Instant instant, CRelAlgebra::LongInts & result);
+    void present(temporalalgebra::Periods periods, 
+                 CRelAlgebra::LongInts & result);
+    void atInstant(Instant instant, IPoints & result);
+    void atPeriods(temporalalgebra::Periods periods, MPoints & result);
+    void passes(Point & value, CRelAlgebra::LongInts & result);
+    void passes(Region & value, CRelAlgebra::LongInts & result);
+    void at(Point & value, MPoints & result);
+    void at(Region & value, MPoints & result);
+    void addRandomRows(CcInt& size, MPoints & result);
+    void index(CcInt& temporalSplits, CcInt& spatialSplits, MPoints & result);
     
-    typedef CcBool AttributeType;
+    void addRow();
+    void addUnit(Interval interval, double x0, double y0, 
+                                    double x1, double y1);
 
-    static const bool isPrecise = true;
+  private:
+    struct GridEntry {
+      int row, unitId;
+      bool operator==(const GridEntry & b) const;
+      bool operator< (const GridEntry & b) const;
+    };
 
-    BoolEntry()
-    {
-    }
+    typedef Grid<3, 31, GridEntry> GridIndex;
 
-    BoolEntry(Values value) :
-      m_Value(value)
-    {
-    }
-
-    BoolEntry(const CcBool &value) :
-      m_Value(value.IsDefined() ? 
-        (value.GetValue() ? DEFINED_TRUE : DEFINED_FALSE) : 
-        UNDEFINED)
-    {
-    }
-
-    bool IsDefined() const
-    {
-      return m_Value != UNDEFINED;
-    }
-
-    int Compare(const BoolEntry &value) const
-    {
-      int d = this->m_Value - value.m_Value;
-      if (d != 0)
-        return d < 0 ? -1 : 1;
-        
-      return 0;
-    }
-
-    int Compare(const CcBool &value) const
-    {
-      BoolEntry b(value);
-      return Compare(b);
-    }
-
-    bool Equals(const BoolEntry &value) const
-    {
-      return Compare(value) == 0;
-    }
-
-    bool Equals(const CcBool &value) const
-    {
-      return Compare(value) == 0;
-    }
-
-    size_t GetHash() const
-    {
-      return m_Value;
-    }
-
-    CcBool *GetAttribute(bool clone = true) const
-    {
-      return new CcBool(m_Value != UNDEFINED, m_Value == DEFINED_TRUE);
-    }
-
-    operator bool() const
-    {
-      return m_Value == DEFINED_TRUE;
-    }
-
-    Values m_Value;
+    shared_ptr<MPointsData> m_MPointsData;
+    shared_ptr<DefTimes> m_DefTimes;
+    GridIndex::Mbr m_Mbr;
+    shared_ptr<GridIndex> m_GridIndex;
   };
 
-  typedef CRelAlgebra::SimpleFSAttrArray<BoolEntry> Bools;
-  typedef CRelAlgebra::SimpleFSAttrArrayIterator<BoolEntry> BoolsIterator;
+
+
+
+  inline MPoints::MPoints() :
+    m_MPointsData(make_shared<MPointsData>()),
+    m_DefTimes(make_shared<DefTimes>())
+  {
+  }
+
+  inline MPoints::MPoints(CRelAlgebra::Reader& source)
+  {
+    m_MPointsData = make_shared<MPointsData>(source);
+    m_DefTimes = make_shared<DefTimes>(source);
+    source.ReadOrThrow(reinterpret_cast<char*>(&m_Mbr), sizeof(m_Mbr));
+
+    bool hasGrid;
+    source.ReadOrThrow(reinterpret_cast<char*>(&hasGrid), sizeof(bool));
+    if (hasGrid)
+      m_GridIndex = make_shared<GridIndex>(source);
+  }
+
+  inline MPoints::MPoints(CRelAlgebra::Reader& source, size_t rowsCount)
+  {
+    m_MPointsData = make_shared<MPointsData>(source);
+    m_DefTimes = make_shared<DefTimes>(source);
+    source.ReadOrThrow(reinterpret_cast<char*>(&m_Mbr), sizeof(m_Mbr));
+
+    bool hasGrid;
+    source.ReadOrThrow(reinterpret_cast<char*>(&hasGrid), sizeof(bool));
+    if (hasGrid)
+      m_GridIndex = make_shared<GridIndex>(source);
+  }
+
+  inline MPoints::MPoints(const MPoints &array, 
+    const CRelAlgebra::SharedArray<const size_t> &filter) :
+    AttrArray(filter),
+    m_MPointsData(array.m_MPointsData),
+    m_DefTimes(array.m_DefTimes),
+    m_GridIndex(array.m_GridIndex)
+  {
+  }
+
+  inline void MPoints::addRow()
+  {
+    m_MPointsData->addRow();
+    m_DefTimes->addRow();
+
+    if (m_GridIndex.get() != 0)
+      m_GridIndex->addRow();
+  }
+
+  inline void MPoints::addUnit(Interval interval, double x0, double y0, 
+                                                  double x1, double y1)
+  {
+    int unitId;
+    m_MPointsData->addUnit(interval, x0, y0, x1, y1, unitId);
+
+    m_DefTimes->addInterval(interval);
+
+    GridIndex::Mbr mbr;
+    mbr.l[0] = min(x0, x1);
+    mbr.h[0] = max(x0, x1);
+    mbr.l[1] = min(y0, y1);
+    mbr.h[1] = max(y0, y1);
+    mbr.l[2] = static_cast<double>(interval.s);
+    mbr.h[2] = static_cast<double>(interval.e);
+    
+    GridEntry e;
+    e.row = m_MPointsData->rowCount() - 1;
+    e.unitId = unitId;
+
+    if (m_GridIndex.get() != 0)
+      m_GridIndex->add(mbr, e);
+
+    if (m_MPointsData->rowCount() == 1 && m_MPointsData->unitCount(0) == 0)
+      m_Mbr = mbr;
+    else
+      m_Mbr = m_Mbr.unite(mbr);
+  }
+
+  inline bool MPoints::GridEntry::operator==(const GridEntry & b) const
+  {
+    return row == b.row && unitId == b.unitId;
+  }
+
+  inline bool MPoints::GridEntry::operator< (const GridEntry & b) const
+  {
+    if (row < b.row)
+      return true;
+    if (row > b.row)
+      return false;
+    
+    return unitId < b.unitId;
+  }
 }
