0c8553fe25f802b3f7c32cd5610110b9d19c4a93
mloris <>
2017-09-05 09:19:53 +0000

    *** empty log message ***

--- Algebras/ImageSimilarity/GeneralTree/ImageSimFuns.h
+++ Algebras/ImageSimilarity/GeneralTree/ImageSimFuns.h
@@ -40,79 +40,42 @@ June 2017, Michael Loris
 #include <cmath>
 
 
-// round to a given precision in order to prevent C++ rounding errors
-double fiveRound(double src, int precision) {
-         int dst;
-         double tmp;
-         double result;
-         tmp = src * pow(10, precision);
-         if(tmp < 0) 
-         {
-            dst = (int)(tmp - 0.5);
-         }
-         else 
-         {
-            dst = (int)(tmp + 0.5);
-         }
-         result = (double)((double)dst * pow(10, -precision));
-         return result;
-}
 
-
-// specialized euclidean distance for PCT feature signatures
-// P = position, C = color, T= texture
-double euclidDist(FeatureSignatureTuple ist1,
+double euclidDistance(FeatureSignatureTuple ist1,
                     FeatureSignatureTuple ist2)
 {
 	double tmpRes = 0.0;
+    
+	tmpRes += std::pow(ist1.centroid.x - ist2.centroid.x, 2);
+	tmpRes += std::pow(ist1.centroid.y - ist2.centroid.y, 2);
+	tmpRes += std::pow(ist1.centroid.colorValue1 
+    - ist2.centroid.colorValue1, 2);
+	tmpRes += std::pow(ist1.centroid.colorValue2 
+    - ist2.centroid.colorValue2, 2);
+	tmpRes += std::pow(ist1.centroid.colorValue3 
+    - ist2.centroid.colorValue3, 2);
+	tmpRes += std::pow(ist1.centroid.coarseness 
+    - ist2.centroid.coarseness, 2);
+	tmpRes += std::pow(ist1.centroid.contrast 
+    - ist2.centroid.contrast, 2);
 	
-	tmpRes += pow(ist1.weight - ist2.weight, 2);
-	tmpRes += pow(ist1.centroid.x - ist2.centroid.x, 2);
-	tmpRes += pow(ist1.centroid.y - ist2.centroid.y, 2);
-	tmpRes += pow(ist1.centroid.r - ist2.centroid.r, 2);
-	tmpRes += pow(ist1.centroid.g - ist2.centroid.g, 2);
-	tmpRes += pow(ist1.centroid.b - ist2.centroid.b, 2);
-	tmpRes += pow(ist1.centroid.coarseness 
-				- ist2.centroid.coarseness, 2);
-	tmpRes += pow(ist1.centroid.contrast - ist2.centroid.contrast, 2);
-	return sqrt(tmpRes);	
+	return std::sqrt(tmpRes);	
 }
 
-double euclidDist(FeatureSignatureTuple ist1,
-                    FeatureSignatureTuple ist2)
+
+// heuristic similarity function
+double f_s(FeatureSignatureTuple fst1,
+                    FeatureSignatureTuple fst2)
 {
-	double tmpRes = 0.0;
-	
-	tmpRes += pow(ist1.weight - ist2.weight, 2);
-	tmpRes += pow(ist1.centroid.x - ist2.centroid.x, 2);
-	tmpRes += pow(ist1.centroid.y - ist2.centroid.y, 2);
-	tmpRes += pow(ist1.centroid.r - ist2.centroid.r, 2);
-	tmpRes += pow(ist1.centroid.g - ist2.centroid.g, 2);
-	tmpRes += pow(ist1.centroid.b - ist2.centroid.b, 2);
-	tmpRes += pow(ist1.centroid.coarseness 
-				- ist2.centroid.coarseness, 2);
-	tmpRes += pow(ist1.centroid.contrast - ist2.centroid.contrast, 2);
-	return tmpRes;	
-}
-
-// similarity function used by SQFD
-static double f_s(FeatureSignatureTuple fst1, 
-            FeatureSignatureTuple fst2)
-{	
-	double res = 1.0 / (1.0 + euclidDist(fst1, fst2));	
+	const double alpha = 2.0;
+	double eucDist = euclidDistance(fst1, fst2);		
+			
+	double res = 1.0 /  (alpha + eucDist);
 	return res;
 }
 
 
-static double f_s_gauss(FeatureSignatureTuple fst1, 
-            FeatureSignatureTuple fst2)
-{	
-    const double e = std::exp(1.0);
-    const double alpha  = -0.9;
-    return pow(e, alpha * euclidDistSquare(fst1, fst2));
-	//double res = 1.0 / (1.0 + euclidDist(fst1, fst2));	
-	//return res;
-}
+
 
 
 // structure to express flows used in EMD
