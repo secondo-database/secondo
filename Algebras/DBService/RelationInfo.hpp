6ddc5b6a75cb42dcbf7abe4b8c8e88adfc9cc1d0
behr <>
2017-11-07 09:05:37 +0000

    removed non-needed include

--- Algebras/DBService/RelationInfo.hpp
+++ Algebras/DBService/RelationInfo.hpp
@@ -31,7 +31,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #ifndef ALGEBRAS_DBSERVICE_RELATIONINFO_HPP_
 #define ALGEBRAS_DBSERVICE_RELATIONINFO_HPP_
 
-#include <map>
 #include <string>
 #include <vector>
 
