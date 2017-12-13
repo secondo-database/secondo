d680a1125c9f2fab607ae533f9e71c42cb3cd05d
behr <>
2017-12-13 15:56:11 +0000

    Added include

--- Algebras/Distributed2/Timeout.h
+++ Algebras/Distributed2/Timeout.h
@@ -31,6 +31,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "Messages.h"
 #include "NestedList.h"
 
+#include <boost/thread/thread.hpp>
 
 namespace distributed2
 {
