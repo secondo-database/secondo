f9ff6c97b920e175170badd3d90b6d2e5edb8344
behr <>
2017-12-13 15:57:27 +0000

    added include

--- Algebras/Distributed2/DArray.h
+++ Algebras/Distributed2/DArray.h
@@ -31,6 +31,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #define DARRAY_H
 
 #include <string>
+#include <boost/thread/recursive_mutex.hpp>
 #include "NestedList.h"
 #include "Algebras/Relation-C++/RelationAlgebra.h"
 
