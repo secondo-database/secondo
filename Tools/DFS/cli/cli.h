1952b63cd77c2b015050f93226b1715377e62cbe
sscheide <>
2018-02-24 15:48:46 +0000

    initial checkin

--- Tools/DFS/cli/cli.h
+++ Tools/DFS/cli/cli.h
@@ -43,58 +43,59 @@ using namespace dfs;
 
 class Duration {
 private:
-    timespec c;
+  timespec c;
 public:
 
-    void start() {
-        clock_gettime(CLOCK_REALTIME,&c);
-    }
+  void start() {
+    clock_gettime(CLOCK_REALTIME, &c);
+  }
 
-    double measureMS() {
-        timespec stopped;
-        clock_gettime(CLOCK_REALTIME,&stopped);
-        return ( stopped.tv_sec - c.tv_sec )+ ( stopped.tv_nsec - c.tv_nsec )
-                                              / BILLION;
-    }
+  double measureMS() {
+    timespec stopped;
+    clock_gettime(CLOCK_REALTIME, &stopped);
+    return (stopped.tv_sec - c.tv_sec) + (stopped.tv_nsec - c.tv_nsec)
+                                         / BILLION;
+  }
 
 };
 
 struct cliException : public std::exception {
 private:
-    char* msg;
+  char *msg;
 public:
-    cliException(const Str& s) {
-        this->msg = s.cstr();
-    }
-    ~cliException() throw() {
-        delete[] msg;
-    }
-    virtual const char* what() { return this->msg;}
+  cliException(const Str &s) {
+    this->msg = s.cstr();
+  }
+
+  ~cliException() throw() {
+    delete[] msg;
+  }
+
+  virtual const char *what() { return this->msg; }
 };
 
-void die(const Str& s) {
-    throw cliException(s);
+void die(const Str &s) {
+  throw cliException(s);
 }
 
-void line(const Str& s) {
-    cout << s << endl;
+void line(const Str &s) {
+  cout << s << endl;
 }
 
 void line() {
-    cout << endl;
+  cout << endl;
 }
 
-void debug(const Str& s) {
-    cout << Str("DEBUG cli ").append(s) << endl;
+void debug(const Str &s) {
+  cout << Str("DEBUG cli ").append(s) << endl;
 }
 
-void debugc(const Str& cmd, const Str& s) {
-    cout << Str("DEBUG cli ").append(cmd).append(" ").append(s) << endl;
+void debugc(const Str &cmd, const Str &s) {
+  cout << Str("DEBUG cli ").append(cmd).append(" ").append(s) << endl;
 }
 
-int randInt(int min, int max)
-{
-    numberUtils::randInt(max,min);
+int randInt(int min, int max) {
+  numberUtils::randInt(max, min);
 }
 
 #endif /* CLI_H */
