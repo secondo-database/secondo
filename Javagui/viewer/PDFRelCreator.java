56a590ca5a727cb4d1fb19c32a9c28d2ecd50253
behr <>
2018-03-07 15:58:44 +0000

    added possibility to import jpg images

--- Javagui/viewer/PDFRelCreator.java
+++ Javagui/viewer/PDFRelCreator.java
@@ -6,6 +6,7 @@ package viewer;
 import gui.SecondoObject;
 import gui.ViewerControl;
 import tools.Base64Encoder;
+import tools.Reporter;
 
 import sj.lang.*;
 import java.io.*;
@@ -14,7 +15,13 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+
+enum FORMAT{PDF,JPG}
 
 
 public class PDFRelCreator extends SecondoViewer {
@@ -27,6 +34,10 @@ private static StringBuffer errorMessage = new StringBuffer();
 private static boolean haltOnError = true;
 private static BufferedReader in;
 
+
+private ButtonGroup formatSelection;
+private JCheckBox pdfFormat;
+private JCheckBox jpgFormat;
 private JTextField relationName;
 private JTextField databaseName;
 private JFileChooser fileChooser;
@@ -37,7 +48,21 @@ private JCheckBox subdirCB;
 public PDFRelCreator(){
   setSize(680,420);
   Frame mainFrame = VC==null?null:VC.getMainFrame();
-  setLayout(new GridLayout(6,1));
+  setLayout(new GridLayout(7,1));
+
+  formatSelection = new ButtonGroup();
+  pdfFormat = new JCheckBox("pdf");
+  jpgFormat = new JCheckBox("jpg");
+  formatSelection.add(pdfFormat);
+  formatSelection.add(jpgFormat);
+
+  pdfFormat.setSelected(true);
+  JPanel formatPanel = new JPanel();
+  formatPanel.add(new JLabel("Format"));
+  formatPanel.add(pdfFormat);
+  formatPanel.add(jpgFormat);
+  add(formatPanel);
+  pdfFormat.setSelected(true);
 
   relationName = new JTextField(30);
   JPanel relNamePanel = new JPanel();
@@ -84,7 +109,7 @@ public PDFRelCreator(){
   add(startPanel);
   startBtn.addActionListener(new ActionListener(){
      public void actionPerformed(ActionEvent e){
-        PDFRelCreator.this.start();
+        PDFRelCreator.this.start(getSelectedFormat());
      }
    }); 
 
@@ -114,31 +139,14 @@ boolean isSymbol(String value){
   return true;
 }
 
-ListExpr getRelType(){
 
-  ListExpr attrList = ListExpr.twoElemList(
-                        ListExpr.twoElemList( 
-                            ListExpr.symbolAtom("File"),
-                            ListExpr.symbolAtom("text")),
-                        ListExpr.twoElemList( 
-                            ListExpr.symbolAtom("Content"),
-                            ListExpr.twoElemList(
-                                  ListExpr.symbolAtom("document"),
-                                  ListExpr.symbolAtom("pdf"))));
 
-  return  ListExpr.twoElemList(
-             ListExpr.symbolAtom("rel"),
-             ListExpr.twoElemList(
-                ListExpr.symbolAtom("tuple"),
-                attrList));
-}
-
-boolean checkType(ListExpr type){
-  ListExpr expected = getRelType();
+boolean checkType(ListExpr type, FORMAT format){
+  ListExpr expected = getRelType(format);
   return expected.equals(type);
 }
 
-boolean checkTypeAsText(ListExpr theText){
+boolean checkTypeAsText(ListExpr theText, FORMAT format){
 
   if(theText.listLength()!=2){
     return false;
@@ -152,10 +160,12 @@ boolean checkTypeAsText(ListExpr theText){
   if(type.readFromString(text)!=0){
     return false;
   }
-  return checkType(type);
+  return checkType(type,format);
 }
 
-private void insertFiles(Stack<File> stack, File[] files, boolean sd){
+
+private void insertFiles(Stack<File> stack, File[] files, boolean sd, FORMAT format){
+  String ext = getExtension(format);
   for(int i=0;i<files.length;i++){
      if(files[i].isDirectory()) {
       if(sd){
@@ -166,7 +176,7 @@ private void insertFiles(Stack<File> stack, File[] files, boolean sd){
       }
      } else {
         String name = files[i].getName().toLowerCase();
-        if(name.endsWith(".pdf")){
+        if(name.endsWith(ext)){
           stack.push(files[i]);
         }
      }
@@ -194,7 +204,9 @@ String getBase64(String fileName){
 
 }
 
-void start(){
+
+
+void start( FORMAT format){
   if(VC==null){
      JOptionPane.showMessageDialog(this, "Internal error, ViewerControl not set");
      return;
@@ -243,7 +255,7 @@ void start(){
        present = true;
      } 
      if(present){
-       if(!checkTypeAsText(resultList)){
+       if(!checkTypeAsText(resultList,format)){
           JOptionPane.showMessageDialog(this, "Relation exists with different schema");
           return;
        } else {
@@ -254,7 +266,7 @@ void start(){
     }
   }
   if(!present){ // create new Relation
-     cmd = "let " + relName + " = [const rel(tuple([File : text, Content : document(pdf)])) value ()]";
+     cmd = "let " + relName + " = " + emptyRelString(format);
      VC.execCommand(cmd, errorCode, resultList , errorMessage);
      if(errorCode.value != 0){
         JOptionPane.showMessageDialog(this, "Problem in creating relation\n" + errorMessage);
@@ -264,7 +276,7 @@ void start(){
   boolean sd = subdirCB.isSelected();
   Stack<File> stack = new Stack<File>();
   File[] files = f.listFiles();
-  insertFiles(stack,files,sd);
+  insertFiles(stack,files,sd, format);
 
   int length = f.getAbsolutePath().length()+1;
   int count = 0;
@@ -272,7 +284,7 @@ void start(){
     File top = stack.pop();
     if(top.isDirectory()){
        if(sd){
-          insertFiles(stack, top.listFiles(),sd);
+          insertFiles(stack, top.listFiles(),sd, format);
        }
     } else {
         String name = top.getAbsolutePath();
@@ -282,9 +294,14 @@ void start(){
            String content = getBase64(top.getAbsolutePath());
            if(content!=null){
               name = name.replace("'"," ");
-              cmd = "query " + relName + " inserttuple[ [const text value '"+name+"'],"
-                  +                                    "[const document(pdf) value '"+content+"']] count";
-
+              String shortName = (new File(name)).getName();
+              if(shortName.length()>47){
+                 shortName = shortName.substring(shortName.length()-47);
+              }
+              String v = getConstValue(format, shortName, content);
+              cmd = "query " + relName + 
+                    " inserttuple[ [const text value '"+name+"'] ,"
+                  + v +"] count"; 
               VC.execCommand(cmd,errorCode, resultList, errorMessage);
               if(errorCode.value != 0){
                  JOptionPane.showMessageDialog(this, "Problem  relation update\n" + errorMessage);
@@ -332,12 +349,96 @@ public boolean addObject(SecondoObject o){
 }
 
 public String getName(){
-  return "PDF-Relation-Creator";
+  return "PDF-JPG-Relation-Creator";
+}
+
+  /* format depending parts */
+
+  private String getExtension(FORMAT format){
+    switch(format){
+      case PDF : return ".pdf";
+      case JPG : return ".jpg";
+    }
+    return null;
+  }
+
+ 
+ListExpr getRelType(FORMAT format){
+
+  ListExpr attrList;
+  if(format==FORMAT.PDF){
+     attrList = ListExpr.twoElemList(
+                        ListExpr.twoElemList( 
+                            ListExpr.symbolAtom("File"),
+                            ListExpr.symbolAtom("text")),
+                        ListExpr.twoElemList( 
+                            ListExpr.symbolAtom("Content"),
+                            ListExpr.twoElemList(
+                                  ListExpr.symbolAtom("document"),
+                                  ListExpr.symbolAtom("pdf"))));
+   } else if(format==FORMAT.JPG){
+     attrList = ListExpr.twoElemList(
+                        ListExpr.twoElemList( 
+                            ListExpr.symbolAtom("File"),
+                            ListExpr.symbolAtom("text")),
+                        ListExpr.twoElemList( 
+                            ListExpr.symbolAtom("Image"),
+                            ListExpr.symbolAtom("picture")));
+   } else {
+      Reporter.showError("invalid Format detected, internal error");
+      return ListExpr.theEmptyList();
+   }
+
+  return  ListExpr.twoElemList(
+             ListExpr.symbolAtom("rel"),
+             ListExpr.twoElemList(
+                ListExpr.symbolAtom("tuple"),
+                attrList));
 }
 
 
- 
+private FORMAT getSelectedFormat(){
+   if(pdfFormat.isSelected()){
+      return FORMAT.PDF;
+   }   
+   if(jpgFormat.isSelected()){
+      return FORMAT.JPG;
+   }
+   return null;
+}
 
+private String emptyRelString(FORMAT format){
+  switch(format){
+    case PDF : return " [const rel(tuple([File : text, Content : document(pdf)])) value ()]";
+    case JPG : return " [const rel(tuple([File : text, Image : picture])) value ()]";
+  }
+  return null;
 
+}
+
+private boolean isPortrait(String filename){
+   try{
+     BufferedImage img = ImageIO.read(new File(filename));
+     return img.getHeight() > img.getWidth();
+   } catch(Exception e){
+     return false;
+   }
+
+}
+
+private String getConstValue(FORMAT format, String fileName, String content){
+
+  if(format.equals(FORMAT.PDF)){
+      return  "[const document(pdf) value '"+content+"']";
+  } 
+  if(format.equals(FORMAT.JPG)){
+      boolean portrait = isPortrait(fileName);
+      SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss"); 
+      String date = formatter.format(new Date());
+      String port =portrait?"TRUE":"FALSE";
+      return "[const picture value (\"" + fileName +"\" \""+date+"\" \"misc\" "+port+" '"+content+"')]";
+  }
+  return null;
+}
 
 }
