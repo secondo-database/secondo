8b67045618b69360dc561b6252257f1605101796
dmerle <>
2018-03-28 15:33:40 +0000

    Added comment

--- UserInterfaces/InteractiveQueryEditor/ui/console/ConsoleDocumentFilterInterceptor.java
+++ UserInterfaces/InteractiveQueryEditor/ui/console/ConsoleDocumentFilterInterceptor.java
@@ -3,6 +3,11 @@ package ui.console;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.DocumentFilter.FilterBypass;
 
+/**
+ * An interface which allows other classes to dynamically change the
+ * behaviour of the ConsoleDocumentFilter.
+ * @author D.Merle
+ */
 public interface ConsoleDocumentFilterInterceptor {
 	public void replace(final FilterBypass fb, final int offset, int length, final String string, final AttributeSet attr);
 	public void remove(final FilterBypass fb, final int offset, final int length);
