e3d8575d83c7c3844f2f71c6e1d39b642dfe13a6
jmende <>
2018-10-29 18:04:01 +0000

    Add Metrics Define macros, create report

---
+++ Algebras/Pregel/Helpers/Metrics.cpp
@@ -0,0 +1,153 @@
+/*
+----
+This file is part of SECONDO.
+
+Copyright (C) 2004, University in Hagen, Department of Computer Science,
+Database Systems for New Applications.
+
+SECONDO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SECONDO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SECONDO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+----
+
+//paragraph [1] Title: [{\Large \bf \begin {center}] [\end {center}}]
+//[TOC] [\tableofcontents]
+
+[1] Header File of the class ~PregelAlgebra~
+
+November 2018, J. Mende
+
+
+[TOC]
+
+1 Overview
+
+This file defines the members of class Metrics
+
+*/
+#include "Metrics.h"
+
+namespace pregel {
+ Metrics Metrics::singleton = Metrics();
+
+ Metrics::Report Metrics::report() {
+  using namespace std::chrono;
+  auto report = Report();
+  auto superstep = SuperstepCounter::get();
+  report.messagesSentOverNetwork = messagesSentOverNetwork;
+
+  report.messagesQueuedDirectly = messagesQueuedDirectly;
+
+  report.messagesSentPerSuperstep =
+   (superstep != 0) ?
+   (messagesSentOverNetwork + messagesQueuedDirectly) / (double) superstep : 0;
+
+  report.messagesReceived = messagesReceived;
+
+  report.messagesReceivedPerSuperstep =
+   (superstep != 0) ?
+   messagesReceived / (double) superstep : 0;
+
+  report.messagesDiscarded = messagesDiscarded;
+
+  report.timeIdle =
+   duration_cast<milliseconds>(timeIdle).count() / (double) 1000;
+
+  report.timeProductive =
+   duration_cast<milliseconds>(timeProductive).count() / (double) 1000;
+
+  auto timeTotal = timeProductive + timeIdle;
+
+  report.productivity =
+   (timeTotal != TIME_ZERO) ?
+   timeProductive.count() / (double) timeTotal.count() : 0;
+
+  return report;
+ }
+
+ void Metrics::sentMessage() {
+  ++messagesSentOverNetwork;
+ }
+
+ void Metrics::queuedMessage() {
+  ++messagesQueuedDirectly;
+ }
+
+ void Metrics::receivedMessage() {
+  ++messagesReceived;
+ }
+
+ void Metrics::discardedMessage() {
+  ++messagesDiscarded;
+ }
+
+ void Metrics::startClock(bool productive) {
+  using namespace std::chrono;
+  if (stopWatch.clock != TIME_ZERO) {
+   std::cerr << "Was already counting something\n";
+  }
+
+  stopWatch.countingProductiveTime = productive;
+  stopWatch.clock = duration_cast<milliseconds>(
+   system_clock::now().time_since_epoch()
+  );
+ }
+
+ void Metrics::stopClock() {
+  using namespace std::chrono;
+  if (stopWatch.clock == TIME_ZERO) {
+   std::cerr << "Wasn't counting\n";
+   return;
+  }
+  auto now = duration_cast<milliseconds>(
+   system_clock::now().time_since_epoch()
+  );
+
+  auto duration = now - stopWatch.clock;
+  if (stopWatch.countingProductiveTime) {
+   timeProductive += duration;
+  } else {
+   timeIdle += duration;
+  }
+
+  stopWatch.clock = TIME_ZERO;
+ }
+
+ Metrics &Metrics::get() {
+  return singleton;
+ }
+
+ void Metrics::Report::print(std::stringstream &sstream) {
+  sstream << "  == Metrics ==" << std::endl;
+  sstream << "    Messages sent over network : " <<
+          messagesSentOverNetwork << std::endl;
+  sstream << "               queued directly : " <<
+          messagesQueuedDirectly << std::endl;
+  sstream << "                 per Superstep : " <<
+          messagesSentPerSuperstep << std::endl;
+  sstream << "    Messages received          : " <<
+          messagesReceived << std::endl;
+  sstream << "                 per Superstep : " <<
+          messagesReceivedPerSuperstep << std::endl;
+  sstream << "    Messages discarded         : " <<
+          messagesDiscarded << std::endl;
+  sstream << std::endl;
+  sstream << "    Time productive            : " <<
+          timeProductive << "s" << std::endl;
+  sstream << "    Time idle                  : " <<
+          timeIdle << "s" << std::endl;
+  sstream << "    Productivity               : " <<
+          (100.0 * productivity) << "%" << std::endl;
+  sstream << std::endl;
+ }
+}
---
+++ Algebras/Pregel/Helpers/Metrics.h
@@ -0,0 +1,125 @@
+/*
+----
+This file is part of SECONDO.
+
+Copyright (C) 2004, University in Hagen, Department of Computer Science,
+Database Systems for New Applications.
+
+SECONDO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SECONDO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SECONDO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+----
+
+//paragraph [1] Title: [{\Large \bf \begin {center}] [\end {center}}]
+//[TOC] [\tableofcontents]
+
+[1] Header File of the class ~PregelAlgebra~
+
+November 2018, J. Mende
+
+
+[TOC]
+
+1 Overview
+
+This header file defines the class Metrics
+
+2 Defines and includes
+
+*/
+
+#ifndef SECONDO_STATISTICS_H
+#define SECONDO_STATISTICS_H
+
+#define GATHER_PREGEL_METRICS
+
+#ifdef GATHER_PREGEL_METRICS
+#define SENT_MESSAGE Metrics::get().sentMessage();
+#define QUEUED_MESSAGE Metrics::get().queuedMessage();
+#define RECEIVED_MESSAGE Metrics::get().receivedMessage();
+#define DISCARDED_MESSAGE Metrics::get().discardedMessage();
+
+#define PRODUCTIVE Metrics::get().startClock(true);
+#define IDLE Metrics::get().startClock(false);
+#define STOP_CLOCK Metrics::get().stopClock();
+#else
+#define SENT_MESSAGE do {} while(0);
+#define QUEUED_MESSAGE do {} while(0);
+#define RECEIVED_MESSAGE do {} while(0);
+#define DISCARDED_MESSAGE do {} while(0);
+
+#define PRODUCTIVE do {} while(0);
+#define IDLE do {} while(0);
+#define STOP_CLOCK do {} while(0);
+#endif
+
+#include "../PregelContext.h"
+#include <chrono>
+
+namespace pregel {
+#define TIME_ZERO \
+std::chrono::duration_values<std::chrono::milliseconds>::zero()
+
+ class Metrics {
+ private:
+  static Metrics singleton;
+
+  unsigned int messagesSentOverNetwork;
+  unsigned int messagesQueuedDirectly;
+  unsigned int messagesReceived;
+  unsigned int messagesDiscarded;
+  std::chrono::milliseconds timeIdle;
+  std::chrono::milliseconds timeProductive;
+
+  struct StopWatch {
+   std::chrono::milliseconds clock = TIME_ZERO;
+   bool countingProductiveTime;
+  };
+  
+  StopWatch stopWatch;
+
+ public:
+  struct Report {
+   unsigned int messagesSentOverNetwork;
+   unsigned int messagesQueuedDirectly;
+   double messagesSentPerSuperstep;
+   unsigned int messagesReceived;
+   double messagesReceivedPerSuperstep;
+   unsigned int messagesDiscarded;
+   double timeIdle;
+   double timeProductive;
+   double productivity;
+
+   void print(std::stringstream &sstream);
+  };
+
+  static Metrics &get();
+
+  Report report();
+
+  void sentMessage();
+
+  void queuedMessage();
+
+  void receivedMessage();
+
+  void discardedMessage();
+
+  void startClock(bool productive = true);
+
+  void stopClock();
+ };
+}
+
+
+#endif //SECONDO_STATISTICS_H
