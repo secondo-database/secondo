5460db9dac5e730e2956574bf036d94be12c8443
jmgrassau <>
2019-04-03 11:03:19 +0000

    content of JoinEdge.cpp cleared (as CVS won't properly delete the file)

--- Algebras/CDACSpatialJoin/JoinEdge.cpp
+++ Algebras/CDACSpatialJoin/JoinEdge.cpp
@@ -2,35 +2,3 @@
 1 JoinEdge struct
 
 */
-
-#include <string>
-#include <iostream>
-#include <sstream>
-
-#include "JoinEdge.h"
-#include "Base.h"
-
-using namespace cdacspatialjoin;
-using namespace std;
-
-JoinEdge::JoinEdge(const double yMin_, const double yMax_,
-                   const EdgeIndex_t counterPartEdgeIndex_,
-                   const bool isLeft_, const SET set_,
-                   const BlockIndex_t block_, const RowIndex_t row_) :
-                   yMin(yMin_),
-                   yMax(yMax_),
-                   counterPartEdgeIndex(counterPartEdgeIndex_),
-                   isLeft(isLeft_),
-                   set(set_),
-                   block(block_),
-                   row(row_) {
-}
-
-std::string JoinEdge::toString() const {
-   stringstream st;
-   st << "y = [" << yMin << "; " << yMax << "]; ";
-   st << (isLeft ? "left" : "right") << " edge ";
-   st << "from set " << SET_NAMES[set] << ", ";
-   st << "block " << block << ", row " << row;
-   return st.str();
-}
