4aa65949387fba7a788abd25b4cf0ed5984d77a9
anikolidakis <>
2019-05-10 08:04:27 +0000

    Files for Row Implementation

---
+++ Algebras/SpatialJoinTOUCH/Row/SpatialJoinRowLocalInfo.cpp
@@ -0,0 +1,155 @@
+/*
+This file is part of SECONDO.
+
+Copyright (C) 2004, University in Hagen, Department of Computer Science,
+Database Systems for New Applications.
+
+SECONDO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SECONDO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SECONDO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+class NestedList;
+class QueryProcessor;
+class tchNode;
+#include "../RTreeTouch.h"
+#include "SpatialJoinRowLocalInfo.h"
+#include "Algebras/Relation-C++/RelationAlgebra.h"
+
+
+
+
+
+extern NestedList* nl;
+extern QueryProcessor *qp;
+
+using namespace std;
+using namespace mmrtreetouch;
+
+
+namespace STR {
+vector<mmrtreetouch::tchNode *> createBuckets(
+        vector<::Tuple*> tuples,
+        int firstStreamWordIndex,
+        int _numOfPartitions
+);
+}
+
+
+SpatialJoinRowLocalInfo::~SpatialJoinRowLocalInfo() {
+    for (Tuple * tuple:tuplesA) {
+        delete tuple;
+    }
+
+    for (Tuple * tupleB:tuplesB) {
+        delete tupleB;
+    }
+
+    delete rtree;
+
+    tt->DeleteIfAllowed();
+};
+
+
+SpatialJoinRowLocalInfo::SpatialJoinRowLocalInfo(
+        Word firstStreamWordParam,
+        Word secondStreamWordParam,
+        int firstStreamWordIndex,
+        int secondStreamWordIndex,
+        ListExpr ttl,
+        int _fanout,
+        int _numOfPartitions,
+        int _cellFactor
+)
+{
+    turnOn = false;
+
+    tt = new ::TupleType(ttl);
+    _firstStreamWordIndex = firstStreamWordIndex;
+    cellFactor = _cellFactor;
+
+    firstStreamWord = firstStreamWordParam;
+    secondStreamWord = secondStreamWordParam;
+
+    tuplesA = getAllTuplesFromStream(firstStreamWord);
+    tuplesB = getAllTuplesFromStream(secondStreamWord);
+
+    vector<mmrtreetouch::tchNode * > buckets = STR::createBuckets(
+            tuplesA,
+            _firstStreamWordIndex,
+            _numOfPartitions
+            );
+
+    rtree = new RTreeTouch(
+            tt,
+            firstStreamWordIndex,
+            secondStreamWordIndex,
+            cellFactor,
+            false
+            );
+
+    int fanout = _fanout;
+
+    rtree->constructTree(buckets, fanout);
+
+    assignTuplesB(tuplesB);
+
+    findMatchings();
+};
+
+void SpatialJoinRowLocalInfo::assignTuplesB(vector<Tuple*> tuplesB)
+{
+    for (Tuple* tupleB: tuplesB) {
+        rtree->assignTupleB(tupleB);
+    }
+}
+
+void SpatialJoinRowLocalInfo::findMatchings()
+{
+    matchingVector = rtree->findMatchings();
+}
+
+Tuple* SpatialJoinRowLocalInfo::NextResultTuple () {
+    if (!matchingVector.empty()) {
+        Tuple *tupleToReturn = matchingVector.back();
+        matchingVector.pop_back();
+
+        return tupleToReturn;
+    } else {
+        return 0;
+    }
+}
+
+vector<Tuple*> SpatialJoinRowLocalInfo::getAllTuplesFromStream(
+        const Word stream)
+{
+    Word streamTupleWord;
+    vector<Tuple*> tuples;
+
+    qp->Open (stream.addr);
+    qp->Request (stream.addr, streamTupleWord);
+
+    while (qp->Received (stream.addr) )
+    {
+        Tuple* t = (Tuple*) streamTupleWord.addr;
+
+        tuples.push_back(t);
+
+        qp->Request ( stream.addr, streamTupleWord);
+    }
+
+    qp->Close (stream.addr);
+
+    return tuples;
+}
---
+++ Algebras/SpatialJoinTOUCH/Row/SpatialJoinRowLocalInfo.h
@@ -0,0 +1,80 @@
+/*
+This file is part of SECONDO.
+
+Copyright (C) 2004, University in Hagen, Department of Computer Science,
+Database Systems for New Applications.
+
+SECONDO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SECONDO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SECONDO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+#ifndef SECONDO_SPATIALJOINROWLOCALINFO_H
+#define SECONDO_SPATIALJOINROWLOCALINFO_H
+
+class TupleType;
+
+namespace mmrtreetouch {
+    class RTreeTouch;
+}
+
+extern NestedList* nl;
+extern QueryProcessor *qp;
+
+
+class SpatialJoinRowLocalInfo
+{
+private:
+    TupleType* tt;
+    Word firstStreamWord;
+    Word secondStreamWord;
+    int _firstStreamWordIndex;
+    int cellFactor;
+
+    std::vector<Tuple*> getAllTuplesFromStream (const Word stream);
+
+
+    std::vector<Tuple*> matchingVector;
+    std::vector<Tuple*> tuplesA;
+    std::vector<Tuple*> tuplesB;
+
+    bool turnOn;
+
+    void findMatchings();
+
+public:
+    mmrtreetouch::RTreeTouch * rtree;
+
+    SpatialJoinRowLocalInfo(
+            Word firstStreamWordParam,
+            Word secondStreamWordParam,
+            int firstStreamWordIndex,
+            int secondStreamWordIndex,
+            ListExpr ttl,
+            int _fanout,
+            int _numOfPartitions,
+            int _cellFactor
+    );
+
+    ~SpatialJoinRowLocalInfo();
+
+    std::stringstream ss;
+
+    void assignTuplesB(std::vector<Tuple*> tuplesB);
+
+    Tuple* NextResultTuple ();
+
+};
+
+#endif //SECONDO_SPATIALJOINROWLOCALINFO_H
