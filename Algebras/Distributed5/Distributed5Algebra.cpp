58529a71f7c738680a172f91a364677037ace7a6
akoppers <>
2019-08-13 08:47:45 +0000

    Added Task Type. Added schedule_S Operator to start Tasks on workers. Added dmap_S Operator so that it can consume DARRAY and a stream of tasks and adds a stream of task. The schedule Operator consumes the stream of tasks and executes the tasks on workers.

---
+++ Algebras/Distributed5/Distributed5Algebra.cpp
@@ -0,0 +1,64 @@
+/*
+----
+This file is part of SECONDO.
+
+Copyright (C) 2015,
+Faculty of Mathematics and Computer Science,
+Database Systems for New Applications.
+
+SECONDO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SECONDO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SECONDO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+----
+
+
+//[$][\$]
+
+*/
+
+#include "Algebras/Distributed5/schedule_S.h"
+#include "Algebras/Distributed5/Task.h"
+#include "Algebras/Distributed5/dmap_S.h"
+#include "Algebras/Distributed5/ARRAYORTASKSFUNARG.h"
+#include <boost/bind.hpp>
+#include <boost/ref.hpp>
+
+using namespace std;
+using namespace distributed2;
+
+namespace distributed5{
+
+class Distributed5Algebra : public Algebra
+{
+public:
+    Distributed5Algebra() : Algebra()
+    {
+        AddOperator(&dmap_SOp);
+        dmap_SOp.SetUsesArgsInTypeMapping();
+        AddOperator(&schedule_SOp);
+        AddTypeConstructor(&TaskTC);
+        TaskTC.AssociateKind(Kind::SIMPLE());
+
+        AddOperator(&ARRAYORTASKSFUNARG1Op);
+    }
+};
+
+} // namespace distributed5
+
+extern "C" Algebra *
+InitializeDistributed5Algebra(NestedList *nlRef,
+                              QueryProcessor *qpRef,
+                              AlgebraManager *amRef)
+{
+    return new distributed5::Distributed5Algebra();
+}
