1d6a6b464b9f3d70c48af3fad99be1d6cd09790e
gstancul <>
2019-08-26 15:48:39 +0000

    Fixed imports

--- Algebras/Kafka/StreamStopping.cpp
+++ Algebras/Kafka/StreamStopping.cpp
@@ -29,9 +29,10 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "Attribute.h" // implementation of attribute types
 #include "NestedList.h" // required at many places
 #include "Operator.h" // for operator creation
-#include "Symbols.h" // predefined strings
 #include "ListUtils.h" // useful functions for nested lists
 #include "Algebras/Stream/Stream.h" // wrapper for secondo streams
+#include "Algebras/Relation-C++/RelationAlgebra.h" // use of tuples
+#include <stack>
 
 using namespace std;
 
@@ -42,12 +43,11 @@ namespace kafka {
             return listutils::typeError(" wrong number of args ");
         }
 
-        // TODO: Enable
-//        if (!Stream<Tuple>::checkType(nl->First(args))
-//            || !CcInt::checkType(nl->Second(args))) {
-//            return listutils::typeError(" stream(Tuple) x Port "
-//                                        "expected ");
-//        }
+        if (!Stream<Tuple>::checkType(nl->First(args))
+            || !CcInt::checkType(nl->Second(args))) {
+            return listutils::typeError(" stream(Tuple) x Port "
+                                        "expected ");
+        }
         return nl->First(args);
     }
 
