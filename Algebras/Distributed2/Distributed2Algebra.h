795eb31dd21ebebe07fb0528359eec396dfc078c
behr <>
2019-09-24 06:50:38 +0000

    Enabled distributed progress visualization

--- Algebras/Distributed2/Distributed2Algebra.h
+++ Algebras/Distributed2/Distributed2Algebra.h
@@ -32,7 +32,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "ErrorWriter.h"
 #include "DArray.h"
 
- // #define DPROGRESS
+#define DPROGRESS
 
 #ifdef DPROGRESS
 #include "ProgressObserver.h"
