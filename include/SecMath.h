9575f90f3ddc3500e9c15cda5fca6ff95c40ac6f
behr <>
2019-12-19 15:52:00 +0000

    Simple constant definitions

---
+++ include/SecMath.h
@@ -0,0 +1,43 @@
+
+/*
+---- 
+This file is part of SECONDO.
+
+Copyright (C) 2011, University in Hagen, Faculty of Mathematics and Computer Science, 
+Database Systems for New Applications.
+
+SECONDO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SECONDO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SECONDO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+----
+
+1 This file provides some functions handling with strings.
+
+
+*/
+
+
+#pragma once
+
+#include <cmath>
+#include <math.h>
+
+#ifndef M_PI
+#define M_PI   3.14159265358979323846
+#endif
+#ifndef M_E
+#define M_E         2.7182818284590452354
+#endif
+
+
+
