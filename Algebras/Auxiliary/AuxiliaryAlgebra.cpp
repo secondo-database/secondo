b0df953f8ed75d383154751fe35be6d81c705994
behr <>
2019-12-20 13:48:33 +0000

    added timersub definition

--- Algebras/Auxiliary/AuxiliaryAlgebra.cpp
+++ Algebras/Auxiliary/AuxiliaryAlgebra.cpp
@@ -68,6 +68,19 @@ extern AlgebraManager *am;
 
 using namespace std;
 
+
+#ifndef timersub
+#define timersub(a, b, result) \
+        do { \
+                (result)->tv_sec = (a)->tv_sec - (b)->tv_sec; \
+                (result)->tv_usec = (a)->tv_usec - (b)->tv_usec; \
+                if ((result)->tv_usec < 0) { \
+                        --(result)->tv_sec; \
+                        (result)->tv_usec += 1000000; \
+                } \
+        } while (0)
+#endif // timersub
+
 // Activate debug messages
 //#define __DEBUG__
 
