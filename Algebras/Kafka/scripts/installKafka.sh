1e9ddc5ad0b28d2d56ba04f4562b4c11ff7d6228
gstancul <>
2020-04-20 15:46:57 +0000

    Kafka download url changed

--- Algebras/Kafka/scripts/installKafka.sh
+++ Algebras/Kafka/scripts/installKafka.sh
@@ -14,7 +14,8 @@ fi
 mkdir kafka
 cd kafka
 
-wget http://apache.mirror.digionline.de/kafka/2.2.0/kafka_2.12-2.2.0.tgz
+# wget http://apache.mirror.digionline.de/kafka/2.2.0/kafka_2.12-2.2.0.tgz
+wget https://archive.apache.org/dist/kafka/2.2.0/kafka_2.12-2.2.0.tgz
 mkdir kafka_dist
 tar -xvzf kafka_2.12-2.2.0.tgz --directory kafka_dist --strip 1
 
