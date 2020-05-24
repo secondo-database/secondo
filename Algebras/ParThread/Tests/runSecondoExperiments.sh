e3e5355a4bd930c02f9c9a40cbb72739dc86dd15
tfischer <>
2020-05-24 16:44:29 +0000

    updated tests

--- Algebras/ParThread/Tests/runSecondoExperiments.sh
+++ Algebras/ParThread/Tests/runSecondoExperiments.sh
@@ -5,42 +5,72 @@ if [ "$1" = "" ]; then
 	echo "Testserver-DBs: ARNSBERG, GERMANY, NRW"
 	exit
 fi
+if [ "$2" = "" ]; then
+    echo "ERROR: second parameter must be the testgroup."
+    echo "This is used to replace some tablenames in queries"
+	echo "e.g TCA, TCB, TCB"
+	exit
+fi
 
 export SECONDO_PARAM_RTFlags="DEBUG:DemangleStackTrace,CMSG:Color,SMI:NoTransactions,CTLG:SkipExamples,SI:PrintCmdTimes,SI:ShowCommandTime,SI:NoCommandEcho,QP:OpTree2SVG"
 export SECONDO_PARAM_NodeMem=32000
 export SECONDO_PARAM_StringMem=8000
 export SECONDO_PARAM_TextMem=8000
 
+
 dbname=$1
+testgroup=$2
 basePath=$PWD
 queryPath=$PWD/TestQueries/*.sql
 configPath=$PWD/TestConfigs/*.ini
 
 secondoBin="$HOME/secondo/bin"
 
+rm "$basePath/TestResults/runtimes.log"
+
+#iterate all scripts in scriptpath
 for s in $queryPath
 do
-    sed -i "s/^open database .*;$/open database ${dbname};/g" ${s}
-
+    #iterate all configs in configpath
 	sn=$(basename "${s}" ".${s##*.}")
 	for c in $configPath
 	do
 		cn=$(basename "${c}" ".${c##*.}")
-		logdir="$basePath/TestResults/${sn}_${cn}"
- 
-        rm -r ${logdir}
-        mkdir ${logdir}
-        logname="output.log"
+        
+		#execute each query multiple times
+		for n in 1 2 3
+		do
+            #copy the config and sql script to working directory
+			logdir="$basePath/TestResults/${testgroup}_${sn}_${cn}_${n}"
+		    rm -r ${logdir}
+		    mkdir ${logdir}
 
-		cd $secondoBin
+            sl="$logdir/${sn}.sql"
+            cl="$logdir/${cn}.ini"         
+            cp $s $sl
+            cp $c $cl
 
-        runner="SecondoBDB -i $s -c $c"
-        $runner 2>&1 | tee "$logdir/$logname"
+            #replace test specific parts of the sql statement
+			sed -i "s/^open database .*;$/open database ${dbname};/g" ${sl}
+			sed -i "s/Osm_Ways/${testgroup}_Ways/g" ${sl}
+			sed -i "s/Osm_Nodes/${testgroup}_Nodes/g" ${sl}
+
+		    logname="output.log"
+
+			cd $secondoBin
+
+            export SECONDO_PARAM_RTFlags="SMI:NoTransactions,DEBUG:DemangleStackTrace,CMSG:Color,CTLG:SkipExamples,SI:PrintCmdTimes,SI:ShowCommandTime,SI:NoCommandEcho,QP:OpTree2SVG,QP:ProgDisable"
+
+		    runner="SecondoBDB -i $sl -c $cl"
+		    $runner 2>&1 | tee "$logdir/$logname"
+        done
 
         cd "$basePath/TestResults"
 		mv *.log $logdir
 		mv *.gv $logdir
 		cd $basePath
+
+        #python analyzeLogFiles.py "$logdir/$logname" "$basePath/TestResults/runtimes.log"
     done
 done
 
--- Algebras/ParThread/Tests/runTestRunner.sh
+++ Algebras/ParThread/Tests/runTestRunner.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-cd /home/fischert/secondo/bin/
+if [ "$1" = "" ]; then
+    echo "ERROR: first parameter must be the path to the configuration file"
+	echo "e.g realtive to bin directory: ../Algebras/ParThread/Tests/TestConfigs/scfgMaxThreads.ini"
+	exit
+fi
 
-TestRunner --no-tmp -c '/home/fischert/secondo/Algebras/ParThread/Tests/SecondoConfigMaxThreads.ini' -i '/home/fischert/secondo/Algebras/ParThread/Tests/parthreadAlgebra.test'
+configPath=$1
+secondoBin="$HOME/secondo/bin"
+
+cd "$secondoBin"
+
+export SECONDO_PARAM_RTFlags="SMI:NoTransactions,DEBUG:DemangleStackTrace,CMSG:Color,CTLG:SkipExamples,SI:PrintCmdTimes,SI:ShowCommandTime,SI:NoCommandEcho,QP:OpTree2SVG,QP:ProgDisable"
+
+SecondoBDB -test -c "$configPath" -i '../Algebras/ParThread/Tests/parthreadAlgebra.test'
