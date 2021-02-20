e4c20b98364e98a37ef7ed88cb7ae8c137ff702a
pfedorow <pfedorow>
2021-02-20 14:52:06 +0000

    AssociationAnalysis: first cut of an apriori operator

--- Algebras/AssociationAnalysis/AssociationAnalysisAlgebra.cpp
+++ Algebras/AssociationAnalysis/AssociationAnalysisAlgebra.cpp
@@ -79,7 +79,7 @@ ListExpr genTransactionsTM(ListExpr args) {
   NList type(args);
 
   if (type.length() == 5) {
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= 5; i += 1) {
       if (!type.elem(i).isSymbol(CcInt::BasicType())) {
         return NList::typeError("Wrong argument type passed.");
       }
@@ -279,10 +279,233 @@ struct genTransactionsInfo : OperatorInfo {
 
 // endregion
 
+ListExpr frequentItemsetTupleType() {
+  NList attrs =
+      NList(NList(NList().symbolAtom("Itemset"),
+                  NList().symbolAtom(collection::IntSet::BasicType())),
+            NList(NList().symbolAtom("Support"),
+                  NList().symbolAtom(CcReal::BasicType())));
+  ListExpr type = NList().tupleOf(attrs).listExpr();
+  return type;
+}
+
+// region Implementation of the apriori operator.
+
+ListExpr aprioriTM(ListExpr args) {
+  NList type(args);
+
+  NList attrs;
+  if (type.length() == 3) {
+    if (!type.elem(1).checkRel(attrs)) {
+      return NList::typeError("Wrong argument type passed.");
+    }
+    if (!type.isSymbol(2)) {
+      return NList::typeError("Wrong argument type passed.");
+    }
+    if (!type.elem(3).isSymbol(CcReal::BasicType())) {
+      return NList::typeError("Wrong argument type passed.");
+    }
+  } else {
+    return NList::typeError("Wrong number of arguments passed.");
+  }
+
+  string attrName = type.elem(2).str();
+  int attrIndex = -1;
+  for (int i = 1; i <= (int)attrs.length(); i += 1) {
+    NList attr = attrs.elem(i);
+    if (attr.elem(1).isSymbol(attrName)) {
+      attrIndex = i;
+    }
+  }
+
+  if (attrIndex == -1) {
+    return NList::typeError(
+        "The given attribute was not found in the tuple description.");
+  }
+
+  NList tupleType = NList(frequentItemsetTupleType());
+  return NList(Symbols::APPEND(), NList().intAtom(attrIndex - 1).enclose(),
+               NList().streamOf(tupleType))
+      .listExpr();
+}
+
+class aprioriLI {
+public:
+  aprioriLI(GenericRelation *relation, double minSupport, int attrIndex) {
+    int transactionCount = relation->GetNoTuples();
+
+    vector<set<set<int>>> freqItemsets;
+
+    // Generate the set of frequent 1-Itemsets.
+    freqItemsets.resize(2);
+    {
+      // Count how many transactions contain any given item.
+      GenericRelationIterator *rit = relation->MakeScan();
+      Tuple *t;
+      map<int, int> counts;
+      while ((t = rit->GetNextTuple()) != nullptr) {
+        auto transaction = (collection::IntSet *)t->GetAttribute(attrIndex);
+        for (int i = 0; i < (int)transaction->getSize(); i += 1) {
+          counts[transaction->get(i)] += 1;
+        }
+      }
+      // Add any item as an 1-itemset to the frequent itemsets if it satisfies
+      // the minimum support.
+      for (auto& [item, count] : counts) {
+        double support = (double)count / (double)transactionCount;
+        if (support >= minSupport) {
+          set<int> itemset = {item};
+          freqItemsets[1].insert(itemset);
+          this->frequentItemsets.emplace_back(itemset, support);
+        }
+      }
+    }
+
+    for (int size = 2; !freqItemsets[size - 1].empty(); size += 1) {
+      set<set<int>> candidates =
+          aprioriLI::genCandidates(freqItemsets[size - 1]);
+      if (candidates.empty()) {
+        break;
+      }
+      // Count how many transactions contain any given candidate.
+      GenericRelationIterator *rit = relation->MakeScan();
+      Tuple *t;
+      map<set<int>, int> counts;
+      while ((t = rit->GetNextTuple()) != nullptr) {
+        auto transaction = (collection::IntSet *)t->GetAttribute(attrIndex);
+        for (const set<int>& candidate : candidates) {
+          int containsCandidate = true;
+          for (int item : candidate) {
+            if (!transaction->contains(item)) {
+              containsCandidate = false;
+              break;
+            }
+          }
+          if (containsCandidate) {
+            counts[candidate] += 1;
+          }
+        }
+      }
+      // Add any candidate to the frequent itemsets if it satisfies the minimum
+      // support.
+      freqItemsets.resize(size + 1);
+      for (auto& [itemset, count] : counts) {
+        double support = (double)count / (double)transactionCount;
+        if (support >= minSupport) {
+          freqItemsets[size].insert(itemset);
+          this->frequentItemsets.emplace_back(itemset, support);
+        }
+      }
+    }
+
+    this->it = this->frequentItemsets.cbegin();
+
+    // Setup resulting tuple type.
+    this->tupleType = new TupleType(
+        SecondoSystem::GetCatalog()->NumericType(frequentItemsetTupleType()));
+  }
+
+  ~aprioriLI() { this->tupleType->DeleteIfAllowed(); }
+
+  Tuple *getNext() {
+    if (this->it != this->frequentItemsets.cend()) {
+      auto &[itemset, support] = *this->it;
+      auto tuple = new Tuple(this->tupleType);
+      tuple->PutAttribute(0, new collection::IntSet(itemset));
+      tuple->PutAttribute(1, new CcReal(support));
+      this->it++;
+      return tuple;
+    } else {
+      return nullptr;
+    }
+  }
+
+private:
+  vector<pair<set<int>, double> >::const_iterator it;
+  vector<pair<set<int>, double> > frequentItemsets;
+  TupleType *tupleType;
+
+  static set<set<int>>
+  genCandidates(const set<set<int>> &prevFrequentItemsets) {
+    if (prevFrequentItemsets.empty()) {
+      return {};
+    }
+    int size = (int)(*prevFrequentItemsets.cbegin()).size() + 1;
+    set<set<int>> candidates;
+    // join step
+    for (const set<int> &itemset1 : prevFrequentItemsets) {
+      for (const set<int> &itemset2 : prevFrequentItemsets) {
+        if (&itemset1 == &itemset2) {
+          continue;
+        }
+        set<int> itemset;
+        itemset.insert(itemset1.cbegin(), itemset1.cend());
+        itemset.insert(itemset2.cbegin(), itemset2.cend());
+        if ((int)itemset.size() == size) {
+          candidates.insert(itemset);
+        }
+      }
+    }
+    // prune step
+    for (const set<int> &itemset : candidates) {
+      for (int item : itemset) {
+        set<int> subset(itemset);
+        subset.erase(item);
+        if (prevFrequentItemsets.count(subset) == 0) {
+          candidates.erase(itemset);
+          break;
+        }
+      }
+    }
+    return candidates;
+  }
+};
+
+int aprioriVM(Word *args, Word &result, int message, Word &local, Supplier s) {
+  auto *li = (aprioriLI *)local.addr;
+  switch (message) {
+  case OPEN: {
+    delete li;
+    local.addr =
+        new aprioriLI((GenericRelation *)args[0].addr,        // relation
+                      ((CcReal *)args[2].addr)->GetRealval(), // minSupport
+                      ((CcInt *)args[3].addr)->GetIntval()    // attrIndex
+        );
+    return 0;
+  }
+  case REQUEST:
+    result.addr = li ? li->getNext() : nullptr;
+    return result.addr ? YIELD : CANCEL;
+  case CLOSE:
+    delete li;
+    local.addr = nullptr;
+    return 0;
+  default:
+    return 0;
+  }
+}
+
+struct aprioriInfo : OperatorInfo {
+  aprioriInfo() : OperatorInfo() {
+    this->name = "apriori";
+    this->signature = "rel(tuple(...)) attr real -> stream(tuple(Itemset: "
+                      "intset, Support: real))";
+    this->syntax = "_ apriori[_, _]";
+    this->meaning =
+        "Discovers the frequent itemsets in the given relation of "
+        "transactions. The expected arguments are: the relation that contains "
+        "the transactions, the name of the attribute that contains the items "
+        "as an intset and the minimum support to look for.";
+  }
+};
+
+// endregion
+
 class AssociationAnalysisAlgebra : public Algebra {
 public:
   AssociationAnalysisAlgebra() : Algebra() {
     AddOperator(genTransactionsInfo(), genTransactionsVM, genTransactionsTM);
+    AddOperator(aprioriInfo(), aprioriVM, aprioriTM);
   }
 };
 
