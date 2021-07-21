a3221627652d4c2c1e7c2f7fb851d6da8051adb5
valdes <valdes>
2021-07-21 16:58:27 +0000

    NTree.h included

--- Algebras/MainMemory2/MainMemoryExt.h
+++ Algebras/MainMemory2/MainMemoryExt.h
@@ -54,6 +54,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "graph.h"
 #include "PointerWrapper.h"
 #include "MPointer.h"
+#include "NTree.h"
 
 #include "MemoryObject.h"
 #include "Algebras/Temporal/TemporalAlgebra.h"
