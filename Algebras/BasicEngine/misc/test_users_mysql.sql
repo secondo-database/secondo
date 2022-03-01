db94a2a6915b99528fa6c67a9853c630c40e945c
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2022-03-01 21:33:43 +0100

    Added test SQL scripts

---
+++ Algebras/BasicEngine/misc/test_users_mysql.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users (
+  id        int AUTO_INCREMENT,
+  firstname VARCHAR(100) NOT NULL,
+  lastname  VARCHAR(100) NULL,
+  age       INTEGER,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO users (firstname, lastname, age) values('Jan', 'Jansen', 25);
+INSERT INTO users (firstname, lastname, age) values('Max', 'Maxen', 28);
+INSERT INTO users (firstname, lastname, age) values('Klaus', 'Klausen', 38);
+INSERT INTO users (firstname, lastname, age) values('Jörg', 'Jansen', 60);
+INSERT INTO users (firstname, lastname, age) values('James', 'Jansen', 25);
+INSERT INTO users (firstname, lastname, age) values('Klaus', 'Jansen', 53);
+
