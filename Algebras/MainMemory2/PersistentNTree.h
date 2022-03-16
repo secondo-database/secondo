e18868b700fc1a4f8e92b6e87871ac6e726ba66a
fabioValdes9 <101708072+fabioValdes9@users.noreply.github.com>
2022-03-16 10:59:03 +0100

    just a test

--- Algebras/MainMemory2/PersistentNTree.h
+++ Algebras/MainMemory2/PersistentNTree.h
@@ -25,7 +25,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #ifndef PERSISTENTNTREE_H
 #define PERSISTENTNTREE_H
 
-// #include "MainMemoryExt.h"
 #include "NTree.h"
 
 namespace mm2algebra {
