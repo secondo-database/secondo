26d0a39b2782ffb8be4fbeaaec4a4679c5bbdd33
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2026-07-13 20:28:16 +0200

    Switch to distribution version of 'JSON for Modern C++' library

--- Algebras/Distributed2/JSONProcessor.h
+++ Algebras/Distributed2/JSONProcessor.h
@@ -26,7 +26,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
 
-#include "Tools/Utilities/json.hpp"
+#define JSON_USE_IMPLICIT_CONVERSIONS 0
+#include <nlohmann/json.hpp>
 #include "Profiles.h"
 #include "DistributedJob.h"
 #include <fstream>
--- packaging/debian/secondo_ubuntu_2404/volume/build.sh
+++ packaging/debian/secondo_ubuntu_2404/volume/build.sh
@@ -8,7 +8,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install -y flex bison gcc g++ libdb5.3 libdb5.3-dev libdb5.3++ libdb5.3++-dev db5.3-util libjpeg62 libjpeg62-dev libgsl0-dev libreadline-dev librecode-dev libgmp-dev libncurses-dev libxml2-dev libboost-all-dev build-essential debhelper fakeroot openjdk-8-jdk libxml2 libxml2-dev wget swi-prolog-nox swi-prolog-java swi-prolog libquadmath0 libgmp-dev libgmp10 original-awk libboost-all-dev libbison-dev git vim wget rsync dpkg-dev screen
+apt-get install -y flex bison gcc g++ libdb5.3 libdb5.3-dev libdb5.3++ libdb5.3++-dev db5.3-util libjpeg62 libjpeg62-dev libgsl0-dev libreadline-dev librecode-dev libgmp-dev libncurses-dev libxml2-dev libboost-all-dev build-essential debhelper fakeroot openjdk-8-jdk libxml2 libxml2-dev wget swi-prolog-nox swi-prolog-java swi-prolog libquadmath0 libgmp-dev libgmp10 original-awk libboost-all-dev libbison-dev nlohmann-json3-dev git vim wget rsync dpkg-dev screen
 
 cd /tmp
 
